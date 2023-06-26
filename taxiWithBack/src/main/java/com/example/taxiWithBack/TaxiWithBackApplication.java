dd92b4af715d9a81ef0072efd5644bc0c6803f21
DoSeungJae <dsj5508@naver.com>
2023-06-26 19:04:33 +0900

    1

---
+++ taxiWithBack/src/main/java/com/example/taxiWithBack/TaxiWithBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxiWithBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiWithBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiWithBackApplication.class, args);
+	}
+
+}
---
+++ taxiWithBack/src/test/java/com/example/taxiWithBack/TaxiWithBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxiWithBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiWithBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
