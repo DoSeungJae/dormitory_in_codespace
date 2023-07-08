f0ee2a2cb4eb7e696dd3a8011481e64bcdd11a12
DoSeungJae <dsj5508@naver.com>
2023-07-08 14:41:49 +0900

    로그인 예외처리  기능 추가

--- taxi-with-front/src/hooks/logIn/useLogInButton.js
+++ taxi-with-front/src/hooks/logIn/useLogInButton.js
@@ -7,32 +7,47 @@ import 'bootstrap/dist/js/bootstrap.js';
 function UseLogInButton() {
     const [id, setId] = useState('');
     const [pw, setPw] = useState('');
+    const [idError,setIdError]=useState(false);
+    const [pwError,setPwError]=useState(false);
   
     const buttonPressed = () => {
-      console.log(id, pw);
+      if(id===''){
+        setIdError(true);
+
+      }
+      if(pw===''){
+        setPwError(true);
+      }
+
+      else{
+        
+      
+      }
       
     };
   
     return (
-      <div className="container">
-        <div className="row mt-5">
+      <div class="container">
+        <div class="row mt-5">
           <InputForm
             type="text"
             placeholder="아이디"
             value={id}
             onChange={(e) => setId(e.target.value)}
           />
+          {idError && <small class="mt-2">아이디를 입력해주세요.</small>}
         </div>
-        <div className="row mt-3">
+        <div class="row mt-3">
           <InputForm
             type="password"
             placeholder="비밀번호"
             value={pw}
             onChange={(e) => setPw(e.target.value)}
           />
+          {pwError && <small class="mt-2" style={{ Left: '10px' }}>비밀번호를 입력해주세요.</small>}
         </div>
   
-        <div className="d-grid mt-4 pb-4">
+        <div class="d-grid mt-4 pb-4">
           <Button onClick={buttonPressed}>로그인</Button>
         </div>
       </div>
