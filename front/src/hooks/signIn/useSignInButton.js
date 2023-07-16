ed1cae212dfb88f4fef0292be1b1b54c8b60f80e
DoSeungJae <dsj5508@naver.com>
2023-07-16 18:50:02 +0900

    회원가입 페이지 기틀/닉네임 정규표현식 수정 필요

--- front/src/hooks/signIn/useSignInButton.js
+++ front/src/hooks/signIn/useSignInButton.js
@@ -17,16 +17,18 @@ const UseSignInButton = () => {
 
     const rex_id = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]{6,12}$/;
     const rex_pw = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9!@#$%^&*()]{8,16}$/;
+    const rex_tel = /^010\d{8}$/;
+    const rex_nick=/^[a-zA-Z0-9ㄱ-ㅎ]{1,8}$/;
+
     return (
         <div class="SignInPage">
 
         <div className="container p-4">
         <label class="row mt-3">
-
                 <h6 class="col" id="signInForm">아이디</h6>
                 <div class="col">
-                {id.length!==0 && rex_id.test(id)===false 
-                    && <small id="signInSmall">
+                    {id.length!==0 && rex_id.test(id)===false 
+                        && <small id="signInSmall">
                         양식에 맞지 않아요!
                         </small>}
                 </div>
@@ -40,11 +42,11 @@ const UseSignInButton = () => {
             </label>
 
             <label class="row mt-3">
-                <h6 id="signInForm" >비밀번호</h6>
+                <h6 class="col" id="signInForm" >비밀번호</h6>
                 <div class="col">
                     {pw.length!==0 && rex_pw.test(pw)===false
                     && <small id="signInSmall">
-                        양식에 맞지 않아요!
+                        양식에 맞춰주세요!
                     </small>}
                 </div>
        
@@ -57,11 +59,11 @@ const UseSignInButton = () => {
             </label>
 
             <label class="row mt-3">
-                <h6 id="signInForm">비밀번호 확인</h6>
+                <h6 class="col" id="signInForm">비밀번호 확인</h6>
                 <div class="col">
                         {pw2.length!==0 && pw!==pw2 && 
                         <small id="signInSmall">
-                        비밀번호가 일치하지 않아요!
+                        일치하지 않아요!
                         </small>}
                 </div>
 
@@ -74,10 +76,14 @@ const UseSignInButton = () => {
             </label>
 
             <label class="row mt-3">
-                <h6 id="signInForm">전화번호</h6>
-                {telError && <small id="signInSmall">
-                    
+                <h6 class="col" id="signInForm">전화번호</h6>
+                <div class="col">
+                    {tel.length!==0 && rex_tel.test(tel)===false 
+                    && <small id="signInSmall">
+                        양식에 맞지 않아요!
                     </small>}
+                </div>
+
                 <InputForm
                     type="text"
                     placeholder="'-'가 필요없어요!"
@@ -88,12 +94,13 @@ const UseSignInButton = () => {
 
             <label class="row mt-3">
                 <h6 id="signInForm">닉네임</h6>
-                {nickError && <small id="signInSmall">
-                    
+                {nick.length!==0 && rex_nick.test(nick) 
+                && <small id="signInSmall">
+                    양식을 확인해주세요!
                     </small>}
                 <InputForm
                     type="text"
-                    placeholder="특수문자 없이 8자리 이내 "
+                    placeholder="특수문자 없이 8자리 이하 "
                     value={nick}
                     onChange={(e)=> setNick(e.target.value)}
                 />
