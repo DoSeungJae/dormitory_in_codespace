c261df5084d597bba56180eaccd22a460458785c
DoSeungJae <dsj5508@naver.com>
2023-09-05 14:44:32 +0900

    JwtTokenUtil 파일 생성, field 구성

--- back/src/main/java/com/taxiWithBack/config/SecurityConfig.java
+++ back/src/main/java/com/taxiWithBack/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
---
+++ back/src/main/java/com/taxiWithBack/jwt/JwtTokenUtil.java
@@ -0,0 +1,26 @@
+package com.taxiWithBack.jwt;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtTokenUtil {
+
+    private static final long serialVersionUID=-2550185165626007488L;
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+
+}
