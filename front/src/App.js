6f27094400fb9e82addebdc6ba92f69ef95a2e40
DoSeungJae <dsj5508@naver.com>
2023-10-15 20:22:30 +0900

    버튼 클릭시 서버로 입장 요청 전송(유효성 검사 필요) -> /home/home : 수정 필요, Browser-Router 위치 조정 필요

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,25 @@ function App() {
       
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<HomePage/>}></Route>
+          <Route exact path="/" element={<HomePage/>}></Route> {/*홈 주소 : /home*/ }
+          <Route path="/oreum1"></Route> {/* /home/oreum1 ... */}
+          <Route path="/oreum2"></Route>
+          <Route path="/oreum3"></Route>
+
+          <Route path="/preum1"></Route>
+          <Route path="/preum2"></Route>
+          <Route path="/preum3"></Route>
+          <Route path="/preum4"></Route>
+
+          {/* 기숙사 페이지는 로그인 없이 이동할 수 있도록 함, 단
+            해당 페이지 안에 있는 글을 조회하거나 홈 메뉴를 제외한 하단 메뉴로 이동할 때에는
+            로그인이 필요하도록 하여 서버에서 jwt 유효성 검사가 수행됨..*/}
+
+          <Route path="/myWriting"></Route>
+          <Route path="/newWriting"></Route>
+          <Route path="/alarm"></Route>
+
+
           <Route path="/signIn" element={<SignInPage/>}></Route>
         </Routes>
       </BrowserRouter>  
--- front/src/routeComponents/home/HomePage.js
+++ front/src/routeComponents/home/HomePage.js
@@ -1,15 +1,46 @@
 import React from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
 function HomePage() {
+
+  const buttonToPath = {
+    '오름1': 'oreum1',
+    '오름2': 'oreum2',
+    '오름3': 'oreum3',
+    '푸름1': 'preum1',
+    '푸름2': 'preum2',
+    '푸름3': 'preum3',
+    '푸름4': 'preum4',
+    "홈": "home",
+    "내 글": "myWriting",
+    "글쓰기": "newWriting",
+    "알림": "alarm"
+  };
+
+  const handleButtonClick = async (buttonName) => {
+    const path = buttonToPath[buttonName];
+    if (!path) return; // 만약 해당 버튼 이름이 buttonToPath 객체에 없다면 함수 종료
+
+    try {
+      const response = await axios.post(`http://localhost:8080/api/v1/home/${path}`, {
+        token: "your-token-here"
+      });
+      console.log(response.data); // 서버 응답 데이터 처리
+    } catch (error) {
+      console.error(error);
+    }
+};
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>header</h1>
       </header>
+      
       <main className="App-main">
-      <div className="slide-menu">
-        {['오름1', '오름2', '오름3', '푸름1', '푸름2', '푸름3', '푸름4'].map((item, i) => {
+        <div className="slide-menu">
+          {['오름1', '오름2', '오름3', '푸름1', '푸름2', '푸름3', '푸름4'].map((item, i) => {
             let color;
             if (item.startsWith('오름')) {
                 // forestgreen 계열
@@ -18,28 +49,36 @@ function HomePage() {
                 // skyblue 계열
                 color = `hsl(197, 71%, ${65 - (i-3) * 10}%)`;
             }
+            
             return (
-                <div key={i} className="slide-item" style={{backgroundColor: color, color: '#fff'}}>
-                    {item}
-                </div>
+              <div 
+                key={i} 
+                className="slide-item" 
+                style={{backgroundColor: color, color: '#fff'}}
+                onClick={() => handleButtonClick(item.toLowerCase())}
+              >
+                  {item}
+              </div>
             );
-        })}
-    </div>
-
-
-
-      </main>
-      <footer className="App-footer">
-      <div className="bottom-menu">
-            {['홈','내 글', '글쓰기', '알림'].map((item, i) => (
-            <div key={i} className="menu-item">
-                {item}
-            </div>
-        ))}
+          })}
         </div>
-      </footer>
-    </div>
-  );
+      </main>
+
+      <footer className="App-footer">
+        <div className="bottom-menu">
+          {['홈','내 글', '글쓰기', '알림'].map((item, i) => (
+            <div 
+              key={i} 
+              className="menu-item"
+              onClick={() => handleButtonClick(item.replace(' ', '').toLowerCase())}
+            >
+               {item}
+             </div>
+           ))}
+         </div>
+       </footer>
+     </div>
+   );
 }
 
 export default HomePage;
