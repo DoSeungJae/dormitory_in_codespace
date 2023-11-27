7befa770297a5d6fde6829249692e13a3983f067
DoSeungJae <dsj5508@naver.com>
2023-11-27 23:30:46 +0900

    13

--- front/src/App.js
+++ front/src/App.js
@@ -17,10 +17,6 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage/>}></Route> {/*홈 주소 : /home*/ }
 
-          {/* 기숙사 페이지는 로그인 없이 이동할 수 있도록 함, 단
-            해당 페이지 안에 있는 글을 조회하거나 홈 메뉴를 제외한 하단 메뉴로 이동할 때에는
-            로그인이 필요하도록 하여 서버에서 jwt 유효성 검사가 수행됨..*/}
-
           <Route path="/myWriting" element={<MyWritingPage/>}></Route>
           <Route path="/alarm" element={<AlarmPage/>}></Route>
 
