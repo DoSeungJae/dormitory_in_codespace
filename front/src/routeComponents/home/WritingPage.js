5b7e163449d025e3cac867af4c981cdd32dbc616
DoSeungJae <dsj5508@naver.com>
2023-11-28 20:38:58 +0900

    [UPDATE] WritingPage - 기숙사, 카테고리 선택 메뉴 추가

--- front/src/routeComponents/home/WritingPage.js
+++ front/src/routeComponents/home/WritingPage.js
@@ -8,8 +8,15 @@ function WritingPage() {
     const handleSubmit = (event) => {
         event.preventDefault();
         const post = { title, content};
-        // 여기서 post 객체를 처리하거나 서버로 전송합니다.
+        // 여기서 post 객체를 처리하거나 서버로 전송
     };
+
+    const [selectedOption, setSelectedOption] = useState("");
+
+    const handleChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+
 
     return (
         <div className="App">
@@ -19,16 +26,49 @@ function WritingPage() {
                         
             <main /*className="App-writingPage-main"*/>
 
-                <form className="form-writingPage"onSubmit={handleSubmit} style={{border: 'none',width:"100%"}}>
-                <label style={{width:"100%"}}>
-                    <input type="text" placeholder='제목'  style={{border:'none',outline:'none'}} onChange={e => setTitle(e.target.value)}  />
-                </label>
-                <br />
-                <br />
+                <form className="form-writingPage" onSubmit={handleSubmit} style={{border: 'none'}}>
+                    <input type="text" placeholder='제목'  style={{border:'none',outline:'none',width:'90%'}} onChange={e => setTitle(e.target.value)}  />
+
+                <br/>
+
+                <div className="containerSelect">
+                    <select value={selectedOption} onChange={handleChange} className="dorSelect">
+                        <option value="">기숙사</option>
+                        <option value="0">오름 1</option>
+                        <option value="1">오름 2</option>
+                        <option value="2">오름 3</option>
+                        <option value="3">푸름 1</option>
+                        <option value="4">푸름 2</option>
+                        <option value="5">푸름 3</option>
+                        <option value="6">푸름 4</option>
+
+                    </select>
+                </div>
+
+                <div className="containerSelect">
+                    <select value={selectedOption} onChange={handleChange} className="CateSelect">
+                        <option value="">카테고리</option>
+                        <option value="0">족발•보쌈</option>
+                        <option value="1">찜•탕•찌개</option>
+                        <option value="2">돈까스•일식</option>
+                        <option value="3">피자</option>
+                        <option value="4">고기•구이</option>
+                        <option value="5">백반•죽•국수</option>
+                        <option value="6">양식</option>
+                        <option value="7">치킨</option>
+                        <option value="8">중식</option>
+                        <option value="9">아시안</option>
+                        <option value="10">도시락</option>
+                        <option value="11">분식</option>
+                        <option value="12">카페•디저트</option>
+                        <option value="13">패스트푸드</option>
+                        
+
+                    </select>
+                </div>
                 
-                <label>
-                    <textarea value={content} placeholder='내용을 입력하세요.' style={{border:'none',outline:'none'}} onChange={e => setContent(e.target.value)}  />
-                </label>
+                
+                <textarea value={content} placeholder='내용을 입력하세요.' style={{border:'none',outline:'none',width:'90%'}} onChange={e => setContent(e.target.value)}  />
                 <br />
                 <input type="submit" value="글 작성" />
                 </form>
