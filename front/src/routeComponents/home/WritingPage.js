7d5ed2c3fbafd216ea5615b5ff1b54e2680b5604
DoSeungJae <dsj5508@naver.com>
2023-11-29 23:29:12 +0900

    [EDIT] WritingPage - 드롭다운 생성, 레이아웃 지정 [NEED] 페이지 이벤트 처리, 드롭다운 색 변경

--- front/src/routeComponents/home/WritingPage.js
+++ front/src/routeComponents/home/WritingPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'; 
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 
 function WritingPage() {
     const [step,setStep]=useState(1);
@@ -20,137 +21,62 @@ function WritingPage() {
 
     const buttonPressed = () => {
         //서버로 전송 ,토큰을 담아서 보내야함 -> 유저 정보 파싱
-        
-
     }
-
-    const nextStep=()=>{
-        setStep(step+1);
-    }
-
-
+    
     return (
         <div className="App">
-            {step===1&&(
-                <div className="step1">
-                    <main className='App-main'>
-                        <div className="flexContainer">
-                            <div className="containerSelect">
-                                <select value={dorSelect} onChange={DorHandleChange} className="dorSelect">
-                                    <option value="">기숙사</option>
-                                    <option value="0">오름 1</option>
-                                    <option value="1">오름 2</option>
-                                    <option value="2">오름 3</option>
-                                    <option value="3">푸름 1</option>
-                                    <option value="4">푸름 2</option>
-                                    <option value="5">푸름 3</option>
-                                    <option value="6">푸름 4</option>
-                                </select>
-                            </div>
-                        </div>
-                        <button type="button" className="btn btn-outline-dark" onClick={nextStep}>다음</button>
-                    </main>
 
-                </div>
-            )}
-            {step===2 &&(
-                <div className="step2">
-                    <main className='App-main'>
-                        <div className="containerSelect">
-                            <select value={cateSelect} onChange={CateHandleChange} className="CateSelect">
-                                <option value="">카테고리</option>
-                                <option value="0">족발•보쌈</option>
-                                <option value="1">찜•탕•찌개</option>
-                                <option value="2">돈까스•일식</option>
-                                <option value="3">피자</option>
-                                <option value="4">고기•구이</option>
-                                <option value="5">백반•죽•국수</option>
-                                <option value="6">양식</option>
-                                <option value="7">치킨</option>
-                                <option value="8">중식</option>
-                                <option value="9">아시안</option>
-                                <option value="10">도시락</option>
-                                <option value="11">분식</option>
-                                <option value="12">카페•디저트</option>
-                                <option value="13">패스트푸드</option>
-                            </select>
-                        </div>
-                        <button type="button" className="btn btn-outline-dark" onClick={nextStep}>다음</button>
-                    </main>
-                </div>
-
-
-            )}
-            {step===3 &&(
-                <div className="step3">
-                    <header className="App-writingPage-header">
-                            <h6>글 쓰기</h6> 
-                            <button type="button" className='btn btn-outline-dark'onClick={buttonPressed}>작성</button>
-                    </header>
+            <header className="App-writingPage-header">
+                    <h6>글 쓰기</h6> 
+                    <button type="button" className='btn btn-outline-dark'onClick={buttonPressed}>작성</button>
+            </header>
                                 
-                    <main className="App-main">
+            <main className="App-main">
         
-                        <input type="text" value={title} placeholder='제목'  style={{border:'none',outline:'none',width:'90%'}} onChange={e => setTitle(e.target.value)}  />
+                <input type="text" value={title} placeholder='제목'  style={{border:'none',outline:'none',width:'90%'}} onChange={e => setTitle(e.target.value)}  />
         
-                        <br/>
-                        <br/>
+                <br/>
+                <br/>
         
-                        <textarea value={content} placeholder='내용을 입력하세요.' style={{border:'none',outline:'none',width:'90%'}} onChange={e => setContent(e.target.value)}  />
-                        <br />
-                        
-        
-                    </main>
-                </div>
-            )}
+                <textarea value={content} placeholder='내용을 입력하세요.' style={{border:'none',outline:'none',width:'90%',height:'50%'}} onChange={e => setContent(e.target.value)}  />
+                <br />
+            </main>
 
 
-        
+            <div className="selects">
 
-
-{/*
-            <footer className="App-footer">
-            <div className="flexContainer">
-                <div className="containerSelect">
-                    <select value={dorSelect} onChange={DorHandleChange} className="dorSelect">
-                        <option value="">기숙사</option>
-                        <option value="0">오름 1</option>
-                        <option value="1">오름 2</option>
-                        <option value="2">오름 3</option>
-                        <option value="3">푸름 1</option>
-                        <option value="4">푸름 2</option>
-                        <option value="5">푸름 3</option>
-                        <option value="6">푸름 4</option>
-
-                    </select>
+                <div>
+                    <DropdownButton id="dropdown-item-button" title="기숙사" drop="up">
+                        <Dropdown.Item as="button">오름1</Dropdown.Item>
+                        <Dropdown.Item as="button">오름2</Dropdown.Item>
+                        <Dropdown.Item as="button">오름3</Dropdown.Item>
+                        <Dropdown.Item as="button">푸름1</Dropdown.Item>
+                        <Dropdown.Item as="button">푸름2</Dropdown.Item>
+                        <Dropdown.Item as="button">푸름3</Dropdown.Item>
+                        <Dropdown.Item as="button">푸름4</Dropdown.Item>
+                    </DropdownButton>
                 </div>
 
-                <div className="containerSelect">
-                    <select value={cateSelect} onChange={CateHandleChange} className="CateSelect">
-                        <option value="">카테고리</option>
-                        <option value="0">족발•보쌈</option>
-                        <option value="1">찜•탕•찌개</option>
-                        <option value="2">돈까스•일식</option>
-                        <option value="3">피자</option>
-                        <option value="4">고기•구이</option>
-                        <option value="5">백반•죽•국수</option>
-                        <option value="6">양식</option>
-                        <option value="7">치킨</option>
-                        <option value="8">중식</option>
-                        <option value="9">아시안</option>
-                        <option value="10">도시락</option>
-                        <option value="11">분식</option>
-                        <option value="12">카페•디저트</option>
-                        <option value="13">패스트푸드</option>
-                        
-                    </select>
+                <div>
+                    <DropdownButton id="dropdown-item-button" title="카테고리" drop="up">
+                        <Dropdown.Item as="button"></Dropdown.Item>
+                        <Dropdown.Item as="button">족발•보쌈</Dropdown.Item>
+                        <Dropdown.Item as="button">찜•탕•찌개</Dropdown.Item>
+                        <Dropdown.Item as="button">돈까스•일식</Dropdown.Item>
+                        <Dropdown.Item as="button">피자</Dropdown.Item>
+                        <Dropdown.Item as="button">고기•구이</Dropdown.Item>
+                        <Dropdown.Item as="button">백반•죽•국수</Dropdown.Item>
+                        <Dropdown.Item as="button">양식</Dropdown.Item>
+                        <Dropdown.Item as="button">치킨</Dropdown.Item>
+                        <Dropdown.Item as="button">중식</Dropdown.Item>
+                        <Dropdown.Item as="button">아시안</Dropdown.Item>
+                        <Dropdown.Item as="button">도시락</Dropdown.Item>
+                        <Dropdown.Item as="button">분식</Dropdown.Item>
+                        <Dropdown.Item as="button">카페•디저트</Dropdown.Item>
+                        <Dropdown.Item as="button">패스트푸드</Dropdown.Item>    
+                    </DropdownButton>
                 </div>
             </div>
-            
-
-            </footer>
-*/}
-
-            
 
 
         </div>
