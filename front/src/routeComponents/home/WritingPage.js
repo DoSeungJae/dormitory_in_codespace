d8b695128473f20a5a46ba87ba5cd36253a221f7
DoSeungJae <dsj5508@naver.com>
2023-11-29 11:33:35 +0900

    [ADD] 순차 페이지 생성 [NEED] 순차 페이지(step1,2) 레이아웃 설정,step3에서 전송 버튼으로 http request 전송(hedaer jwt)

--- front/src/routeComponents/home/WritingPage.js
+++ front/src/routeComponents/home/WritingPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'; 
 
 function WritingPage() {
+    const [step,setStep]=useState(1);
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
 
@@ -23,26 +24,88 @@ function WritingPage() {
 
     }
 
+    const nextStep=()=>{
+        setStep(step+1);
+    }
+
 
     return (
         <div className="App">
-            <header className="App-writingPage-header">
-                    <h6>글 쓰기</h6> 
-                    <button type="button" className='btn btn-dark'onClick={buttonPressed}>작성</button>
-            </header>
+            {step===1&&(
+                <div className="step1">
+                    <main className='App-main'>
+                        <div className="flexContainer">
+                            <div className="containerSelect">
+                                <select value={dorSelect} onChange={DorHandleChange} className="dorSelect">
+                                    <option value="">기숙사</option>
+                                    <option value="0">오름 1</option>
+                                    <option value="1">오름 2</option>
+                                    <option value="2">오름 3</option>
+                                    <option value="3">푸름 1</option>
+                                    <option value="4">푸름 2</option>
+                                    <option value="5">푸름 3</option>
+                                    <option value="6">푸름 4</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button type="button" className="btn btn-outline-dark" onClick={nextStep}>다음</button>
+                    </main>
+
+                </div>
+            )}
+            {step===2 &&(
+                <div className="step2">
+                    <main className='App-main'>
+                        <div className="containerSelect">
+                            <select value={cateSelect} onChange={CateHandleChange} className="CateSelect">
+                                <option value="">카테고리</option>
+                                <option value="0">족발•보쌈</option>
+                                <option value="1">찜•탕•찌개</option>
+                                <option value="2">돈까스•일식</option>
+                                <option value="3">피자</option>
+                                <option value="4">고기•구이</option>
+                                <option value="5">백반•죽•국수</option>
+                                <option value="6">양식</option>
+                                <option value="7">치킨</option>
+                                <option value="8">중식</option>
+                                <option value="9">아시안</option>
+                                <option value="10">도시락</option>
+                                <option value="11">분식</option>
+                                <option value="12">카페•디저트</option>
+                                <option value="13">패스트푸드</option>
+                            </select>
+                        </div>
+                        <button type="button" className="btn btn-outline-dark" onClick={nextStep}>다음</button>
+                    </main>
+                </div>
+
+
+            )}
+            {step===3 &&(
+                <div className="step3">
+                    <header className="App-writingPage-header">
+                            <h6>글 쓰기</h6> 
+                            <button type="button" className='btn btn-outline-dark'onClick={buttonPressed}>작성</button>
+                    </header>
+                                
+                    <main className="App-main">
+        
+                        <input type="text" value={title} placeholder='제목'  style={{border:'none',outline:'none',width:'90%'}} onChange={e => setTitle(e.target.value)}  />
+        
+                        <br/>
+                        <br/>
+        
+                        <textarea value={content} placeholder='내용을 입력하세요.' style={{border:'none',outline:'none',width:'90%'}} onChange={e => setContent(e.target.value)}  />
+                        <br />
                         
-            <main className="App-main">
+        
+                    </main>
+                </div>
+            )}
 
-                <input type="text" placeholder='제목'  style={{border:'none',outline:'none',width:'90%'}} onChange={e => setTitle(e.target.value)}  />
 
-                <br/>
-                <br/>
+        
 
-                <textarea value={content} placeholder='내용을 입력하세요.' style={{border:'none',outline:'none',width:'90%'}} onChange={e => setContent(e.target.value)}  />
-                <br />
-                
-
-            </main>
 
 {/*
             <footer className="App-footer">
