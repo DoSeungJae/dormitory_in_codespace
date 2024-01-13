23bce2fd70648c7698732a7d458b17be84e1fe6e
DoSeungJae <dsj5508@naver.com>
2024-01-13 18:02:09 +0900

    [CREATE] ArticlePage.js

--- front/src/routeComponents/article/ArticlePage.js
+++ front/src/routeComponents/article/ArticlePage.js
@@ -1,14 +1,40 @@
-import {React} from 'react'
-import {useLocation} from 'react-router-dom'
+import {React,useState,useEffect} from 'react';
+import axios from 'axios';
+import {useLocation} from 'react-router-dom';
+import BackButton from '../../components/common/BackButton';
+import ThreeDots from '../../components/common/ThreeDots';
 
 function ArticlePage(){
+    const[writerNickName,setWriterNickName]=useState("");
     const location=useLocation();
     const article=location.state;
-    console.log(article);
+
+    const getWriterNickName = async () => {
+        try{
+          const response = await axios.get(`http://localhost:8080/api/v1/user/${article.userId}`, {
+        });
+        const nickName=response.data.nickName;
+        setWriterNickName(nickName);
+        }
+        catch(error){
+          console.log('an error occurred:',error);
+        }
+      }
+
+    useEffect(()=>{
+        getWriterNickName();
+    },[]);
+
 
 
     return (
-        <h1>Article Page</h1>
+        <div className="App">
+            <div className="app-article-header">
+                <BackButton></BackButton>
+                <ThreeDots></ThreeDots>
+
+            </div>
+        </div>
     );
 
 }
