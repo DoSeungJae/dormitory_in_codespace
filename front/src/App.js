e001920e6aa24415ea4e19f203c7546a353549d4
DoSeungJae <dsj5508@naver.com>
2024-02-02 22:48:33 +0900

    [UPDATE] App.js testFunction을  TestContext에 적용

--- front/src/App.js
+++ front/src/App.js
@@ -7,32 +7,38 @@ import WritingPage from './routeComponents/home/WritingPage';
 import MyWritingPage from './routeComponents/home/MyWritingPage';
 import AlarmPage from './routeComponents/home/AlarmPage';
 import ArticlePage from './routeComponents/article/ArticlePage';
-import React,{useState} from 'react';
+import React,{useState,useContext} from 'react';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import TestContext from './components/common/TestContext';
 
 function App() {
+  const testFunction= () => {
+    console.log("전역 함수");
+  };
   
   return (
     <div className="App">
-        <ToastContainer
-          className="toast-position"
-          position='top-center'/>
+      <ToastContainer
+        className="toast-position"
+        position='top-center'/>
 
-          <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<HomePage/>}></Route> 
-          <Route path="/newWriting" element={<WritingPage/>}></Route>
-          <Route path="/myWriting" element={<MyWritingPage/>}></Route>
-          <Route path="/alarm" element={<AlarmPage/>}></Route>
 
-          <Route path="/article" element={<ArticlePage/>}></Route>
-          
-          <Route path="/signIn" element={<SignInPage/>}></Route>
-          <Route path="/logIn" element={<LogInPage/>}></Route>
-        </Routes>
-      </BrowserRouter>
-     
+      <TestContext.Provider value={testFunction}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<HomePage/>}></Route> 
+            <Route path="/newWriting" element={<WritingPage/>}></Route>
+            <Route path="/myWriting" element={<MyWritingPage/>}></Route>
+            <Route path="/alarm" element={<AlarmPage/>}></Route>
+
+            <Route path="/article" element={<ArticlePage/>}></Route>
+            
+            <Route path="/signIn" element={<SignInPage/>}></Route>
+            <Route path="/logIn" element={<LogInPage/>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </TestContext.Provider>
 
       
     </div>
