3bf1e336ef3e2fa62dfeb469d69a6b898b260af7
DoSeungJae <dsj5508@naver.com>
2024-02-15 23:14:29 +0900

    [UPDATE] reportDTO.java

--- back/src/main/java/com/DormitoryBack/domain/report/dto/ReportDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/report/dto/ReportDTO.java
@@ -1,4 +1,19 @@
 package com.DormitoryBack.domain.report.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReportDTO {
+    private Long articleId;
+    private Long commentId;
+    private LocalDateTime time;
+    private String reason;
+
 }
