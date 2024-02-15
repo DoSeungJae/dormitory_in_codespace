0cb0d3797e3f93e25a3f9bf71fad710142f70443
DoSeungJae <dsj5508@naver.com>
2024-02-15 23:15:55 +0900

    [UPDATE] ReportRepositoy.java - interface에 find 메서드 추가 : user,comment,article

--- back/src/main/java/com/DormitoryBack/domain/report/repository/ReportRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/report/repository/ReportRepository.java
@@ -1,7 +1,17 @@
 package com.DormitoryBack.domain.report.repository;
 
+import com.DormitoryBack.domain.article.comment.domain.entity.Comment;
+import com.DormitoryBack.domain.article.domain.entity.Article;
+import com.DormitoryBack.domain.member.entity.User;
 import com.DormitoryBack.domain.report.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportRepository extends JpaRepository<Report,Long> {
+
+    List<Report> findAllByArticle(Article article);
+    List<Report> findAllByComment(Comment comment);
+    List<Report> findAllByUser(User user);
+
 }
