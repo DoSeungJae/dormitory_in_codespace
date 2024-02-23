186b79992ea98787eea47d3eae8d05d4fe9ac7e4
DoSeungJae <dsj5508@naver.com>
2024-02-23 22:05:47 +0900

    [UPDATE] UserResponseDTO.java - 비밀번호는 데이터 필드에 포함하지 않도록 작성

--- back/src/main/java/com/DormitoryBack/domain/member/dto/UserResponseDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/member/dto/UserResponseDTO.java
@@ -1,2 +1,15 @@
-package com.DormitoryBack.domain.member.dto;public class UserResponseDTO {
+package com.DormitoryBack.domain.member.dto;
+
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+    private String eMail;
+    private String nickName;
+
 }
