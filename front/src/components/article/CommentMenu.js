fc106d3b94d1dd4803beb0d2bfe8b79bee029411
DoSeungJae <dsj5508@naver.com>
2024-02-27 19:21:30 +0900

    [UPDATE] CommentMenu.js -  대댓글 아이콘을 눌렀을 때 sweetAlert를 통해 대댓글 모드로 전환 -> commentForm의 placeHolder "대댓글"로 변경, commentForm에 focus 부여

--- front/src/components/article/CommentMenu.js
+++ front/src/components/article/CommentMenu.js
@@ -3,23 +3,44 @@ import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import {IconButton} from '@mui/material';
+import Swal from 'sweetalert2';
+import { RestartAltOutlined } from '@mui/icons-material';
 
-function CommentMenu({isReply,setIsReply}){
+function CommentMenu({setPlaceHolder,inputRef,isReply,setIsReply}){
     const changeToReplyMode = () => {
-        setIsReply(1);
-        console.log(isReply);
+        setTimeout(()=>{
+            setIsReply(1);
+            setPlaceHolder("대댓글을 입력하세요.");
+            inputRef.current.focus();
+        },300)
+
     }
+    //문제 해결 블로그 작성하기!
+    const handleSwal= () => {
+        Swal.fire({
+          confirmButtonColor:"#FF8C00",
+          icon:"question",
+          text: "대댓글을 작성하시겠어요?",
+          confirmButtonText:"예",
+          cancelButtonText:"아니오",
+          showCancelButton: true
+        }).then((result)=>{
+            if(result.isConfirmed){
+                changeToReplyMode();
+            }
+
+        });
+      }
     return (
         <div className="comment-menu">
             <div className="comment-menu-item">
                 <IconButton
                     onClick={() => {
-                    changeToReplyMode();
+                    handleSwal();
                     }}>
                     <ForumOutlinedIcon  fontSize="small"/>
                 </IconButton>
                 
-
             </div>
             <div className="comment-menu-item">
                 <IconButton
@@ -29,7 +50,6 @@ function CommentMenu({isReply,setIsReply}){
                     <ThumbUpOutlinedIcon  fontSize="small"/>
                 </IconButton>
                 
-                
             </div>
             <div className="comment-menu-item">
                 <IconButton
@@ -38,18 +58,10 @@ function CommentMenu({isReply,setIsReply}){
                     }}>
                     <MoreVertOutlinedIcon  fontSize="small"/>
                 </IconButton>
-
-                
+ 
             </div>
         </div>
     )
 }
 
 export default CommentMenu;
-//forum
-//comment
-
-//like
-
-//MoreVert??
-//(~=~ threeDotsMenu)
