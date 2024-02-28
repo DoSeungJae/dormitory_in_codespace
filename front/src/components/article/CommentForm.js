75db857cb3057711278f69d34379f9069b1dddd4
DoSeungJae <dsj5508@naver.com>
2024-02-28 20:38:51 +0900

    [FIX] CommentForm.js - reply모드에서 제출 버튼을 누르면 대댓글 종료 확인 창이 뜨는 문제 해결

--- front/src/components/article/CommentForm.js
+++ front/src/components/article/CommentForm.js
@@ -1,29 +1,39 @@
-import React, { useState} from 'react';
+import React, { useState,useRef} from 'react';
 import { IconButton, TextField } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
 import {toast} from 'react-toastify';
+import Swal from 'sweetalert2';
 
-function CommentForm({placeHolder,setPlaceHolder,inputRef,article_Id,isReply,setIsReply}) {
+function CommentForm({y,rootCommentId,SetRootCommentId,placeHolder,setPlaceHolder,inputRef,article_Id,isReply,setIsReply}) {
   const [comment, setComment] = useState('');
   const navigate=useNavigate();
   const token=localStorage.getItem('token');
-  
+  const formRef=useRef(null);
 
   const sendReply = async () => {
-    console.log("reply mode");
-  }
-  const sendComment = async () => {
-    if(isReply==1){
-      sendReply();
-      return;
+    console.log(rootCommentId);
+    console.log(isReply);
+    if(comment===""){
+      toast.warn("내용을 입력해주세요!");
+      inputRef.current.focus();
     }
-    if(comment==''){
-      toast.warn("댓글 내용을 입력해주세요!");
-      return 
+    else{
+      //서버에 보내기 
+      setIsReply(0);
+      setPlaceHolder("댓글을 입력하세요");
+      setComment("");
     }
 
+  }
+
+  const sendComment = async () => {
+
+    if(comment==''){
+      toast.warn("내용을 입력해주세요!");
+      return 
+    }
     const fullPath = `http://localhost:8080/api/v1/comment/new`;
     const data = {
       articleId:article_Id,
@@ -46,14 +56,45 @@ function CommentForm({placeHolder,setPlaceHolder,inputRef,article_Id,isReply,set
 }
 
 const handleBlur = () => {
+  const rect=formRef.current.getBoundingClientRect();
+  console.log(isReply);
+  console.log(rect.top);
+  console.log(y);
+  if(!(y+10<rect.top)){
+    return 
+  }
+
   if(placeHolder==="대댓글을 입력하세요."){
-    setPlaceHolder("댓글을 입력하세요.");
+    if(comment!=""){
+      Swal.fire({
+        confirmButtonColor:"#FF8C00",
+        icon:"question",
+        text: "대댓글을 작성을 종료하시겠어요?",
+        confirmButtonText:"예",
+        cancelButtonText:"아니오",
+        showCancelButton: true
+      }).then((result)=>{
+          if(result.isConfirmed){
+              setPlaceHolder("댓글을 입력하세요.");
+              setComment("");
+              setIsReply(0);
+              return 
+          }
+          else{
+            inputRef.current.focus();
+          }
+      });
+    }
+    else{
+      setIsReply(0);
+      setPlaceHolder("댓글을 입력하세요.");
+    }
   }
 }
 
   return (
     <div className='App'>
-      <div className="comment-submit" >
+      <div className="comment-submit" ref={formRef} >
         <input
           ref={inputRef}
           type='text'
@@ -65,9 +106,13 @@ const handleBlur = () => {
         />
         <IconButton
           onClick={() => {
-          console.log("button clicked");
-          sendComment();
-                      }}>
+            if(!isReply){
+              sendComment();
+            }
+            else{
+              sendReply();
+            }
+            }}>
           <SendIcon/>
         </IconButton>
       </div>
