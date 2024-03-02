a63a52494b85124073c539d2cf036b8076123a81
DoSeungJae <dsj5508@naver.com>
2024-03-02 22:57:12 +0900

    [UPDATE] Comment.java - Reply 처리를 위한 필드 추가

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/entity/Comment.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/entity/Comment.java
@@ -5,13 +5,18 @@ import com.DormitoryBack.domain.article.domain.entity.Article;
 import com.DormitoryBack.domain.member.entity.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Builder
 @AllArgsConstructor
@@ -21,6 +26,7 @@ import java.time.LocalDateTime;
 @Setter
 @Table(name="comment")
 public class Comment {
+
     @Id
     @GeneratedValue
     @Column(name="id")
@@ -33,7 +39,7 @@ public class Comment {
 
     @JsonProperty
     public Long getArticleId(){
-        return article.getId();
+        return article ==null? null : article.getId();
     }
 
     @JsonIgnore
@@ -53,9 +59,31 @@ public class Comment {
     private LocalDateTime createdTime;
 
     @Column(name="updated")
+    //jsonView?
     private Boolean isUpdated;
 
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.REMOVE,mappedBy = "rootComment")
+    private Set<Comment> replyComments=new HashSet<>();
 
+    @JsonProperty
+    public Set<Comment> getReplyComments(){
+        return replyComments;
+    }
+
+    @JsonIgnore
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="root_comment_id")
+    private Comment rootComment=null;
+
+    public void addReplyComment(Comment replyComment){
+        this.replyComments.add(replyComment);
+        replyComment.setRootComment(this);
+    }
+
+    public void deleteReplyComment(Comment replyComment){
+        this.replyComments.remove(replyComment);
+    }
     public void update(CommentUpdateDTO dto){
         this.isUpdated=true;
         this.content=dto.getContent();
