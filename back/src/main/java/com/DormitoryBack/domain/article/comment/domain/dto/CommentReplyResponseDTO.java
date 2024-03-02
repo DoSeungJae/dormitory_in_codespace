1206d9e78c3604fa59c3d7218554d2b0f96e4988
DoSeungJae <dsj5508@naver.com>
2024-03-02 22:52:51 +0900

    [REFACTOR,UPDATE] CommentReplyResponseDTO.java - 클래스 이름 변경(from CommentResponseDTO.java), 롬복 어노테이션 추가

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/dto/CommentReplyResponseDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/dto/CommentReplyResponseDTO.java
@@ -1,7 +1,14 @@
 package com.DormitoryBack.domain.article.comment.domain.dto;
 
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class CommentResponseDTO {
     String content;
     private LocalDateTime time;
