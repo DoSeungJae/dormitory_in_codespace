8dd24d4ca684ba27390a73d3a93d6bb40919db2e
DoSeungJae <dsj5508@naver.com>
2024-03-02 22:56:13 +0900

    [UPDATE] CommentReplyDTO.java - 롬복 어노테이션 추가

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/dto/CommentReplyDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/dto/CommentReplyDTO.java
@@ -1,5 +1,13 @@
 package com.DormitoryBack.domain.article.comment.domain.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class CommentReplyDTO {
     private Long rootCommentId;
     private String content;
