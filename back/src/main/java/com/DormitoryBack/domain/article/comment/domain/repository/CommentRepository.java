8087dbac108aeff5b9a080b20cd6c100e8ecf99c
DoSeungJae <dsj5508@naver.com>
2024-03-16 16:50:01 +0900

    [FIX] findAllByArticle가 페이지를 반환하도록 코드 수정

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/repository/CommentRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/repository/CommentRepository.java
@@ -3,12 +3,14 @@ package com.DormitoryBack.domain.article.comment.domain.repository;
 import com.DormitoryBack.domain.article.comment.domain.entity.Comment;
 import com.DormitoryBack.domain.article.domain.entity.Article;
 import com.DormitoryBack.domain.member.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-    List<Comment> findAllByArticle(Article article);
+    //List<Comment> findAllByArticle(Article article);
     //articleId가 아니라
     //article로 해야함
 
@@ -16,6 +18,8 @@ public interface CommentRepository extends JpaRepository<Comment,Long> {
     //Entity의 attribute의 이름을 기준으로 Query가 실행되는 것으로 추측
 
     List<Comment> findAllByUser(User user);
+    Page<Comment> findAllByArticle(Article article,Pageable pageable);
+
 
 
 }
