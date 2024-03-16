512b8a63de2d21369ec6461ce642110d74860cb3
DoSeungJae <dsj5508@naver.com>
2024-03-16 22:51:19 +0900

    [UPDATE] CommentPageResponseDTO.java - 페이지네이션 요청을 위한 dto

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/dto/CommentPageResponseDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/dto/CommentPageResponseDTO.java
@@ -1,2 +1,18 @@
-package com.DormitoryBack.domain.article.comment.domain.dto;public class CommentPageResponseDTO {
+package com.DormitoryBack.domain.article.comment.domain.dto;
+
+import com.DormitoryBack.domain.article.comment.domain.entity.Comment;
+import lombok.*;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentPageResponseDTO {
+    List<String> rootComments;
+    List<String> replyComments;
+
 }
