8d4f5f6018c436a81f3299f921ecb7bb035ed139
DoSeungJae <dsj5508@naver.com>
2024-04-11 19:04:13 +0900

    [BACK, UPDATE] ArticleRepository.java - user 필터 기능을 사용하기 위한 jpa repository의 메서드 추가

--- back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package com.DormitoryBack.domain.article.domain.repository;
 
 import com.DormitoryBack.domain.article.domain.entity.Article;
+import com.DormitoryBack.domain.member.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,9 @@ import java.util.List;
 public interface ArticleRepository extends JpaRepository<Article,Long> {
     //List<Article> findAllByDorId(Long dorId);
     Page<Article> findAllByDorId(Long dorId, Pageable pageable);
+
+    Page<Article> findAllByUsrId(User user, Pageable pageable);
+
 
 
 }
