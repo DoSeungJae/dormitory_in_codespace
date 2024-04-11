1ac0fefbfea19c0cf7274ee8ae09cf5b568f8d27
DoSeungJae <dsj5508@naver.com>
2024-04-11 23:24:30 +0900

    [FRONT, UPDATE] MyWritingPage.js - 글쓰기 페이지 구현 중...

--- front/src/routeComponents/home/MyWritingPage.js
+++ front/src/routeComponents/home/MyWritingPage.js
@@ -1,9 +1,22 @@
-import {React} from 'react'
+import {React} from 'react';
+import BackButton from '../../components/article/BackButton';
 
 function MyWritingPage(){
 
     return (
-        <h1>MyWritingPage</h1>
+        <div className='App'>
+            <div className='app-myWritingPage-header'>
+                <BackButton></BackButton>
+            </div>
+            <div className='app-myWritingPage-main'>
+                <div className='myWritings'>
+                    <div className='myWritingPage-main-text'>내가 쓴 글</div>
+                </div>
+                <div className='commentedOn'>
+                    <div className='myWritingPage-main-text'>내가 댓글을 단 글</div>
+                </div>
+            </div>
+        </div>
     );
 
 }
