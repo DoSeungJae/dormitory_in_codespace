5fb9c3704c0cb8fd8d2423551486f1b025473a94
DoSeungJae <dsj5508@naver.com>
2024-04-12 18:08:26 +0900

    [BACK, UPDATE] ArticleRepository.java - comment filter를 구현하기 위해 repository에 query 메서드 선언

--- back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
@@ -5,6 +5,8 @@ import com.DormitoryBack.domain.member.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,6 +16,9 @@ public interface ArticleRepository extends JpaRepository<Article,Long> {
 
     Page<Article> findAllByUsrId(User user, Pageable pageable);
 
+
+    @Query("SELECT a FROM Article a JOIN a.comments c WHERE c.user.id = :usrId")
+    Page<Article> findAllArticlesByCommentedUsrId(@Param("usrId") Long userId, Pageable pageable);
 
 
 }
