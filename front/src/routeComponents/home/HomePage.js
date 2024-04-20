a55a6cd97e5ee341af183213e291ea1d37e3026c
DoSeungJae <dsj5508@naver.com>
2024-04-20 21:55:55 +0900

    [FRONT, UPDATE] HomePage.js - ArticlePreview 컴포넌트의 도입이 추가적인 에러를 발생시키지 않으므로 "preview" 클래스를 해당 컴포넌트로 대체

--- front/src/routeComponents/home/HomePage.js
+++ front/src/routeComponents/home/HomePage.js
@@ -2,6 +2,7 @@ import {React,useEffect,useState,useContext,useRef} from 'react';
 import axios from 'axios';
 import {useNavigate,useLocation} from 'react-router-dom';
 import AlertContext from '../../components/common/AlertContext.js';
+import ArticlePreview from '../../components/article/ArticlePreview.js';
 
 function HomePage() {
   const[articleList,setArticleList]=useState([]);
@@ -410,35 +411,20 @@ return (
              );
             })}
           </div>
-            <div className="preview" ref={articleListRef}>
               
               {articleList===null && <h3>아직 글이 없어요!</h3>}
               
-              {articleList && articleList.map((article, index) => (
-              <div key={index} className="article-item" onClick={() => goArticlePage(article)}>
-                <div className='article-item-summary'>
-                  <div className="article-item-text" 
-                        id='article-item-title'>
-                          {article.title}
-                  </div>
-                  <div className="article-item-text"
-                        id='article-item-content'>
-                          {article.content}
-                  </div>
-                </div>
-                <div className='article-item-icons'>
-                  <div className='dor-icon'
-                    style={calculateDorItemStyle(dorId-1,article.dorId-1)}>
-                    {dorIdToDorName[article.dorId]}
-                  </div>
-          
-                  
-                </div>
-                
-              </div>
-            ))}
+              <ArticlePreview
+              articleList={articleList}
+              articleListRef={articleListRef}
+              goArticlePage={goArticlePage}
+              calculateDorItemStyle={calculateDorItemStyle}
+              saveScrollState={saveScrollState}
+              dorId={dorId}
+              dorIdToDorName={dorIdToDorName}
+              />
 
-            </div>
+
     </main>
 
     <footer className="App-footer">
