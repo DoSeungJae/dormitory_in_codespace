dcafee70b8fdce71e5ad33efa012c9c2b48fd2cf
DoSeungJae <dsj5508@naver.com>
2024-04-21 22:11:53 +0900

    [FRONT, REFACTOR] HomePage.js - 필요없는 코드 제거

--- front/src/routeComponents/home/HomePage.js
+++ front/src/routeComponents/home/HomePage.js
@@ -221,30 +221,7 @@ function HomePage() {
   const token=localStorage.getItem('token');
   const navigate = useNavigate();
 
-  const checkToken = async (item) => {
-    try {
-        const response = await axios.get('http://localhost:8080/api/v1/article/validate', {
-            headers: {
-                'Authorization': `${token}`
-            }
-        });
 
-        if (response.data === true) {
-          const path=buttonToPath[item]
-          saveScrollState();
-          navigate(`/${path}`);
-        } else {
-            saveScrollState();
-            navigate('/logIn',{state:
-              {from:'/',type:"error",
-              message:'회원 정보가 유효하지 않아요! 로그인해주세요.'}
-                              });
-
-        }
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
-  };
 
   const goArticlePage = async (article) => {
     try {
@@ -287,9 +264,6 @@ function HomePage() {
     };
   }
   
-
-  
-  
   const buttonToPath = {
     "홈": "",
     "내 글": "myWriting",
@@ -314,73 +288,7 @@ function HomePage() {
     7:"푸름4"
   };
 
-  const svgMap = {
-    '홈': (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 26" 
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M2 15L12 3l10 12v15a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-15z" />
-          <path d="M9 30V15h6v15" />
-        </svg>
-    ),
-    '내 글': (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <line x1="3" y1="6" x2="17" y2="6" />
-          <line x1="3" y1="12" x2="21" y2="12" />
-          <line x1="3" y1="18" x2="15" y2="18" />
-        </svg>
-    ),
-    '글쓰기': (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M18.5 3a2.5 2.5 0 0 1 3.5 3.5L7.5 21H3v-4.5L18.5 3z" />
-          <path d="M15 5L21 11" />
-        </svg>
-    ),
-    '알림': (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="26"
-          height="26"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M12 22c1.1 0 2-.9 2-2H10c0 1.1.9 2 2 2z" />
-          <path d="M18 16v-5c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.93 6 11v5l-2 2v1h16v-1l-2-2z" />
-        </svg>
-    ),
-  };
+
 
 return (
   <div className="App">
