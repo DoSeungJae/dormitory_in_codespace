ee134316e414756fad22ccb60ff34b2498b49429
DoSeungJae <dsj5508@naver.com>
2024-04-21 22:16:11 +0900

    [FRONT, REFACTOR] ArticlePreview.js - 필요없는 코드 제거

--- front/src/components/article/ArticlePreview.js
+++ front/src/components/article/ArticlePreview.js
@@ -1,6 +1,5 @@
 import {React} from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 function ArticlePreview({articleList,articleListRef,goArticlePage,calculateDorItemStyle,
                         saveScrollState,dorId,dorIdToDorName}){
