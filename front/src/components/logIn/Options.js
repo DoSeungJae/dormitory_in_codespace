f6c5fa277e03bcd61367aaf2c816c1bdd4ba3ed9
DoSeungJae <dsj5508@naver.com>
2024-04-29 20:30:22 +0900

    [FRONT, UPDATE] Options.js

--- front/src/components/logIn/Options.js
+++ front/src/components/logIn/Options.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import {React,useContext} from 'react'
 import 'bootstrap/dist/css/bootstrap.css'; 
 import 'bootstrap/dist/js/bootstrap.js'; 
-import { BrowserRouter as Router,Routes,Route,Link, BrowserRouter} from 'react-router-dom';
-import useSignInButton from '../../hooks/signIn/UseSignInButton';
-import SignInPage from '../../routeComponents/signIn/SignInPage';
- // CSS 파일을 import 합니다.
+import HomeSelectContext from '../home/HomeSelectContext';
 
 const Options = () => {
+    const {selectComponentIndex,setSelectComponentIndex}=useContext(HomeSelectContext);
+    const handleTextClick = () => {
+        setSelectComponentIndex(7);
+    }
     return (
         <div className="continer options-container">
             <div className="row mt-1">
-                <div className="col"><Link to="/signIn">회원가입</Link></div>
+                <div className="col">
+                    <div onClick={handleTextClick}>회원가입</div>    
+                </div>
                 <div className="col">아이디 찾기</div>
                 <div className="col">비밀번호 찾기</div>
             </div>
