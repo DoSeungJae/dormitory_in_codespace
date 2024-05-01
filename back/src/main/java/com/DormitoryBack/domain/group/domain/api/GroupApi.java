fab8baf40bfabc74c2f58b87b6cbf1e1a38c25ea
DoSeungJae <dsj5508@naver.com>
2024-05-01 14:39:24 +0900

    [FRONT, UPDATE] GroupApi.java - group 도메인 controller의 메서드 기틀(스켈레톤 코드) 구현

--- back/src/main/java/com/DormitoryBack/domain/group/domain/api/GroupApi.java
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/api/GroupApi.java
@@ -1,4 +1,46 @@
 package com.DormitoryBack.domain.group.domain.api;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("api/v1/group")
+@Slf4j
 public class GroupApi {
+
+    @GetMapping("/test")
+    public String groupTest(){
+        return "test in group domain";
+    }
+
+    @GetMapping("")
+    public ResponseEntity allGroups(){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity newGroup(){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+    @PatchMapping("/join/{userId}")
+    public ResponseEntity newJoiner(@PathVariable("userId") Long userId){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+    @PatchMapping("quit/{userId}")
+    public ResponseEntity quit(@PathVariable("userId") Long userId){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+     @DeleteMapping("/delete/{groupId}")
+    public ResponseEntity deleteGroup(@PathVariable("groupId") Long groupId){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+
 }
