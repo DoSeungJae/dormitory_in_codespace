97a404ad5292fe2569a36bf39e61acf696188af3
DoSeungJae <dsj5508@naver.com>
2024-05-03 14:07:22 +0900

    [BACK, UPDATE] GroupCreateDto.java - 필요한 데이터필드 설정

--- back/src/main/java/com/DormitoryBack/domain/group/domain/dto/request/GroupCreateDto.java
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/dto/request/GroupCreateDto.java
@@ -1,5 +1,11 @@
 
 package com.DormitoryBack.domain.group.domain.dto.request;
+
+import com.DormitoryBack.domain.article.domain.entity.Article;
+import lombok.Getter;
+
+@Getter
 public class GroupCreateDto {
-    
+    Article article;
+
 }
