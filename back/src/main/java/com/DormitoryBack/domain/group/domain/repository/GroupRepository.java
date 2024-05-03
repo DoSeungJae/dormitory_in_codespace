f21dd4d994c610241e6e1108c1306a90a689b4c5
DoSeungJae <dsj5508@naver.com>
2024-05-03 14:08:41 +0900

    [BACK, UPDATE] GroupRepository.java  - 현재 진행중인 group만 가져올 수 있도록 jpa 메서드 작성

--- back/src/main/java/com/DormitoryBack/domain/group/domain/repository/GroupRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/repository/GroupRepository.java
@@ -1,4 +1,11 @@
 package com.DormitoryBack.domain.group.domain.repository;
 
-public interface GroupRepository {
+
+import com.DormitoryBack.domain.group.domain.entitiy.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group,Long> {
+    List<Group> findAllByIsProceeding(Boolean isProceeding);
 }
