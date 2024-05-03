2b986dbd92dab015e40d02117898514da4626f18
DoSeungJae <dsj5508@naver.com>
2024-05-03 14:09:18 +0900

    [BACK, UPDATE] GroupService.java - 새로운 그룹 만들기, (진행중인) 그룹 모두 가져오기 기능의 비지니스 로직 구현

--- back/src/main/java/com/DormitoryBack/domain/group/domain/service/GroupService.java
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/service/GroupService.java
@@ -1,4 +1,56 @@
 package com.DormitoryBack.domain.group.domain.service;
 
+import com.DormitoryBack.domain.article.domain.entity.Article;
+import com.DormitoryBack.domain.group.domain.dto.request.GroupCreateDto;
+import com.DormitoryBack.domain.group.domain.dto.response.GroupCreatedDto;
+import com.DormitoryBack.domain.group.domain.entitiy.Group;
+import com.DormitoryBack.domain.group.domain.repository.GroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GroupService {
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    public GroupCreatedDto createNewGroup(GroupCreateDto requestDto) {
+        Article article=requestDto.getArticle();
+
+        Group newGroup=Group.builder()
+                .dormId(article.getDorId())
+                .hostId(article.getUserId())
+                .article(article)
+                .createdTime(LocalDateTime.now())
+                .category(article.getCategory())
+                .membersId(new HashSet<>())
+                .isProceeding(true)
+                .build();
+
+        Group saved=groupRepository.save(newGroup);
+        GroupCreatedDto responseDto=GroupCreatedDto.builder()
+                .id(saved.getId())
+                .dormId(saved.getDormId())
+                .hostId(saved.getHostId())
+                .category(saved.getCategory())
+                .build();
+
+        return responseDto;
+    }
+
+    public List<GroupCreatedDto> getAllProceedingGroups() {
+        List<Group> proceedingGroups=groupRepository
+                .findAllByIsProceeding(true);
+
+        List<GroupCreatedDto> responseDto=proceedingGroups
+                .stream()
+                .map(Group::groupToCreatedDto)
+                .collect(Collectors.toList());
+
+        return responseDto;
+
+    }
 }
