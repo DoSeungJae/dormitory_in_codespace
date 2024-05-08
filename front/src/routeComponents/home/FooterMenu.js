812a58f586a115117ea0219b87a75a1cb047e16a
DoSeungJae <dsj5508@naver.com>
2024-05-08 21:28:39 +0900

    [FRONT, DELETE, ALTER] FooterMenu.js - svgMap을 iconMap으로 변경, svg 엘리먼트 대신 mui-icon을 import해서 매핑, '글쓰기' 메뉴를 하단 메뉴바에서 삭제(-> 이후에 HomePage에서 PostingPage로 이동할 수 있는 트리거(icon) 추가 예정)

--- front/src/routeComponents/home/FooterMenu.js
+++ front/src/routeComponents/home/FooterMenu.js
@@ -1,77 +1,20 @@
+import GroupsIcon from '@mui/icons-material/Groups';
+import HomeIcon from '@mui/icons-material/Home';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import NotesIcon from '@mui/icons-material/Notes';
 
 function FooterMenu ({selectMenu,saveScrollState,isEndPage,dorId,scrollPosition,page,
                         selectComponentIndex,setSelectComponentIndex}){
-    const svgMap = {
-        '홈': (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 26" 
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M2 15L12 3l10 12v15a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-15z" />
-              <path d="M9 30V15h6v15" />
-            </svg>
-        ),
-        '내 글': (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <line x1="3" y1="6" x2="17" y2="6" />
-              <line x1="3" y1="12" x2="21" y2="12" />
-              <line x1="3" y1="18" x2="15" y2="18" />
-            </svg>
-        ),
-        '글쓰기': (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M18.5 3a2.5 2.5 0 0 1 3.5 3.5L7.5 21H3v-4.5L18.5 3z" />
-              <path d="M15 5L21 11" />
-            </svg>
-        ),
-        '알림': (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="26"
-              height="26"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M12 22c1.1 0 2-.9 2-2H10c0 1.1.9 2 2 2z" />
-              <path d="M18 16v-5c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.93 6 11v5l-2 2v1h16v-1l-2-2z" />
-            </svg>
-        ),
+    const iconMap = {
+        '홈': <HomeIcon fontSize='medium'/>,
+        '내 글': <NotesIcon fontSize='medium'/>,
+        '알림': <NotificationsNoneOutlinedIcon fontSize='medium'/>,
+        '내 그룹':<GroupsIcon fontSize="medium"/>,
       };
     return (
         <div className='App'>
             <div className="bottom-menu">
-                {['홈','내 글', '글쓰기', '알림'].map((item, i) => (
+                {['홈','내 그룹','내 글', '알림'].map((item, i) => (
                 <div 
                     key={i}
                     className="menu-item"
@@ -92,7 +35,7 @@ function FooterMenu ({selectMenu,saveScrollState,isEndPage,dorId,scrollPosition,
                         }
                     }}
                 >
-                    {svgMap[item]}
+                    {iconMap[item]}
                     {<div style={{fontSize: '1.7vh',paddingTop:'0.5vh'}}>{item}</div>}
                 </div>
                 ))}
