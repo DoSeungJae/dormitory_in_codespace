e7b8f58ec1fbcf1c239649997bc2772334ae3964
DoSeungJae <dsj5508@naver.com>
2024-05-11 13:39:47 +0900

    [BACK, UPDATE] GroupChangedDto.java - (그룹) 참가, 나가기 요청에 대한 response dto

--- back/src/main/java/com/DormitoryBack/domain/group/domain/dto/response/GroupChangedDto.java
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/dto/response/GroupChangedDto.java
@@ -1,4 +1,19 @@
 package com.DormitoryBack.domain.group.domain.dto.response;
 
-public class GroupResponseDto {
+import com.DormitoryBack.domain.member.dto.UserResponseDTO;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class GroupChangedDto {
+    private String mode;
+    //join or exit
+
+    private UserResponseDTO userChanged;
+    //나가거나 들어온 user의 정보
+
+    private long numberOfMember;
+
+
 }
