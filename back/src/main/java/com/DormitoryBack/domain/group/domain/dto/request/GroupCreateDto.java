ddbc762ad4e9b4d7b1438750a2d4d7d7bbec6e9f
DoSeungJae <dsj5508@naver.com>
2024-05-11 19:11:44 +0900

    [BACK, UPDATE] GroupCreateDto.java - 그룹 생성 요청 Dto에 maxCapacity 추가, articleId에 NotNull 어노테이션 추가

--- back/src/main/java/com/DormitoryBack/domain/group/domain/dto/request/GroupCreateDto.java
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/dto/request/GroupCreateDto.java
@@ -3,9 +3,17 @@ package com.DormitoryBack.domain.group.domain.dto.request;
 
 import com.DormitoryBack.domain.article.domain.entity.Article;
 import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
+@Setter
 public class GroupCreateDto {
-    Long articleId;
+
+    @NotNull
+    private Long articleId;
+    private Long maxCapacity;
+
 
 }
