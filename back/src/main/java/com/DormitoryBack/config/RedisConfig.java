95a62e808a9f9eb81a6a936f94a4fee160e2ba76
DoSeungJae <dsj5508@naver.com>
2024-05-11 13:32:26 +0900

    [BACK, FIX] RedisConfig.java - 타입을 Object에서 Long으로 변경하여 group service 계층에서 발생하는 에러 해결

--- back/src/main/java/com/DormitoryBack/config/RedisConfig.java
+++ back/src/main/java/com/DormitoryBack/config/RedisConfig.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String host;
@@ -22,11 +25,12 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String,Object> redisTemplate(){
-        RedisTemplate<String,Object> redisTemplate=new RedisTemplate<>();
+    public RedisTemplate<String,Long> redisTemplate(){
+        RedisTemplate<String,Long> redisTemplate=new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
         redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<Long>(Long.class));
 
         return redisTemplate;
     }
