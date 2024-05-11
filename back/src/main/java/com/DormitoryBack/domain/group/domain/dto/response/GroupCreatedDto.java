1e38307118bafcffa9eca9d9964b96dfcb015189
DoSeungJae <dsj5508@naver.com>
2024-05-11 13:39:04 +0900

    [BACK, UPDATE] GroupCreatedDto.java - ResponseEntity가 접근할 수 있도록 클래스에 Getter 어노테이션 추가

--- back/src/main/java/com/DormitoryBack/domain/group/domain/dto/response/GroupCreatedDto.java
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/dto/response/GroupCreatedDto.java
@@ -1,13 +1,19 @@
 package com.DormitoryBack.domain.group.domain.dto.response;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class GroupCreatedDto {
-    private Long id; //response dto에 꼭 그룹 id가 포함되어야할까?
+    private Long id;
     private Long dormId;
     private Long hostId;
     private Long articleId;
     private String category;
+
 
 }
