b3745c6bc0347b53377bc0d501510aadbb3a357f
DoSeungJae <dsj5508@naver.com>
2024-05-11 13:53:35 +0900

    [BACK, REFACTOR] GroupChangedDto.java - 데이터 필드의 이름 변경

--- back/src/main/java/com/DormitoryBack/domain/group/domain/dto/response/GroupChangedDto.java
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/dto/response/GroupChangedDto.java
@@ -10,7 +10,7 @@ public class GroupChangedDto {
     private String mode;
     //join or exit
 
-    private UserResponseDTO userChanged;
+    private UserResponseDTO userChanges;
     //나가거나 들어온 user의 정보
 
     private long numberOfMember;
