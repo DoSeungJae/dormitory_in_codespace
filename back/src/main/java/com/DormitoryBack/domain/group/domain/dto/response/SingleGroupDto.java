779a9bff50e93b51dc7668a37414333f3d0109a5
DoSeungJae <dsj5508@naver.com>
2024-05-12 12:33:56 +0900

    [BACK, UPDATE] SingleGroupDto.java - 1개의 그룹을 반환하기 위한 resposne용 dto

--- back/src/main/java/com/DormitoryBack/domain/group/domain/dto/response/SingleGroupDto.java
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/dto/response/SingleGroupDto.java
@@ -1,2 +1,24 @@
-package com.DormitoryBack.domain.group.domain.dto.response;public class singleGroupDto {
+package com.DormitoryBack.domain.group.domain.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class SingleGroupDto {
+    private Long id;
+    private Long dormId;
+    private Long hostId;
+    private Long articleId;
+    private LocalDateTime createdTime;
+    private String category;
+    private Long maxCapacity;
+    private Boolean isProceeding;
+    private Long currentNumberOfMembers;
+    private List<String> members;
+    //UserResponseDto -> String
+
 }
