0b71c4e96803e8790119cc662563c97d81a1100d
DoSeungJae <dsj5508@naver.com>
2024-05-12 12:34:13 +0900

    [BACK, UPDATE] dto에 stringify 메서드 추가

--- back/src/main/java/com/DormitoryBack/domain/member/dto/UserResponseDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/member/dto/UserResponseDTO.java
@@ -1,6 +1,9 @@
 package com.DormitoryBack.domain.member.dto;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.*;
 
 @Builder
@@ -12,4 +15,16 @@ public class UserResponseDTO {
     private String eMail;
     private String nickName;
 
+    public String toJsonString(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        String jsonString = "";
+        try {
+            jsonString = objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
 }
