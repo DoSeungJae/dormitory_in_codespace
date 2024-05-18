9857e01e372dfc699af8950642db7c6dcb269bf1
DoSeungJae <dsj5508@naver.com>
2024-05-18 19:21:01 +0900

    [BACK, UPDATE] SocketService.java - builder에 attribute 추가

--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/service/SocketService.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/service/SocketService.java
@@ -4,24 +4,23 @@ import com.DormitoryBack.domain.group.chat.domain.entity.Message;
 import com.DormitoryBack.domain.group.chat.domain.enums.MessageType;
 import com.corundumstudio.socketio.SocketIOClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SocketService {
-
     private final MessageService messageService;
-
     public void sendSocketMessage(SocketIOClient senderClient, Message message, String room) {
-        for (
-                SocketIOClient client: senderClient.getNamespace().getRoomOperations(room).getClients()
-        ) {
+        for (SocketIOClient client: senderClient.getNamespace().getRoomOperations(room).getClients()) {
             if (!client.getSessionId().equals(senderClient.getSessionId())) {
                 client.sendEvent("readMessage", message);
             }
         }
     }
-
     public void saveMessage(SocketIOClient senderClient, Message message) {
         Message storedMessage = messageService.saveMessage(
                 Message.builder()
@@ -29,11 +28,11 @@ public class SocketService {
                         .message(message.getMessage())
                         .room(message.getRoom())
                         .username(message.getUsername())
+                        .createdTime(LocalDateTime.now())
                         .build()
         );
         sendSocketMessage(senderClient, storedMessage, message.getRoom());
     }
-
     public void saveInfoMessage(SocketIOClient senderClient, String message, String room) {
         Message storedMessage = messageService.saveMessage(
                 Message.builder()
