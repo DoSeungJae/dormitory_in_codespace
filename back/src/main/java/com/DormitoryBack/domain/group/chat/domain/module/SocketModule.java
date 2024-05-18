97f4528e9ae7dbf19f149ec506b2981963e0a77a
DoSeungJae <dsj5508@naver.com>
2024-05-18 12:00:15 +0900

    [BACK, FIX] SocketModule.java - 코드 에러 디버깅

--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/module/SocketModule.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/module/SocketModule.java
@@ -10,6 +10,7 @@ import com.corundumstudio.socketio.listener.DisconnectListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -25,7 +26,7 @@ public class SocketModule {
         this.socketService = socketService;
         server.addConnectListener(this.onConnected());
         server.addDisconnectListener(this.onDisconnected());
-        server.addEventListener("send_message", Message.class, this.onChatReceived());
+        server.addEventListener("sendMessage", Message.class, this.onChatReceived());
     }
 
     private DataListener<Message> onChatReceived() {
@@ -34,26 +35,42 @@ public class SocketModule {
             socketService.saveMessage(senderClient, data);
         };
     }
-
+    
     private ConnectListener onConnected() {
         return (client) -> {
             var params = client.getHandshakeData().getUrlParams();
-            String room = params.get("room").stream().collect(Collectors.joining());
-            String username = params.get("username").stream().collect(Collectors.joining());
+
+            // room과 username 값이 null인지 체크하고, null이면 빈 문자열을 사용
+            List<String> roomList = params.get("room");
+            String room = (roomList != null) ? roomList.stream().collect(Collectors.joining()) : "";
+            if(roomList==null){
+                return ;
+            }
+            List<String> usernameList = params.get("username");
+            String username = (usernameList != null) ? usernameList.stream().collect(Collectors.joining()) : "";
+            if(username==null){
+                return ;
+            }
             client.joinRoom(room);
             socketService.saveInfoMessage(client, String.format(Constants.WELCOME_MESSAGE, username), room);
             log.info("Socket ID[{}] - room[{}] - username [{}]  Connected to chat module through", client.getSessionId().toString(), room, username);
         };
-
     }
+
 
     private DisconnectListener onDisconnected() {
         return client -> {
             var params = client.getHandshakeData().getUrlParams();
             String room = params.get("room").stream().collect(Collectors.joining());
+            if(room==null){
+                return ;
+            }
             String username = params.get("username").stream().collect(Collectors.joining());
+            if(username==null){
+                return ;
+            }
             socketService.saveInfoMessage(client, String.format(Constants.DISCONNECT_MESSAGE, username), room);
-            log.info("Socket ID[{}] - room[{}] - username [{}]  discnnected to chat module through", client.getSessionId().toString(), room, username);
+            log.info("Socket ID[{}] - room[{}] - username [{}]  disconnected to chat module through", client.getSessionId().toString(), room, username);
         };
     }
 
