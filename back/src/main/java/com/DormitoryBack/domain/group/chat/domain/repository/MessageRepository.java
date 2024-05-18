7603a862aab3fc642a9f13acedbd13b88ca0ee46
DoSeungJae <dsj5508@naver.com>
2024-05-18 19:18:48 +0900

    [BACK, ALTER] MessageRepository.java - JpaRepository를 MongoRepository로 변경

--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/repository/MessageRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/repository/MessageRepository.java
@@ -1,14 +1,12 @@
 package com.DormitoryBack.domain.group.chat.domain.repository;
 
 import com.DormitoryBack.domain.group.chat.domain.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.List;
 
-@Repository
-public interface MessageRepository extends JpaRepository<Message,Long> {
+@EnableMongoRepositories
+public interface MessageRepository extends MongoRepository<Message,String> {
     List<Message> findAllByRoom(String room);
-    //Page,Pageable로 변경 고려
-
 }
