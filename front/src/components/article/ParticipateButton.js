365a47126e3b32dfd5a069ef21a1f93ab72ed55b
DoSeungJae <dsj5508@naver.com>
2024-05-21 16:51:42 +0900

    [FRONT, UPDATE] ParticipateButton.js - 참여버튼의 그룹 참여 기능 추가

--- front/src/components/article/ParticipateButton.js
+++ front/src/components/article/ParticipateButton.js
@@ -1,21 +1,56 @@
 import { Button } from "@mui/material";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import HomeSelectContext from "../home/HomeSelectContext";
 
-function ParticipateButton() {
+function ParticipateButton({articleId}) {
     const token = localStorage.getItem("token");
-    const participate = async ()  => {
-        console.log(1);
-
+    const {selectComponentIndex,setSelectComponentIndex}=useContext(HomeSelectContext);
+    const [isProceeding, setIsProceeding]=useState(1);
+    
+    const isGroupProceeding = async () => {
+        const path=`http://localhost:8080/api/v1/group/${articleId}`;
+        try{
+            const response=await axios.get(path);
+            console.log(response);
+            //startGroup 기능 구현 이후 코드 작성 예정
+            //startGroup 기능 구현 이후 테스트 가능
+            setIsProceeding(1);
+        }catch(error){
+            if(error.response.data=="GroupNotFound"){
+                setIsProceeding(0);
+            }
+        }
     }
+    const participate = async ()  => {
+        const path=`http://localhost:8080/api/v1/group/participate?groupId=${articleId}`;
+        const headers = {
+            'Authorization':`${token}`
+        };
+        try{
+            const response=await axios.patch(path,{},{headers});
+            console.log(response);
+        }catch(error){
+            console.error(error)
+        }
+    }
+    useEffect(()=>{
+        if(selectComponentIndex!==5){
+            return ;
+        }
+        isGroupProceeding();
+
+    },[selectComponentIndex])
+    
 
     return (
-        <Button 
-            variant="contained"
-            size="small"
-            onClick={participate}
-            sx={{ backgroundColor: '#FF8C00', '&:hover': { backgroundColor: '#FF8C00' } }}
-        >
-            참여
-        </Button>
+        <>
+        {isProceeding==1 && <button className="group-participate-button" onClick={()=>participate()}>그룹 참여</button>}
+        </>
+        
+
+        
+
     );
 }
 
