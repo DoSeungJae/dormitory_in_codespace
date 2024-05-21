a7f82afb139e3a252332765fa53d67887a01d3ca
DoSeungJae <dsj5508@naver.com>
2024-05-21 21:15:11 +0900

    [FRONT, DELETE, UPDATE] ParticipateButton.js [DELETE] mui 버튼 제거, [UPDATE] 예외처리 로직 추가

--- front/src/components/article/ParticipateButton.js
+++ front/src/components/article/ParticipateButton.js
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import HomeSelectContext from "../home/HomeSelectContext";
@@ -29,9 +29,25 @@ function ParticipateButton({articleId}) {
         };
         try{
             const response=await axios.patch(path,{},{headers});
-            console.log(response);
+            console.log(response.data);
+            toast.success('그룹에 참여했어요! "내 그룹"을 확인하세요.');
         }catch(error){
-            console.error(error)
+            const errMsg=error.response.data
+            if(errMsg=="InvalidToken"){
+                //토큰 예외 처리 
+            }
+            else if(errMsg=="GroupIdNotGiven"){
+                toast.warn("해당 그룹을 찾을 수 없어요.")
+            }
+            else if(errMsg==="AlreadyBelongToThisGroup"){
+                toast.warn("이미 이 그룹에 속해있어요.");
+            }
+            else if(errMsg==="DuplicatedParticipation"){
+                toast.warn("다른 그룹에 이미 속해있어요.");
+            }
+            else if(errMsg==="GroupFull"){
+                toast.warn("그룹이 꽉 찼어요!");
+            }
         }
     }
     useEffect(()=>{
