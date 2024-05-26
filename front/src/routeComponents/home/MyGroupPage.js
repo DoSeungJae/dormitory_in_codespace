f636f9ec9dff9d6a6c56884270edde67ab180288
DoSeungJae <dsj5508@naver.com>
2024-05-26 18:13:22 +0900

    [FRONT, UPDATE] home/MyGroupPage.js - 컴포넌트가 선택되면 jwt로 nickname과 groupId를 서버로부터 요청함

--- front/src/routeComponents/home/MyGroupPage.js
+++ front/src/routeComponents/home/MyGroupPage.js
@@ -1,9 +1,72 @@
-import {React} from 'react'
+import {React, useContext, useEffect, useState} from 'react'
+import HomeSelectContext from '../../components/home/HomeSelectContext';
+import axios from 'axios';
 
 function MyGroupPage(){
+    const {selectComponentIndex,setSelectComponentIndex}=useContext(HomeSelectContext);
+    const [nickName,setNickName]=useState("");
+    const [groupId,setGroupId]=useState(0);
+    const token=localStorage.getItem("token");
+    
+    useEffect(()=>{
+        if(selectComponentIndex!=4){
+            return ;   
+        }
+        if(nickName==""){
+            getUserNickNameFromToken();
+        }
+        if(groupId==0){
+            getGroupIdThatUserBelongsTo();
+        }
+        console.log(groupId,nickName);
+
+    },[selectComponentIndex])
+
+    useEffect(()=>{
+        console.log(groupId,nickName);
+    },[nickName,groupId])
+
+    
+  const getUserNickNameFromToken = async () => {
+    if(token==null){
+      return ;
+    }
+    const path=`http://localhost:8080/api/v1/user/NickName`;
+    const headers={
+      'Authorization' : `${token}`
+    };
+    try{
+      const response=await axios.get(path,{headers});
+      setNickName(response.data);
+
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+  const getGroupIdThatUserBelongsTo = async () => {
+    if(token==null){
+      return ;
+    }
+    const path=`http://localhost:8080/api/v1/group/userBelongsTo`;
+    const headers={
+      'Authorization' : `${token}`
+    };
+    try{
+      const response=await axios.get(path,{headers});
+      setGroupId(response.data.id);
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+
+
 
     return (
         <h1>MyGroupPage</h1>
+        
+
     );
 
 }
