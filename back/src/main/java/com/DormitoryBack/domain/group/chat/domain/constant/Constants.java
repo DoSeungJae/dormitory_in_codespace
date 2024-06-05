0d32bdad66f96ffede8e9c5b34a5f8297297de14
DoSeungJae <dsj5508@naver.com>
2024-06-05 18:45:16 +0900

    [BACK, UPDATE] Constants.java (chat) - constant 타입 추가

--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/constant/Constants.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/constant/Constants.java
@@ -3,7 +3,10 @@ package com.DormitoryBack.domain.group.chat.domain.constant;
 public class Constants {
     public static final String WELCOME_MESSAGE = "participated:%s"; // %s <- username
     public static final String DISCONNECT_MESSAGE="disconnected:%s"; // %s <- username
+    public static final String MEMBER_ENTER_GROUP="participatedInGroup:%s"; //%s <- username
+    public static final String MEMBER_ENTER_CHAT="participatedInChat:%s"; // %s <- username
     public static final String MEMBER_EXPELLED="expelledFromGroup:%s"; // %s <- username
+    public static final String MEMBER_LEFT="leftGroup:%s"; // %s <- username
     public static final String GROUP_CLOSED="groupClosed:%s"; // %s <- room
     public static final String GROUP_FINISHED="groupFinished:%s"; // %s <- room
 
