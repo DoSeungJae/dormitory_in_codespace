778b91bea5999f9b963eeaa5da80aa6cd75550b6
DoSeungJae <dsj5508@naver.com>
2024-06-05 14:47:01 +0900

    [BACK, UPDATE] SocketModule,java, SocketService.java - 같은 그룹에서 실시간 채팅 기능 구현

--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/module/SocketModule.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/module/SocketModule.java
@@ -3,37 +3,42 @@ package com.DormitoryBack.domain.group.chat.domain.module;
 import com.DormitoryBack.domain.group.chat.domain.constant.Constants;
 import com.DormitoryBack.domain.group.chat.domain.entity.Message;
 import com.DormitoryBack.domain.group.chat.domain.service.SocketService;
+import com.DormitoryBack.domain.group.domain.entitiy.Group;
+import com.DormitoryBack.domain.group.domain.repository.GroupRepository;
+import com.DormitoryBack.domain.member.dto.UserResponseDTO;
+import com.DormitoryBack.domain.member.entity.User;
+import com.DormitoryBack.domain.member.repository.UserRepository;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DataListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
-
 @Slf4j
 @Component
 public class SocketModule {
     private final SocketIOServer server;
     private final SocketService socketService;
-    public SocketModule(SocketIOServer server, SocketService socketService) {
+    private ChatManager chatManager;
+    public SocketModule(SocketIOServer server, SocketService socketService,ChatManager chatManager) {
         this.server = server;
         this.socketService = socketService;
+        this.chatManager=chatManager;
         server.addConnectListener(this.onConnected());
         server.addDisconnectListener(this.onDisconnected());
         server.addEventListener("sendMessage", Message.class, this.onChatReceived());
     }
-
     private DataListener<Message> onChatReceived() {
         return (senderClient, data, ackSender) -> {
             log.info(data.toString());
-            socketService.saveMessage(senderClient, data);
+            socketService.saveMessage(senderClient, data,chatManager);
         };
     }
-
-    
     private ConnectListener onConnected() {
         return (client) -> {
             var params = client.getHandshakeData().getUrlParams();
@@ -49,13 +54,11 @@ public class SocketModule {
             if(usernameList.isEmpty()){
                 return ;
             }
-            socketService.saveInfoMessage(client, String.format(Constants.WELCOME_MESSAGE, username), room);
+            chatManager.mapUserIdToSocketClient(username,client);
+            socketService.saveInfoMessage(client, String.format(Constants.WELCOME_MESSAGE, username), room,chatManager);
             log.info("Socket ID[{}] - room[{}] - username [{}]  Connected to chat module through", client.getSessionId().toString(), room, username);
-            log.info(usernameList.toString());
         };
     }
-
-
     private DisconnectListener onDisconnected() {
         return client -> {
             var params = client.getHandshakeData().getUrlParams();
@@ -69,7 +72,7 @@ public class SocketModule {
             if(usernameList.isEmpty()){
                 return ;
             }
-            socketService.saveInfoMessage(client, String.format(Constants.DISCONNECT_MESSAGE, username), room);
+            socketService.saveInfoMessage(client, String.format(Constants.DISCONNECT_MESSAGE, username), room,chatManager);
             log.info("Socket ID[{}] - room[{}] - username [{}]  disconnected to chat module through", client.getSessionId().toString(), room, username);
         };
     }
--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/service/SocketService.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/service/SocketService.java
@@ -2,22 +2,42 @@ package com.DormitoryBack.domain.group.chat.domain.service;
 
 import com.DormitoryBack.domain.group.chat.domain.entity.Message;
 import com.DormitoryBack.domain.group.chat.domain.enums.MessageType;
+import com.DormitoryBack.domain.group.chat.domain.module.ChatManager;
+import com.DormitoryBack.domain.group.domain.entitiy.Group;
+import com.DormitoryBack.domain.group.domain.repository.GroupRepository;
 import com.corundumstudio.socketio.SocketIOClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class SocketService {
     private final MessageService messageService;
-    public void sendSocketMessage(SocketIOClient client, Message message, String room) {
-        client.sendEvent("readMessage", message);
+    private final GroupRepository groupRepository;
+    private final RedisTemplate<String,Long> redisTemplate;
+
+    public void sendSocketMessage(SocketIOClient client, Message message, String room, ChatManager chatManager) {
+        SetOperations<String,Long> setOperations=redisTemplate.opsForSet();
+        Set<Long> members= setOperations.members(room);
+        Iterator<Long> iterator=members.iterator();
+        while (iterator.hasNext()){
+            Long memberId= iterator.next();
+            SocketIOClient socketClient=chatManager.getSocketClientByUserId(memberId);
+            if(socketClient==null){
+                return ;
+            }
+            socketClient.sendEvent("readMessage", message);
+        }
     }
-    public void saveMessage(SocketIOClient senderClient, Message message) {
+    public void saveMessage(SocketIOClient senderClient, Message message,ChatManager chatManager) {
         Message storedMessage = messageService.saveMessage(
                 Message.builder()
                         .messageType(MessageType.CLIENT)
@@ -28,9 +48,9 @@ public class SocketService {
                         .build()
         );
         storedMessage.setCreatedTime(null);
-        sendSocketMessage(senderClient, storedMessage, message.getRoom());
+        sendSocketMessage(senderClient, storedMessage, message.getRoom(),chatManager);
     }
-    public void saveInfoMessage(SocketIOClient senderClient, String message, String room) {
+    public void saveInfoMessage(SocketIOClient senderClient, String message, String room, ChatManager chatManager) {
         Message storedMessage = messageService.saveMessage(
                 Message.builder()
                         .messageType(MessageType.SERVER)
@@ -40,6 +60,6 @@ public class SocketService {
                         .build()
         );
         storedMessage.setCreatedTime(null);
-        sendSocketMessage(senderClient, storedMessage, room);
+        sendSocketMessage(senderClient, storedMessage, room,chatManager);
     }
 }
