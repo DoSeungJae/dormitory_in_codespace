81aad2cd9fa8f746bd30bf3e37a0a6c68ae4bbc9
DoSeungJae <dsj5508@naver.com>
2024-06-10 14:10:23 +0900

    [BACK, UPDATE] ServerCommandLineRunner.java - 기존의 채팅 소켓 서버(socketIOServer)에 카프카 컨슈머 소켓 서버(socketIOServerKafka)를 추가하고 @Qaulifier 어노테이션을 추가해 다른 계층에서 선택적으로 socket Server를 import할 수 있도록 설정

--- back/src/main/java/com/DormitoryBack/config/netty/ServerCommandLineRunner.java
+++ back/src/main/java/com/DormitoryBack/config/netty/ServerCommandLineRunner.java
@@ -1,21 +1,28 @@
 package com.DormitoryBack.config.netty;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.corundumstudio.socketio.SocketIOServer;
 
-import lombok.RequiredArgsConstructor;
-
 @Component
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ServerCommandLineRunner implements CommandLineRunner {
 
-    private final SocketIOServer server;
+    private final SocketIOServer chatServer;
+    private final SocketIOServer kafkaServer;
+
+    public ServerCommandLineRunner(
+            @Qualifier("socketIOServer") SocketIOServer chatServer,
+            @Qualifier("socketIOServerKafka") SocketIOServer kafkaServer){
+
+        this.chatServer=chatServer;
+        this.kafkaServer=kafkaServer;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-        server.start();
+        chatServer.start();
+        kafkaServer.start();
     }
 }
