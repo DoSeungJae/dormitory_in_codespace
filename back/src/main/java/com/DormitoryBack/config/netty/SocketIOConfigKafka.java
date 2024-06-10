2d5ef309b617fc284010843c78215881223f3059
DoSeungJae <dsj5508@naver.com>
2024-06-10 14:08:58 +0900

    [BACK, UPDATE] SocketIOConfigKafka.java - 카프카 서버 설정

--- back/src/main/java/com/DormitoryBack/config/netty/SocketIOConfigKafka.java
+++ back/src/main/java/com/DormitoryBack/config/netty/SocketIOConfigKafka.java
@@ -1,2 +1,27 @@
-package com.DormitoryBack.config.netty;public class SocketIOConfigKafka {
+package com.DormitoryBack.config.netty;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SocketIOConfigKafka {
+
+    @Value("${socketServerKafka.host}")
+    private String host;
+
+    @Value("${socketServerKafka.port}")
+    private Integer port;
+
+    @Bean
+    public SocketIOServer socketIOServerKafka(){
+        com.corundumstudio.socketio.Configuration kafkaConfig=new com.corundumstudio.socketio.Configuration();
+        kafkaConfig.setHostname(host);
+        kafkaConfig.setPort(port);
+        kafkaConfig.setOrigin("http://localhost:3000");
+
+        return new SocketIOServer(kafkaConfig);
+    }
+
 }
