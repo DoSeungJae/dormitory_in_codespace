d5efa0616da4a933d6631ac314dd79f221683bd4
DoSeungJae <dsj5508@naver.com>
2024-06-11 21:49:23 +0900

    [BACK, FILE_CREATE, UPDATE] NotificationApi.java - 알림 기능에 대한 http request를 받는 컨트롤러 코드 작성

---
+++ back/src/main/java/com/DormitoryBack/domain/notification/api/NotificationApi.java
@@ -0,0 +1,43 @@
+package com.DormitoryBack.domain.notification.api;
+
+import com.DormitoryBack.domain.notification.dto.NotificationDto;
+import com.DormitoryBack.domain.notification.entitiy.Notification;
+import com.DormitoryBack.domain.notification.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("api/v1/notification")
+@Slf4j
+public class NotificationApi {
+    private final NotificationService notificationService;
+    @Autowired
+    public NotificationApi(NotificationService notificationService){
+        this.notificationService=notificationService;
+    }
+    @GetMapping("")
+    public ResponseEntity allNotifications(){
+        List<NotificationDto> notifications=notificationService.getAllNotifications();
+        return ResponseEntity.status(HttpStatus.OK).body(notifications);
+    }
+    @GetMapping("/unconfirmed")
+    public ResponseEntity allUnconfirmedNotifications(){
+        List<NotificationDto> notifications=notificationService.getAllUnconfirmedNotifications();
+        return ResponseEntity.status(HttpStatus.OK).body(notifications);
+    }
+    @PatchMapping("/confirm/{notificationId}")
+    public ResponseEntity confirm(@PathVariable("notificationId") Long notificationId){
+        notificationService.confirmNotification(notificationId);
+        return ResponseEntity.status(HttpStatus.OK).body("NotificationConfirmed");
+    }
+
+
+
+
+}
