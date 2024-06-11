3ed5c702b2f041e06693c443f8a7ce1acb8cfa86
DoSeungJae <dsj5508@naver.com>
2024-06-11 21:57:45 +0900

    [BACK, PATH_ALTER, NAME_ALTER] KafkaConsumer.java - KafkaConsumerNotification.java

--- back/src/main/java/com/DormitoryBack/infrastructure/kafka/notification/KafkaConsumerNotification.java
+++ back/src/main/java/com/DormitoryBack/infrastructure/kafka/notification/KafkaConsumerNotification.java
@@ -2,16 +2,12 @@ package com.DormitoryBack.infrastructure.kafka.notification;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class KafkaConsumer {
-
-
-
+public class KafkaConsumerNotification {
     @KafkaListener(topics = "notification", groupId = "group-delivery-box")
     public void consume(String message){
       log.info(message);
