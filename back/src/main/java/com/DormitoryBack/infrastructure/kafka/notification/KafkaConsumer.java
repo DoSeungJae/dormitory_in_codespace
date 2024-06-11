4ee35633c3ea61568ec4009e8ab765bcefcdc64d
DoSeungJae <dsj5508@naver.com>
2024-06-11 17:53:10 +0900

    [BACK, UPDATE] KafkaConsumer.java - 카프카 Consumer 코드 작성

--- back/src/main/java/com/DormitoryBack/infrastructure/kafka/notification/KafkaConsumer.java
+++ back/src/main/java/com/DormitoryBack/infrastructure/kafka/notification/KafkaConsumer.java
@@ -1,4 +1,19 @@
-package com.DormitoryBack.infrastructure.kafka;
+package com.DormitoryBack.infrastructure.kafka.notification;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class KafkaConsumer {
+
+
+
+    @KafkaListener(topics = "notification", groupId = "group-delivery-box")
+    public void consume(String message){
+      log.info(message);
+    }
 }
