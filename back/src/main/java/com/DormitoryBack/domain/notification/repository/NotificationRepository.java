0bf4ef49d4f97533da71f0e4a960902cb7a7bd56
DoSeungJae <dsj5508@naver.com>
2024-06-12 12:47:01 +0900

    [BACK, UPDATE] NotificationRepository.java - Native Query(SQL)를 사용하여 notificaiton을 조회하는 메서드 추가

--- back/src/main/java/com/DormitoryBack/domain/notification/repository/NotificationRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/notification/repository/NotificationRepository.java
@@ -2,10 +2,15 @@ package com.DormitoryBack.domain.notification.repository;
 
 import com.DormitoryBack.domain.notification.entitiy.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
     List<Notification> findAllByIsConfirmed(Boolean isConfirmed);
+
+    @Query(value = "SELECT * FROM NOTIFICATION WHERE is_confirmed=0 AND is_valid=1",nativeQuery = true)
+    List<Notification> findAliveNotifications();
+        //isConfirmed : 0 && //isValid : 1인 Notifications
 }
