037c6cdde613c904ea1007cd54c3a862422697f8
DoSeungJae <dsj5508@naver.com>
2024-06-17 12:44:53 +0900

    [BACK, ALTER] NotificationServiceExternal.java - NotificationDto 빌드 방식 변경

--- back/src/main/java/com/DormitoryBack/domain/notification/service/NotificationServiceExternal.java
+++ back/src/main/java/com/DormitoryBack/domain/notification/service/NotificationServiceExternal.java
@@ -8,11 +8,14 @@ import com.DormitoryBack.domain.group.domain.entitiy.Group;
 import com.DormitoryBack.domain.group.domain.repository.GroupRepository;
 import com.DormitoryBack.domain.member.entity.User;
 import com.DormitoryBack.domain.member.repository.UserRepository;
+import com.DormitoryBack.domain.notification.dto.Notifiable;
 import com.DormitoryBack.domain.notification.dto.NotificationDto;
 import com.DormitoryBack.domain.notification.entitiy.Notification;
 import com.DormitoryBack.domain.notification.enums.EntityType;
 import com.DormitoryBack.domain.notification.repository.NotificationRepository;
+//import com.DormitoryBack.global.model.Notifiable;
 import com.DormitoryBack.infrastructure.kafka.notification.KafkaProducerNotification;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -39,51 +42,31 @@ public class NotificationServiceExternal {
 
 
     @Transactional
-    public void saveAndPublishNotification(EntityType subjectType, Long subjectId, EntityType triggerType, Long triggerId,String content){
+    public void saveAndPublishNotification(Notifiable subject, Notifiable trigger, String content){
 
         Notification notification=Notification.builder()
-                .subjectType(subjectType)
-                .subjectId(subjectId)
-                .triggerType(triggerType)
-                .triggerId(triggerId)
+                .subjectType(subject.getEntityType())
+                .subjectId(subject.getEntityId())
+                .triggerType(trigger.getEntityType())
+                .triggerId(trigger.getEntityId())
                 .triggerContent(content)
                 .triggeredDate(LocalDateTime.now())
                 .isConfirmed(false)
-                .isValid(true)
                 .build();
 
         notificationRepository.save(notification);
         NotificationDto dto=NotificationDto.builder()
-                .subjectType(subjectType)
-                .subject(stringifyEntity(subjectId,subjectType))
-                .triggerType(triggerType)
-                .trigger(stringifyEntity(triggerId,triggerType))
+                .subject(subject)
+                .trigger(trigger)
+                .content(content)
                 .build();
 
         producer.send(dto);
     }
 
-    public String stringifyEntity(Long targetId, EntityType type){
-        String entity;
-        if(type==EntityType.ARTICLE){
-            Article article=articleRepository.findById(targetId).orElse(null);
-            entity=article.toJsonString();
-        }
-        else if(type==EntityType.COMMENT){
-            Comment comment=commentRepository.findById(targetId).orElse(null);
-            entity=comment.toJsonString();
-        }
-        else if(type==EntityType.GROUP){
-            Group group=groupRepository.findById(targetId).orElse(null);
-            entity=group.toJsonString();
-        }
-        else{//USER
-            User user=userRepository.findById(targetId).orElse(null);
-            entity=user.toJsonString();
-        }
 
-        return entity;
-    }
+
+
 
 
 }
