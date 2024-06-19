3be6a8be95dfdb91dba5be660e501016ffea6ef2
DoSeungJae <dsj5508@naver.com>
2024-06-19 12:06:47 +0900

    [BACK, UPDATE] MessageService.java - 메시지를 저장할 때 알림 생성

--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/service/MessageService.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/service/MessageService.java
@@ -2,6 +2,11 @@ package com.DormitoryBack.domain.group.chat.domain.service;
 
 import com.DormitoryBack.domain.group.chat.domain.entity.Message;
 import com.DormitoryBack.domain.group.chat.domain.repository.MessageRepository;
+import com.DormitoryBack.domain.group.domain.entitiy.Group;
+import com.DormitoryBack.domain.group.domain.repository.GroupRepository;
+import com.DormitoryBack.domain.notification.dto.Notifiable;
+import com.DormitoryBack.domain.notification.enums.EntityType;
+import com.DormitoryBack.domain.notification.service.NotificationServiceExternal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +17,32 @@ import java.util.List;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class MessageService {
     private final MessageRepository messageRepository;
+    private final GroupRepository groupRepository;
+    private final NotificationServiceExternal notificationService;
 
     public List<Message> getMessages(String room){
         return messageRepository.findAllByRoom(room);
     }
 
     public Message saveMessage(Message message){
-        return messageRepository.save(message);
+        Message saved=messageRepository.save(message);
+
+        Group group=groupRepository.findById(Long.valueOf(message.getRoom())).orElse(null);
+
+        Notifiable subject=Notifiable.builder()
+                .entityType(EntityType.GROUP)
+                .entityId(Long.valueOf(message.getRoom()))
+                .stringifiedEntity(group.toJsonString())
+                .build();
+
+        Notifiable trigger=Notifiable.builder()
+                .entityType(EntityType.MESSAGE)
+                .entityId(Long.valueOf(saved.getId()))
+                .stringifiedEntity(message.toJsonString())
+                .build();
+
+        notificationService.saveAndPublishNotification(subject,trigger,saved.getMessage());
+
+        return saved;
     }
 }
