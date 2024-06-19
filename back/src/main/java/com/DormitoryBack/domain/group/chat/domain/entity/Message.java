de16def2ed8614924c6b544a378f41947028af84
DoSeungJae <dsj5508@naver.com>
2024-06-19 12:06:12 +0900

    [BACK, UPDATE] Message.java - 엔티티에 toJsonString 메서드 추가

--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/entity/Message.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/entity/Message.java
@@ -1,6 +1,9 @@
 package com.DormitoryBack.domain.group.chat.domain.entity;
 
 import com.DormitoryBack.domain.group.chat.domain.enums.MessageType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -24,5 +27,19 @@ public class Message {
     private String username;
     private String message;
     private LocalDateTime createdTime;
+
+    public String toJsonString(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        String jsonString = "";
+        try {
+            jsonString = objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+
 
 }
