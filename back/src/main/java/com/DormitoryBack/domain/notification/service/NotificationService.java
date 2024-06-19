e5238fc4e079074bacc1f35373c36f0626906ea7
DoSeungJae <dsj5508@naver.com>
2024-06-19 22:44:49 +0900

    [BACK, UPDATE] NotificationService.java - laze cascade 기능 메서드화

--- back/src/main/java/com/DormitoryBack/domain/notification/service/NotificationService.java
+++ back/src/main/java/com/DormitoryBack/domain/notification/service/NotificationService.java
@@ -13,6 +13,8 @@ import com.DormitoryBack.domain.notification.dto.NotificationDto;
 import com.DormitoryBack.domain.notification.entitiy.Notification;
 import com.DormitoryBack.domain.notification.enums.EntityType;
 import com.DormitoryBack.domain.notification.repository.NotificationRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,6 +37,8 @@ public class NotificationService {
     private GroupRepository groupRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
     public List<NotificationDto> getAllNotifications() {
         List<Notification> notifications=notificationRepository.findAll();
         List<NotificationDto> dtoList=makeDtoList(notifications);
@@ -53,10 +57,7 @@ public class NotificationService {
         if(notification==null){
             throw new RuntimeException("NotificationNotFound");
         }
-        String subject=stringifyEntity(notification.getSubjectId(),notification.getSubjectType());
-        String trigger=stringifyEntity(notification.getTriggerId(),notification.getTriggerType());
-        if(subject==null || trigger==null) {
-            notificationRepository.delete(notification);
+        if(!checkExistenceOfSubjectAndTriggerWithLazyCascade(notification)){
             throw new RuntimeException("NotificationNotFound");
         }
         notification.setIsConfirmed(true);
@@ -67,12 +68,18 @@ public class NotificationService {
         Iterator<Notification> iterator=notifications.iterator();
         while(iterator.hasNext()){
             Notification notification=iterator.next();
-            String subjectString=stringifyEntity(notification.getSubjectId(),notification.getSubjectType());
-            String triggerString=stringifyEntity(notification.getTriggerId(),notification.getTriggerType());
-            if(subjectString==null || triggerString==null){
-                notificationRepository.delete(notification);
+            if(!checkExistenceOfSubjectAndTriggerWithLazyCascade(notification)){
                 continue;
             }
+            String subjectString=getStringifiedEntity(notification.getSubjectId(),notification.getSubjectType());
+            String triggerString=getStringifiedEntity(notification.getTriggerId(),notification.getTriggerType());
+            /*
+            if(notification.getSubjectType()==EntityType.GROUP){
+                if(!isGroupNotificationValid(notification.getTriggerType())){
+                    continue;
+                }
+            }
+             */
 
             Notifiable subject=Notifiable.builder()
                     .entityType(notification.getSubjectType())
@@ -89,6 +96,7 @@ public class NotificationService {
             NotificationDto dto=NotificationDto.builder()
                     .subject(subject)
                     .trigger(trigger)
+                    .content(notification.getTriggerContent())
                     .build();
 
             dtoList.add(dto);
@@ -96,7 +104,36 @@ public class NotificationService {
         return dtoList;
     }
 
-    public String stringifyEntity(Long targetId, EntityType type){
+    private Boolean checkExistenceOfSubjectAndTriggerWithLazyCascade(Notification notification){
+        String subjectString=getStringifiedEntity(notification.getSubjectId(),notification.getSubjectType());
+        String triggerString=getStringifiedEntity(notification.getTriggerId(),notification.getTriggerType());
+        if(subjectString==null || triggerString==null){
+            notificationRepository.delete(notification);
+            return false;
+        }
+        return true;
+    }
+
+    /*
+    private Boolean isGroupNotificationValid(EntityType triggerType){
+        //group noti의 content부터 정의되어야 작성할 수 있음.
+        switch (triggerType){
+            case GROUP:
+                break;
+            case USER:
+                break;
+            case MESSAGE:
+                break;
+            default:
+                break;
+
+        }
+    }
+
+     */
+
+
+    public String getStringifiedEntity(Long targetId, EntityType type){
         String entity;
         if(type==EntityType.ARTICLE){
             Article article=articleRepository.findById(targetId).orElse(null);
