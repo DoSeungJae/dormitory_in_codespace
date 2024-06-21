e5e17c2f0c061612c87541cad4cabb4aaeb064ef
DoSeungJae <dsj5508@naver.com>
2024-06-21 22:03:25 +0900

    [BACK, DELETE] MessageService.java, EntityType.java (notification) - Message 알림 제거 <- 구현이 너무 복잡함

--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/service/MessageService.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/service/MessageService.java
@@ -2,10 +2,7 @@ package com.DormitoryBack.domain.group.chat.domain.service;
 
 import com.DormitoryBack.domain.group.chat.domain.entity.Message;
 import com.DormitoryBack.domain.group.chat.domain.repository.MessageRepository;
-import com.DormitoryBack.domain.group.domain.entitiy.Group;
 import com.DormitoryBack.domain.group.domain.repository.GroupRepository;
-import com.DormitoryBack.domain.notification.dto.Notifiable;
-import com.DormitoryBack.domain.notification.enums.EntityType;
 import com.DormitoryBack.domain.notification.service.NotificationServiceExternal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,23 +23,6 @@ public class MessageService {
 
     public Message saveMessage(Message message){
         Message saved=messageRepository.save(message);
-
-        Group group=groupRepository.findById(Long.valueOf(message.getRoom())).orElse(null);
-
-        Notifiable subject=Notifiable.builder()
-                .entityType(EntityType.GROUP)
-                .entityId(Long.valueOf(message.getRoom()))
-                .stringifiedEntity(group.toJsonString())
-                .build();
-
-        Notifiable trigger=Notifiable.builder()
-                .entityType(EntityType.MESSAGE)
-                .entityId(Long.valueOf(saved.getId()))
-                .stringifiedEntity(message.toJsonString())
-                .build();
-
-        notificationService.saveAndPublishNotification(subject,trigger,saved.getMessage());
-
         return saved;
     }
 }
--- back/src/main/java/com/DormitoryBack/domain/notification/enums/EntityType.java
+++ back/src/main/java/com/DormitoryBack/domain/notification/enums/EntityType.java
@@ -1,5 +1,5 @@
 package com.DormitoryBack.domain.notification.enums;
 
 public enum EntityType {
-    ARTICLE,COMMENT,GROUP,USER,MESSAGE
+    ARTICLE,COMMENT,GROUP,USER
 }
