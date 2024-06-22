cb4e7c156f14a49210cf436ccae2d67b9408aba0
DoSeungJae <dsj5508@naver.com>
2024-06-22 13:40:21 +0900

    [BACK, FILE_CREATE, UPDATE] GroupServiceExternal - 외부 도메인에서 (notification)에서  group 로직을 사용하기 위한 서비스 클래스 생성 및 기능 추가

---
+++ back/src/main/java/com/DormitoryBack/domain/group/domain/service/GroupServiceExternal.java
@@ -0,0 +1,44 @@
+package com.DormitoryBack.domain.group.domain.service;
+
+import com.DormitoryBack.domain.group.domain.entitiy.Group;
+import com.DormitoryBack.domain.group.domain.repository.GroupRepository;
+import com.DormitoryBack.domain.jwt.TokenProvider;
+import com.DormitoryBack.domain.member.entity.User;
+import com.DormitoryBack.domain.member.repository.UserRepository;
+import io.jsonwebtoken.JwtException;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroupServiceExternal {
+
+    private GroupRepository groupRepository;
+    private RedisTemplate<String,Long> redisTemplate;
+
+    public Long getGroupState(Long groupId){
+        //마감,종료, 진행에 관한 상태만 반환, 0L : 그룹 없음, 1L : 진행 중, -1L : 마감됨, -2L :종료됨
+        Group group=groupRepository.findById(groupId).orElse(null);
+        if(group==null){
+            return 0L;
+        }
+        SetOperations<String,Long> setOperations= redisTemplate.opsForSet();
+        Long numMembers=setOperations.size(String.valueOf(groupId));
+        if(group.getIsProceeding()){
+            return 1L;
+        }
+        else{
+            if(numMembers!=0L){
+                return -1L;
+            }
+            else{
+                return -2L;
+            }
+        }
+    }
+    public Boolean isMember(Long groupId, Long userId){
+        HashOperations<String,Long,Long> hashOperations=redisTemplate.opsForHash();
+        return groupId.equals(hashOperations.get("userBelong",userId));
+    }
+}
