f166a72eddae92559408aa1312f365611930ffb6
DoSeungJae <dsj5508@naver.com>
2024-06-24 17:22:21 +0900

    [FRONT, UPDATE] NotificationPage.js - 알림을 서버에 요청하고 받아온 notification들을 useState 변수에 set하는 코드 추가

--- front/src/routeComponents/home/NotificationPage.js
+++ front/src/routeComponents/home/NotificationPage.js
@@ -1,7 +1,41 @@
-import {React} from 'react'
+import {React, useContext, useEffect, useState} from 'react'
 import BackButton from '../../components/home/BackButton';
+import HomeSelectContext from '../../components/home/HomeSelectContext';
+import axios from 'axios';
 
 function NotificationPage(){
+    const [notiList,setNotiList]=useState([]);
+    const [isFetched,setIsFetched]=useState(0);
+    const token=localStorage.getItem("token");
+    const {selectComponentIndex,setSelectComponentIndex}=useContext(HomeSelectContext);
+
+    const getNotifications= async () => {
+        const path=`http://localhost:8080/api/v1/notification/myNotifications`;
+        const headers={
+            'Authorization':`${token}`
+        };
+        try{
+            const response=await axios.get(path,{headers});
+            const data=response.data;
+            setNotiList((prev)=>[...prev,...data]);
+            setIsFetched(1);
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    useEffect(()=>{
+        if(selectComponentIndex!=3){
+            return ;
+        }
+        if(isFetched==1){
+            return ;
+        }
+        getNotifications();
+    })    
+    useEffect(()=>{
+        console.log(notiList);
+    },[notiList]);
 
     return (
         <div className="App">
