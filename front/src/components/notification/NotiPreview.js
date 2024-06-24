771c51a1ad5f8b9b34e3a9b8bd1333aaa62cbab0
DoSeungJae <dsj5508@naver.com>
2024-06-24 18:01:42 +0900

    [FRONT, UPDATE] NotiPreview.js - 아이콘 매핑 객체 추가

--- front/src/components/notification/NotiPreview.js
+++ front/src/components/notification/NotiPreview.js
@@ -1,12 +1,24 @@
+import GroupsIcon from '@mui/icons-material/Groups';
+import ArticleIcon from '@mui/icons-material/Article';
+import CommentIcon from '@mui/icons-material/Comment';
+import PersonIcon from '@mui/icons-material/Person';
 
 const NotiPreview = ({notiList}) => {
+
+    const triggerIcon={
+        "ARTICLE":<ArticleIcon/>,
+        "COMMENT":<CommentIcon/>,
+        "GRUOP":<GroupsIcon/>,
+        "USER":<PersonIcon/>
+    }
     
     return (
         <div className="preview">
             {notiList && notiList.map((noti,index)=>(
                 <div key={index} className="noti-item" 
                     onClick={console.log()}>
-                    {index}
+                    <div className='noti-item-icon'></div>
+                    <div className='noti-item-main'></div>
                 </div>
 
             ))} 
