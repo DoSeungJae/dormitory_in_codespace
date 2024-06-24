e53c5b5d1b9c5be67256de3e80186dd84c0573bc
DoSeungJae <dsj5508@naver.com>
2024-06-24 15:12:18 +0900

    [FRONT, UPDATE] NotificationPage.js, App.css - 알림 페이지 기틀 구현

--- front/src/routeComponents/home/NotificationPage.js
+++ front/src/routeComponents/home/NotificationPage.js
@@ -4,9 +4,18 @@ import BackButton from '../../components/home/BackButton';
 function NotificationPage(){
 
     return (
-        <header className='App-notificationPage-header'>
-            <BackButton></BackButton>
-        </header>
+        <div className="App">
+            <header className='App-notificationPage-header'>
+                <BackButton></BackButton>
+            </header>
+            <div className="App-notificationPage-main">
+                <div className='notificationPage-main-text'>
+                    알림
+                </div>
+            </div>
+        </div>
+
+        
     );
 
 }
