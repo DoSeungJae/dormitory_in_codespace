34e69fba36c207cc709ac947177ad757fe0529d1
DoSeungJae <dsj5508@naver.com>
2024-06-26 17:17:43 +0900

    [FRONT, UPDATE] NotiPreview.js, App.css - confirm 하지 않은 알림은 붉게 빛나도록 코드 추가, 알림이 생성된 시간 noti-item에 추가

--- front/src/components/notification/NotiPreview.js
+++ front/src/components/notification/NotiPreview.js
@@ -2,27 +2,67 @@ import GroupsIcon from '@mui/icons-material/Groups';
 import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 import PersonIcon from '@mui/icons-material/Person';
 import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
+import axios from 'axios';
+import { throttle } from '../../modules/common/clickHandleModule';
 
 const NotiPreview = ({notiList}) => {
 
     const triggerIcon={
         "ARTICLE":<ArticleOutlinedIcon fontSize='large'/>,
         "COMMENT":<ModeCommentOutlinedIcon fontSize='large'/>,
-        "GRUOP":<GroupsIcon fontSize='large'/>,
+        "GROUP":<GroupsIcon fontSize='large'/>,
         "USER":<PersonIcon fontSize='large'/>
     }
 
-    const handleClick = (subjectType) => {
-        console.log(subjectType);
+    const getFormattedDate = (timeArr) => {
+        const curYear=new Date().getFullYear();
+        const year=timeArr[0]
+        const month=timeArr[1].toString().padStart(2,'0');
+        const day=timeArr[2].toString().padStart(2,'0');
+        const hour=timeArr[3].toString().padStart(2,'0');
+        const minute=timeArr[4].toString().padStart(2,'0');
+        let formattedDate;
+        if(year==curYear){
+            formattedDate=`${month}/${day} ${hour}:${minute}`;
+        }
+        else{
+            formattedDate = `${year}/${month}/${day} ${hour}:${minute}`;
+        }
+
+        return formattedDate;
     }
-    
+
+    const confirmNotification = async (notiId) => {
+        const path=`http://localhost:8080/api/v1/notification/confirm/${notiId}`; 
+        try{
+            const response=await axios.patch(path,{},{});
+            console.log(response.data);
+
+        }catch(error){
+            const errMsg=error.response.data;
+            console.error(errMsg);
+        }
+      }
+
+    const handleClick = (noti) => {
+        //confirmNotification(noti.id);
+        console.log(noti);
+    }
+
+    const throttledHandleClick=throttle(handleClick,500);
+
     return (
         <div className="preview" style={{maxHeight:"80vh"}}>
             {notiList && notiList.map((noti,index)=>(
                 <div key={index} className="noti-item" 
-                    onClick={console.log()}>
-                    <div className='noti-item-icon'>{triggerIcon[notiList[index].trigger.entityType]}</div>
-                    <div className='noti-item-main'>{notiList[index].content}</div>
+                    onClick={() => throttledHandleClick(noti)}
+                    style={noti.isConfirmed === false ? { backgroundColor: "rgba(255, 209, 184, 0.5)" } : {}}>
+                    <div className='noti-item-icon'>{triggerIcon[noti.trigger.entityType]}</div>
+                    <div className='noti-item-content-wrapper'>
+                        <div className='noti-item-content'>{noti.content}</div>
+                        <div className='noti-item-time'>{getFormattedDate(JSON.parse(noti.trigger.stringifiedEntity).createdTime)}</div>
+                    </div>
+                    
                 </div>
 
             ))} 
