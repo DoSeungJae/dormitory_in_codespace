dc79733fb41f2f429dfbe57fea4a165a7aa8430b
DoSeungJae <dsj5508@naver.com>
2024-06-26 12:27:09 +0900

    [FRONT, UPDATE] App.css, NotiPreview.js - 서버로부터 http 요청으로 받은 Notificaiton 인스턴스를 페이지에 렌더링하는 코드 작성, 그에 맞는 스타일 지정

--- front/src/components/notification/NotiPreview.js
+++ front/src/components/notification/NotiPreview.js
@@ -1,24 +1,28 @@
 import GroupsIcon from '@mui/icons-material/Groups';
-import ArticleIcon from '@mui/icons-material/Article';
-import CommentIcon from '@mui/icons-material/Comment';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 import PersonIcon from '@mui/icons-material/Person';
+import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
 
 const NotiPreview = ({notiList}) => {
 
     const triggerIcon={
-        "ARTICLE":<ArticleIcon/>,
-        "COMMENT":<CommentIcon/>,
-        "GRUOP":<GroupsIcon/>,
-        "USER":<PersonIcon/>
+        "ARTICLE":<ArticleOutlinedIcon fontSize='large'/>,
+        "COMMENT":<ModeCommentOutlinedIcon fontSize='large'/>,
+        "GRUOP":<GroupsIcon fontSize='large'/>,
+        "USER":<PersonIcon fontSize='large'/>
+    }
+
+    const handleClick = (subjectType) => {
+        console.log(subjectType);
     }
     
     return (
-        <div className="preview">
+        <div className="preview" style={{maxHeight:"80vh"}}>
             {notiList && notiList.map((noti,index)=>(
                 <div key={index} className="noti-item" 
                     onClick={console.log()}>
-                    <div className='noti-item-icon'></div>
-                    <div className='noti-item-main'></div>
+                    <div className='noti-item-icon'>{triggerIcon[notiList[index].trigger.entityType]}</div>
+                    <div className='noti-item-main'>{notiList[index].content}</div>
                 </div>
 
             ))} 
