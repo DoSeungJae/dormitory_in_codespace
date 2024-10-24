c8e28e91ba09798466c1b08a7885b9846a86b019
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-10-24 12:14:31 +0000

    [BACK, ALTER] NotificationConstants.java - 필요없는 format argument 삭제

--- back/src/main/java/com/DormitoryBack/domain/notification/constant/NotificationConstants.java
+++ back/src/main/java/com/DormitoryBack/domain/notification/constant/NotificationConstants.java
@@ -3,13 +3,14 @@ package com.DormitoryBack.domain.notification.constant;
 import java.util.HashMap;
 
 public class NotificationConstants {
-    public static final String MEMBER_ENTER_GROUP_KOR="%s님이 그룹에 참여했어요.:%s"; //%s <- username %s <-messageCode (in,userId)
+    
     public static final String MEMBER_EXPELLED_KOR="%S님이 그룹에서 추방됐어요."; // %s <- username %s <- messageCode (expelled, userId)
     public static final String MEMBER_LEFT_KOR="%s님이 그룹을 떠났어요."; // %s <- username %s <- messageCode ()
-    public static final String GROUP_CLOSED_KOR="참여하신 그룹(%s)이 마감되었어요. "; // %s <- room
-    public static final String GROUP_FINISHED_KOR="참여하신 그룹(%s)이 종료되었어요."; // %s <- room
+    public static final String GROUP_CLOSED_KOR="참여하신 그룹(%s)이 마감되었어요. "; // %s <- room <- 사용자 경험을 위해 %s는 제외하는게 나아보임
+    public static final String GROUP_FINISHED_KOR="참여하신 그룹(%s)이 종료되었어요."; // %s <- room <- 사용자 경험을 위해 %s는 제외하는게 나아보임
     public static final String NEW_COMMENT="새로운 댓글이 달렸어요: %s";
     public static final String NEW_REPLY="새로운 대댓글이 달렸어요: %s";
+    public static final String MEMBER_ENTER_GROUP_KOR="%s님이 그룹에 참여했어요."; //%s <- username %s <-messageCode (in,userId)
 
     private static final HashMap<String, String> reverseLookupMap = new HashMap<>();
 
