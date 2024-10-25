e7c869c19f191e3bf1a87577aaf8a7b60848a4fa
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-10-25 12:09:02 +0000

    [BACK, UPDATE] NotificationDto.java - triggeredDate 데이터 필드 추가

--- back/src/main/java/com/DormitoryBack/domain/notification/dto/NotificationDto.java
+++ back/src/main/java/com/DormitoryBack/domain/notification/dto/NotificationDto.java
@@ -1,12 +1,13 @@
 package com.DormitoryBack.domain.notification.dto;
 
+import java.time.LocalDateTime;
+
 //import com.DormitoryBack.global.model.Notifiable;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @Getter
@@ -16,6 +17,8 @@ public class NotificationDto {
     private Notifiable trigger;
     private String content;
     private Boolean isConfirmed;
+
+    private LocalDateTime triggeredDate;
 
     public String toJsonString(){
         ObjectMapper objectMapper = new ObjectMapper();
