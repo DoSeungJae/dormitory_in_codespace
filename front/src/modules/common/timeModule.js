6ec0cc7afa4a6339d4152b6b5d73a0780c29fdaf
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-01 09:59:21 +0000

    [FRONT, UPDATE] timeModule.js - 타임 파싱 에러 해결

--- front/src/modules/common/timeModule.js
+++ front/src/modules/common/timeModule.js
@@ -1,18 +1,18 @@
 import dayjs from 'dayjs';
 
 export const getRelativeTime = (absTime) => {
-    console.log(absTime);
+  
     if(Array.isArray(absTime)===true){
         absTime=arrayToLocalDateTimeString(absTime);
     }
     const now=dayjs();
     const created=dayjs(absTime);  
-
     const diffInSeconds = now.diff(created, 'second');
     const diffInMinutes = now.diff(created, 'minute');
     const diffInHours = now.diff(created, 'hour');
     const diffInDays = now.diff(created, 'day');
   
+
     if (diffInSeconds < 60) {
       return `${diffInSeconds}초 전`;
     } else if (diffInMinutes < 60) {
@@ -33,15 +33,25 @@ export const getRelativeTime = (absTime) => {
 
 
 const arrayToLocalDateTimeString = (timeArray) => {
-    const [year, month, day, hour, minute, second, millisecond] = timeArray;
-    const date = new Date(year, month - 1, day, hour, minute, second, millisecond || 0);
+  const [year, month, day, hour, minute, second, millisecond] = timeArray;
   
-    let isoString = date.toISOString();
-  
-    if (timeArray.length === 6) {
-      isoString = isoString.replace('Z', '.0Z');
-    }
-  
-    const localDateTimeString = isoString.split('.')[0] + (timeArray.length === 7 ? `.${millisecond}` : '.0');
-    return localDateTimeString;
+  let truncatedMillisecond = millisecond;
+  if (millisecond && millisecond.toString().length > 3) {
+    truncatedMillisecond = parseInt(millisecond.toString().slice(0, 3), 10);
   }
+  
+  const date = new Date(year, month - 1, day, hour, minute, second, truncatedMillisecond || 0);
+  
+  // ISO 형식으로 변환
+  let isoString = date.toISOString();
+  
+  // 밀리초가 없는 경우 .000 추가
+  if (timeArray.length === 6) {
+    isoString = isoString.replace('Z', '.000Z');
+  } else {
+    // 밀리초가 있는 경우, 자른 밀리초로 대체
+    isoString = isoString.replace(/\.\d{3}Z$/, `.${truncatedMillisecond.toString().padStart(3, '0')}Z`);
+  }
+  
+  return isoString;
+};
