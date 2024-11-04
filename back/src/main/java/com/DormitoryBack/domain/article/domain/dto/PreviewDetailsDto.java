c279160d3c0839b71bb5cc73c0880cbf9aec9163
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-04 12:03:33 +0000

    [BACK, CREATE, UPDATE] PreviewDetailsDto.java - article의 preview 디테일 정보

---
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/dto/PreviewDetailsDto.java
@@ -0,0 +1,18 @@
+package com.DormitoryBack.domain.article.domain.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PreviewDetailsDto {
+    Long commentNum;
+    
+    Long membermNum;
+    
+    Long maxCapacity;
+
+    String nickName;
+    
+    String time;
+
+
+}
