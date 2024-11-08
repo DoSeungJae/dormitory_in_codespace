c53e569dc195dd41e3ccc90d4de70345cceaca6f
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-08 08:03:22 +0000

    [BACK, ALTER] ArticleRepository.java - MySQL을 MongoDB로 변경

--- back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
@@ -4,21 +4,19 @@ import com.DormitoryBack.domain.article.domain.entity.Article;
 import com.DormitoryBack.domain.member.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.List;
 
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+@EnableMongoRepositories 
+public interface ArticleRepository extends MongoRepository<Article,Long> {
     //List<Article> findAllByDorId(Long dorId);
-    Page<Article> findAllByDorId(Long dorId, Pageable pageable);
-
+    Page<Article> findAllByDormId(Long dormId, Pageable pageable);
     Page<Article> findAllByUsrId(User user, Pageable pageable);
 
 
-    @Query("SELECT a FROM Article a JOIN a.comments c WHERE c.user.id = :usrId")
-    Page<Article> findAllArticlesByCommentedUsrId(@Param("usrId") Long userId, Pageable pageable);
+    //@Query("SELECT a FROM Article a JOIN a.comments c WHERE c.user.id = :usrId")
+    //Page<Article> findAllArticlesByCommentedUsrId(@Param("usrId") Long userId, Pageable pageable);
 
 
 }
