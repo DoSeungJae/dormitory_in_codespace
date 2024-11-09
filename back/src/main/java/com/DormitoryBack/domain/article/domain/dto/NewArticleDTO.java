20e4e4f08d8aa4c4c9e3f3297eb23eee59a6cfcd
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-09 09:26:22 +0000

    [BACK, ALTER] NewArticleDTO.java - 필요 없는 데이터 필드 삭제

--- back/src/main/java/com/DormitoryBack/domain/article/domain/dto/NewArticleDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/dto/NewArticleDTO.java
@@ -1,7 +1,6 @@
 package com.DormitoryBack.domain.article.domain.dto;
 
 import lombok.Getter;
-import java.time.LocalDateTime;
 
 @Getter
 public class NewArticleDTO {
@@ -14,25 +13,21 @@ public class NewArticleDTO {
     
     private String category;
     
-    private LocalDateTime createdTime;
-    
     public NewArticleDTO(){}
 
-    public NewArticleDTO(Long dormId, String category, String title, String contentHTML, LocalDateTime createdTime){
+    public NewArticleDTO(Long dormId, String category, String title, String contentHTML){
         this.dormId=dormId;
         this.title=title;
         this.contentHTML=contentHTML;
         this.category=category;
-        this.createdTime=createdTime;
     }
 
-    public NewArticleDTO(Long dormId, String title, String contentHTML, LocalDateTime createdTime){ 
+    public NewArticleDTO(Long dormId, String title, String contentHTML){ 
         this.dormId=dormId;
         this.title=title;
         this.contentHTML=contentHTML;
-        this.createdTime=createdTime;
     }
-    //굳이 createdTime이 들어가야 하나?
+
 
 }
 
