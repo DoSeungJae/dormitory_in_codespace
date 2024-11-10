33c4e15d5fec9fecf4db5e0609d4405fd915d563
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-10 10:33:32 +0000

    [BACK, DELETE] ArticleRepository.java - 기존 SQL에서 쓰던 메서드 삭제

--- back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
@@ -2,21 +2,20 @@ package com.DormitoryBack.domain.article.domain.repository;
 
 import com.DormitoryBack.domain.article.domain.entity.Article;
 import com.DormitoryBack.domain.member.entity.User;
+
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @EnableMongoRepositories 
 public interface ArticleRepository extends MongoRepository<Article,Long> {
-    //List<Article> findAllByDorId(Long dorId);
+
     Page<Article> findAllByDormId(Long dormId, Pageable pageable);
     Page<Article> findAllByUsrId(User user, Pageable pageable);
-
-
-    //@Query("SELECT a FROM Article a JOIN a.comments c WHERE c.user.id = :usrId")
-    //Page<Article> findAllArticlesByCommentedUsrId(@Param("usrId") Long userId, Pageable pageable);
-
 
 }
