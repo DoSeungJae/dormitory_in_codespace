0235a42b887ce26e5843d87cf6ba051e71cb3252
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-10 10:30:37 +0000

    [BACK, CREATE, UPDATE] ArticleDTO.java

--- back/src/main/java/com/DormitoryBack/domain/article/domain/dto/ArticleDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/dto/ArticleDTO.java
@@ -1,5 +1,28 @@
 package com.DormitoryBack.domain.article.domain.dto;
 
+import java.time.LocalDateTime;
+
+import com.DormitoryBack.domain.member.dto.UserResponseDTO;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class ArticleDTO {
+
+    Long id;
+
+    String title;
+
+    String contentHTML;
+
+    Long dormId;
+
+    String category;
+
+    UserResponseDTO user;
     
+    LocalDateTime createdTime;
+
 }
