1dd092051e5efcb57df3e27e888c51fd9fe176e2
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-10 10:30:58 +0000

    [BACK, UPDATE] ArticlePreviewDTO.java - 클래스에 Builder 어노테이션 추가

--- back/src/main/java/com/DormitoryBack/domain/article/domain/dto/ArticlePreviewDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/dto/ArticlePreviewDTO.java
@@ -2,9 +2,11 @@ package com.DormitoryBack.domain.article.domain.dto;
 
 import java.time.LocalDateTime;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ArticlePreviewDTO {
 
     Long id;
