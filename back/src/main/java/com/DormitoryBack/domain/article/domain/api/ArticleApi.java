8f06caf7b060430e85fe83ab3a82ea4b6adc4d25
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-10 10:30:23 +0000

    [BACK, ALTER] ArticleApi.java - 지나친 줄띄움 삭제, 일부 api주소, 변수명 변경, article 하나를 반환하는 메서드는 ArticleDTO로, 여러 개를 반환하는 메서드는 ArticlePreviewDTO 타입을 사용하도록 코드 변경

--- back/src/main/java/com/DormitoryBack/domain/article/domain/api/ArticleApi.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/api/ArticleApi.java
@@ -2,15 +2,15 @@ package com.DormitoryBack.domain.article.domain.api;
 import com.DormitoryBack.domain.article.domain.entity.Article;
 import com.DormitoryBack.domain.jwt.TokenProvider;
 import com.DormitoryBack.domain.article.domain.dto.NewArticleDTO;
+import com.DormitoryBack.domain.article.domain.dto.ArticleDTO;
 import com.DormitoryBack.domain.article.domain.dto.ArticlePreviewDTO;
 import com.DormitoryBack.domain.article.domain.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 @CrossOrigin(origins="https://improved-space-tribble-vjvwrwx956jh69w4-3000.app.github.dev")
 @RestController
@@ -19,11 +19,13 @@ import org.springframework.web.bind.annotation.*;
 public class ArticleApi {
     private final ArticleService articleService;
     private final TokenProvider tokenProvider;
+
     @Autowired
     public ArticleApi(ArticleService articleService, TokenProvider tokenProvider){
         this.tokenProvider=tokenProvider;
         this.articleService=articleService;
     }
+
     @GetMapping("/test")
     public String articleTest(){
         String msg="article test";
@@ -31,107 +33,40 @@ public class ArticleApi {
         return msg;
     }
 
-
     @GetMapping("")
-    public ResponseEntity allArticles(@RequestParam(defaultValue="0") int page,
-                                      @RequestParam(defaultValue="10") int size){
-
-        Page<Article> articles=articleService.getAllArticlesWithinPage(page,size);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(articleService.pageStringify(articles));
+    public ResponseEntity allArticles(@RequestParam(defaultValue="0") int page, @RequestParam(defaultValue="10") int size){
+        List<ArticlePreviewDTO> articlesDTO=articleService.getAllArticlesWithinPage(page,size);
+        return ResponseEntity.status(HttpStatus.OK).body(articlesDTO);
     }
-
 
     @GetMapping("/filter/user")
-    public ResponseEntity articlesByUser(@RequestParam(defaultValue = "0")int page,
-                                         @RequestParam(defaultValue = "10")int size,
-                                         @RequestHeader("Authorization") String token){
-        Page<Article> articles=articleService.getArticlesByUser(page,size,token);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(articleService.pageStringify(articles));
-
+    public ResponseEntity userArticles(@RequestParam(defaultValue = "0")int page, @RequestParam(defaultValue = "10")int size, @RequestHeader("Authorization") String token){
+        List<ArticlePreviewDTO> articlesDTO=articleService.getUserArticlesWithinPage(page,size,token);
+        return ResponseEntity.status(HttpStatus.OK).body(articlesDTO);
     }
 
-    /*
-    @GetMapping("/filter/userComment")
-    public ResponseEntity articlesCommentedFromUser(@RequestParam(defaultValue = "0")int page,
-                                                    @RequestParam(defaultValue = "100")int size,
-                                                    @RequestHeader("Authorization") String token){
-
-        Page<Article> articles=articleService.getArticlesCommentedFromUser(page,size,token);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(articleService.pageStringify(articles));
+    @GetMapping("/filter/userCommented")
+    public ResponseEntity userCommentedArticles(@RequestParam(defaultValue = "0")int page,@RequestParam(defaultValue = "10")int size,@RequestHeader("Authorization") String token){
+        List<ArticlePreviewDTO> articlesDTO=articleService.getUserCommentedArticlesWithinPage(page,size,token);
+        return ResponseEntity.status(HttpStatus.OK).body(articlesDTO);
     }
- */
-
-    @GetMapping("/range")
-    public ResponseEntity allArticleRangePage
-            (@RequestParam(defaultValue="0") int start,
-             @RequestParam(defaultValue="0") int end,
-             @RequestParam(defaultValue = "10") int size){
-
-        Page<Article> articleRangePage=articleService
-                .getAllArticlesWithinRangePage(start,end,size);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(articleService.pageStringify(articleRangePage));
-    }
-
-
-    /*
-    @GetMapping("/dor/{dorId}")
-    public ResponseEntity dorArticles(@PathVariable("dorId") Long dorId){
-        List<Article> articles=articleService.getDorArticles(dorId);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(articleService.listStringify(articles));
-    }
-     */
-    @GetMapping("/dor/{dorId}")
-    public ResponseEntity dorArticlesPerPage(
-            @PathVariable("dorId") Long dorId,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10")int size){
-
-        Page<Article> dorArticlePage=articleService
-                .getDorArticlesPerPage(dorId,page,size);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(articleService.pageStringify(dorArticlePage));
-
-    }
-    @GetMapping("/dor/{dorId}/range")
-    public ResponseEntity dorArticleRangePage(
-            @PathVariable("dorId") Long dorId,
-            @RequestParam(defaultValue = "0") int start,
-            @RequestParam(defaultValue = "0") int end,
-            @RequestParam(defaultValue = "10") int size){
-
-        Page<Article> dorArticleRangePage=articleService.
-                getDorArticlesWithinRangePage(dorId,start,end,size);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(articleService.pageStringify(dorArticleRangePage));
-
-    }
-    @GetMapping("/isArticle/{articleId}")
-    public ResponseEntity isArticle(@PathVariable("articleId") Long articleId){
-        Boolean isArticle=articleService.checkArticleExist(articleId);
-        return ResponseEntity.status(HttpStatus.OK).body(isArticle);
+  
+    @GetMapping("/dorm/{dormId}")
+    public ResponseEntity DormArticles(@PathVariable("dormId") Long dormId, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10")int size){
+        List<ArticlePreviewDTO> articlesDTO=articleService.getDormArticlesWithinPage(dormId,page,size);
+        return ResponseEntity.status(HttpStatus.OK).body(articlesDTO);
     }
 
     @GetMapping("/{articleId}")
     public ResponseEntity article(@PathVariable("articleId") Long articleId){
-        Article article=articleService.getArticle(articleId);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(article.toJsonString());
+        ArticleDTO articleDTO=articleService.getArticle(articleId);
+        return ResponseEntity.status(HttpStatus.OK).body(articleDTO);
+    }
+    
+    @GetMapping("/isArticle/{articleId}")
+    public ResponseEntity isArticle(@PathVariable("articleId") Long articleId){
+        Boolean isArticle=articleService.checkArticleExist(articleId);
+        return ResponseEntity.status(HttpStatus.OK).body(isArticle);
     }
 
     @GetMapping("/writerNickName/{articleId}")
@@ -139,39 +74,29 @@ public class ArticleApi {
         String userNickName=articleService.getWriterNickName(articleId);
         return ResponseEntity.status(HttpStatus.OK).body(userNickName);
     }
-
     
+    @GetMapping("/validate") //WritingPage에 입장하기전 토큰 유효성 검사
+    public ResponseEntity tokenValidation(@RequestHeader("Authorization") String token){
+        Boolean isValid=tokenProvider.validateToken(token);
+        return ResponseEntity.status(HttpStatus.OK).body(isValid);
+    }
 
     @PostMapping("/new")
     public ResponseEntity newArticle(@RequestBody NewArticleDTO dto, @RequestHeader("Authorization") String token){
         Article article=articleService.saveNewArticle(dto,token);
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(article.toJsonString());
+        return ResponseEntity.status(HttpStatus.CREATED).body(article.toJsonString());
     }
 
     @PatchMapping("/{articleId}")
     public ResponseEntity updateArticle(@PathVariable("articleId") Long articleId, @RequestBody NewArticleDTO dto,@RequestHeader("Authorization") String token) {
         Article updatedArticle=articleService.updateArticle(dto,articleId,token);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(updatedArticle.toJsonString());
+        return ResponseEntity.status(HttpStatus.OK).body(updatedArticle.toJsonString());
     }
 
     @DeleteMapping("/{articleId}")
     public ResponseEntity deleteArticle(@PathVariable("articleId") Long articleId, @RequestHeader("Authorization") String token){
         articleService.deleteArticle(articleId,token);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body("ARTICLE DELETED");
-    }
-    @GetMapping("/validate") //글쓰기 페이지에 입장하기전 토큰 유효성 검사
-    public ResponseEntity tokenValidation(@RequestHeader("Authorization") String token){
-        Boolean isValid=tokenProvider.validateToken(token);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(isValid);
+        return ResponseEntity.status(HttpStatus.OK).body("ARTICLE DELETED");
     }
 
 }
