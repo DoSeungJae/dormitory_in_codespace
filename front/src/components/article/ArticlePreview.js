2c737f5c05cb30d94bd7d8ad9319c8b2fb025228
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-12 10:04:59 +0000

    [FRONT, DELETE, UPDATE] ArticlePreview.js - 불필요한 줄띄움 제거, ArticlePreview의 모든 세부 정보(detail)가 보이도록 코드 추가

--- front/src/components/article/ArticlePreview.js
+++ front/src/components/article/ArticlePreview.js
@@ -3,6 +3,7 @@ import { goArticlePage,calculateDorItemStyle,dorIdToDorName } from '../home/Home
 import HomeSelectContext from '../home/HomeSelectContext';
 import commentImage from '../../images/comment.png';
 import groupImage from '../../images/group.png';
+import { getRelativeTime } from '../../modules/common/timeModule';
 
 function ArticlePreview({articleList,articleListRef,page,isEndPage,dorId,heightStyle}){
   //detailList 파라미터로 받기 
@@ -10,8 +11,9 @@ function ArticlePreview({articleList,articleListRef,page,isEndPage,dorId,heightS
   const token=localStorage.getItem('token');
   const {selectComponentIndex,setSelectComponentIndex}=useContext(HomeSelectContext);
   
-  //commentNum, memberNum, maxCapacity, time, nickName
+  //위 결과 : 빈 array
 
+  //commentNum, memberNum, maxCapacity, time, nickName
     return (
         <div className="preview" style={heightStyle} ref={articleListRef}>
               
@@ -19,10 +21,7 @@ function ArticlePreview({articleList,articleListRef,page,isEndPage,dorId,heightS
         
         {articleList && articleList.map((article, index) => (
         <div key={index} className="article-item" 
-        onClick={() => goArticlePage(article,dorId,isEndPage,
-                                    page,token,
-                                    setSelectComponentIndex,
-                                    selectComponentIndex)}>
+        onClick={() => goArticlePage(article,dorId,isEndPage,page,token,setSelectComponentIndex,selectComponentIndex)}>
           <div className='article-item-summary'>
             <div className="article-item-text" 
                   id='article-item-title'>
@@ -30,27 +29,32 @@ function ArticlePreview({articleList,articleListRef,page,isEndPage,dorId,heightS
             </div>
             <div className="article-item-text"
                   id='article-item-content'>
-                    {article.content}
+                    {article.contentText}
             </div>
           </div>
           <div className='article-item-icons'>
             <div className='dor-icon'
-              style={calculateDorItemStyle(dorId-1,article.dorId-1)}>
-              {dorIdToDorName[article.dorId]}
+              style={calculateDorItemStyle(dorId-1,article.dormId-1)}>
+              {dorIdToDorName[article.dormId]}
             </div>
           </div>
 
           <div className='article-item-details'>
             <div className='article-item-details-comment'>
               <img src={commentImage} className="image-comment"alt="Comment"/>
-              <div className='comment-num'>n</div>
+              <div className='comment-num'>{article.numComments}</div>
             </div>
+            {(article.groupNumMembers !== 0 && article.groupMaxCapacity !== 0) && (
             <div className='article-item-details-group'>
-              <img src={groupImage} className="image-group"alt="Group"/>
-              <div className='group-num'>n/n</div>
+              <img src={groupImage} className="image-group" alt="Group" />
+              <div className='group-num'>
+                {article.groupNumMembers}/{article.groupMaxCapacity}
+              </div>
             </div>
-            <div className='article-item-details-time '>n분 전</div>
-            <div className='article-item-details-nickName'>닉네임</div>
+            )}
+
+            <div className='article-item-details-time '>{getRelativeTime(article.createdTime)}</div>
+            <div className='article-item-details-nickName'>{article.userNickName}</div>
           </div>
         </div>
       ))}
