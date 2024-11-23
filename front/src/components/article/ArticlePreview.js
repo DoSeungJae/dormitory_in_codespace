517755c5a63e83a6285afd8a2030210e7bd5dcef
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-23 10:34:01 +0000

    [FRONT, ALTER] ArticlePreview.js - ArticleContext의 자원을 가져와 goArticlePage 함수의 파라미터로 주입, 일부 변수명을 article에서 articlePreview로 변경

--- front/src/components/article/ArticlePreview.js
+++ front/src/components/article/ArticlePreview.js
@@ -4,12 +4,14 @@ import HomeSelectContext from '../home/HomeSelectContext';
 import commentImage from '../../images/comment.png';
 import groupImage from '../../images/group.png';
 import { getRelativeTime } from '../../modules/common/timeModule';
+import ArticleContext from './ArticleContext';
 
 function ArticlePreview({articleList,articleListRef,page,isEndPage,dorId,heightStyle}){
   //detailList 파라미터로 받기 
   
   const token=localStorage.getItem('token');
   const {selectComponentIndex,setSelectComponentIndex}=useContext(HomeSelectContext);
+  const {article,setArticle}=useContext(ArticleContext);
   
   //위 결과 : 빈 array
 
@@ -19,42 +21,42 @@ function ArticlePreview({articleList,articleListRef,page,isEndPage,dorId,heightS
               
         {articleList===null && <h3>아직 글이 없어요!</h3>}
         
-        {articleList && articleList.map((article, index) => (
+        {articleList && articleList.map((articlePreview, index) => (
         <div key={index} className="article-item" 
-        onClick={() => goArticlePage(article,dorId,isEndPage,page,token,setSelectComponentIndex,selectComponentIndex)}>
+        onClick={() => goArticlePage(articlePreview,dorId,isEndPage,page,token,setSelectComponentIndex,selectComponentIndex,setArticle)}>
           <div className='article-item-summary'>
             <div className="article-item-text" 
                   id='article-item-title'>
-                    {article.title}
+                    {articlePreview.title}
             </div>
             <div className="article-item-text"
                   id='article-item-content'>
-                    {article.contentText}
+                    {articlePreview.contentText}
             </div>
           </div>
           <div className='article-item-icons'>
             <div className='dor-icon'
-              style={calculateDorItemStyle(dorId-1,article.dormId-1)}>
-              {dorIdToDorName[article.dormId]}
+              style={calculateDorItemStyle(dorId-1,articlePreview.dormId-1)}>
+              {dorIdToDorName[articlePreview.dormId]}
             </div>
           </div>
 
           <div className='article-item-details'>
             <div className='article-item-details-comment'>
               <img src={commentImage} className="image-comment"alt="Comment"/>
-              <div className='comment-num'>{article.numComments}</div>
+              <div className='comment-num'>{articlePreview.numComments}</div>
             </div>
-            {(article.groupNumMembers !== 0 && article.groupMaxCapacity !== 0) && (
+            {(articlePreview.groupNumMembers !== 0 && articlePreview.groupMaxCapacity !== 0) && (
             <div className='article-item-details-group'>
               <img src={groupImage} className="image-group" alt="Group" />
               <div className='group-num'>
-                {article.groupNumMembers}/{article.groupMaxCapacity}
+                {articlePreview.groupNumMembers}/{articlePreview.groupMaxCapacity}
               </div>
             </div>
             )}
 
-            <div className='article-item-details-time '>{getRelativeTime(article.createdTime)}</div>
-            <div className='article-item-details-nickName'>{article.userNickName}</div>
+            <div className='article-item-details-time '>{getRelativeTime(articlePreview.createdTime)}</div>
+            <div className='article-item-details-nickName'>{articlePreview.userNickName}</div>
           </div>
         </div>
       ))}
