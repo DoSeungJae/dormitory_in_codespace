8a8d61a7d27ad2fda60089d78842b54d5e5aa6ba
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-25 10:58:28 +0000

    [FRONT, FIX] NotiPreview.js - NotificationPage의 ArticlePreview에서 Article로 이동할 때 발생하는 에러 해결

--- front/src/components/notification/NotiPreview.js
+++ front/src/components/notification/NotiPreview.js
@@ -7,11 +7,20 @@ import { throttle } from '../../modules/common/clickHandleModule';
 import { useContext, useEffect } from 'react';
 import HomeSelectContext from '../home/HomeSelectContext';
 import { getRelativeTime } from '../../modules/common/timeModule';
+import ArticleContext from '../article/ArticleContext';
 
 const NotiPreview = ({notiList}) => {
     /* noti에 동적 스타일링을 하지 않는 이유는 클릭했을 때 다른 컴포넌트로 이동하고
     backButton을 통해 notiPage로 돌아오면 Noti들을 다시 fetch하기 때문 */
     const {selectComponentIndex,setSelectComponentIndex}=useContext(HomeSelectContext);
+    const {article, setArticle}=useContext(ArticleContext);
+
+    useEffect(()=>{
+        if(Object.keys(article).length !==0){
+            console.log(article);
+            setSelectComponentIndex(5);
+        }
+    },[article])
 
     const triggerIcon={
         "ARTICLE":<ArticleOutlinedIcon fontSize='large'/>,
@@ -29,11 +38,12 @@ const NotiPreview = ({notiList}) => {
             
             const subjectType=noti.subject.entityType;
             if(subjectType==="ARTICLE"){
-                goToArticlePage(JSON.parse(noti.subject.stringifiedEntity));
+                const articleId=JSON.parse(noti.subject.stringifiedEntity).id;
+                getArticle(articleId).then(result=>goToArticlePage(result)); //goToArticle에서 getArticle 호출하도록 변경하기
             }
             else if(subjectType==="COMMENT"){
                 const articleId=JSON.parse(noti.subject.stringifiedEntity).articleId;
-                getArticle(articleId).then(result=>goToArticlePage(result));
+                getArticle(articleId).then(result=>goToArticlePage(result)); //goToArticle에서 getArticle 호출하도록 변경하기
             }
             else{
                 localStorage.setItem("nextIndex",selectComponentIndex);
@@ -51,17 +61,16 @@ const NotiPreview = ({notiList}) => {
 
     const throttledHandleClick=throttle(handleClick,500);
 
-    const goToArticlePage = async (article) => {
-
-        localStorage.setItem("article",JSON.stringify(article));
+    const goToArticlePage = async (article1) => { //수정 필요 
         localStorage.setItem("nextIndex",selectComponentIndex);
-        setSelectComponentIndex(5);
+        setArticle(article1);
     }
 
     const getArticle = async (articleId) => {
         const path=`https://improved-space-tribble-vjvwrwx956jh69w4-8080.app.github.dev/api/v1/article/${articleId}`;
         try{
             const response=await axios.get(path);
+            console.log("article:",response.data);
             return response.data;
         }
         catch(error){
