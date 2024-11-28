71412f38cb57a2f8d9410b9b978165eed60328ed
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-11-28 12:04:02 +0000

    [FRONT, UPDATE] MyPage.js, App.css - 내 정보 페이지 기틀 구현, css 코드 추가

---
+++ front/src/routeComponents/myInfo/MyPage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import userDefault from '../../images/userDefault.png';
+
+const MyPage = () => {
+
+    return (
+        <div className="App">
+            <div className="myPage-profile">
+                <div className="profile-image"><img src={userDefault} alt="description" className='rounded-image'/></div>
+                <div className="profile-details">
+                    <div className="details-nickName">닉네임</div>
+                    <div className="details-dormitory">기숙사</div>
+                </div>
+            </div>
+            <div className="myPage-info">
+                <div className="info-writings"></div>
+                <div className="info-account">
+                    <div className="account-id"></div>
+                    <div className="account-eMail"></div>
+                    <div className="account-changePassWord"></div>
+                </div>
+                <div className="info-community">
+                    <div className="community-restrictionDetails"></div>
+                    <div className="community-guide"></div>
+                </div>
+                <div className="etc">
+                    <div className="etc-inquiry"></div>
+                    <div className="etc-logOut"></div>
+                    <div className="etc-deleteAccount"></div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MyPage;
