94930728af759c2dc3bfe43fa2acdd0871e019dd
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-02 12:13:53 +0000

    [BACK, CREATE] EmailEncryptor.java, PasswordEncryptor.java - 비밀번호와 이메일을 암호화하는 모듈(클래스) 추가

---
+++ back/src/main/java/com/DormitoryBack/module/crypt/PasswordEncryptor.java
@@ -0,0 +1,22 @@
+package com.DormitoryBack.module.crypt;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncryptor {
+    
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public PasswordEncryptor(){
+        this.passwordEncoder=new BCryptPasswordEncoder();
+    }
+
+    public String encryptPassword(String password){
+        return passwordEncoder.encode(password);
+    }
+
+    public boolean matchesPassword(String rawPassword, String encodedPassword){
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
