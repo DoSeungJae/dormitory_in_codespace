ca999f502820a70280c28e57e8b9944ca3b68e9e
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-03 11:34:07 +0000

    [BACK, COMMENT] UserRequestDTO.java - 주석 추가

--- back/src/main/java/com/DormitoryBack/domain/member/dto/UserRequestDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/member/dto/UserRequestDTO.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserRequestDTO {
 
+    //데이터 필드 이름을 eMail로 설정하면 에러 발생. 따라서 eMail -> mail, 
+    //근데 다른 클래스에서는 eMail로도 되는 경우도 있던데? 왜이럼?
     String mail;
     String passWord;
     String nickName;
