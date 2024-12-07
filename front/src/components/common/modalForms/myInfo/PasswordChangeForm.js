c71cd4ac8678f1e4474c7a0f7c0dd62453c744a3
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-07 12:15:05 +0000

    [FRONT, UPDATE] App.css, PasswordChangeForm.js - 버튼 스타일 수정, 비밀번호를 변경하는 로직 구현함.

--- front/src/components/common/modalForms/myInfo/PasswordChangeForm.js
+++ front/src/components/common/modalForms/myInfo/PasswordChangeForm.js
@@ -1,22 +1,50 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import ModalContext from "../../ModalContext";
 
-function PasswordChangeForm() {
+function PasswordChangeForm({userId}) {
+
     const [currentPassword, setCurrentPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+    const [newPasswordAgain, setNewPasswordAgain] = useState("");
+    const {isOpen, openModal, closeModal}=useContext(ModalContext);
+    
+    const handleButton = () => {
+        if(currentPassword=="" || newPassword=="" || newPasswordAgain==""){
+            toast.warn("비워진 부분이 있어요, 모두 입력해주세요.");
+            return ;
+        }
+        if(newPassword!==newPasswordAgain){
+            toast.warn("확인 비밀번호가 올바르지 않아요!");
+            return ;
+        }
+        changePassword();
+    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // http request 호출하기
-    };
+    const changePassword = async () => {
+        const path=`https://improved-space-tribble-vjvwrwx956jh69w4-8080.app.github.dev/api/v1/user/${userId}`;
+        const body={
+            passWord:newPassword,
+            confirmPassword:currentPassword
+          };
+        try{
+            const response=await axios.patch(path,body,{});
+            toast.success("변경 사항을 반영했어요!");
+            closeModal();
+        }catch(error){
+            console.error(error);
+        }
+    }
 
     return (
         <div>
             <div className="modal-title">비밀번호 바꾸기</div>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <div className="modal-container">
                     <div className="modal-container-title">현재 비밀번호</div>
                     <input
+                        class="form-control form-control-sm"
                         type="password"
                         placeholder="현재 비밀번호"
                         value={currentPassword}
@@ -26,22 +54,24 @@ function PasswordChangeForm() {
                 <div className="modal-container">
                     <div className="modal-container-title">변경할 비밀번호</div>
                     <input
+                        class="form-control form-control-sm"
                         type="password"
                         placeholder="변경할 비밀번호"
                         value={newPassword}
                         onChange={(e) => setNewPassword(e.target.value)}
                     />
                 </div>
-                <div className="modal-container">
+                <div className="modal-container" style={{paddingBottom:'2vh'}}>
                     <div className="modal-container-title">변경할 비밀번호 확인</div>
                     <input
+                        class="form-control form-control-sm"
                         type="password"
                         placeholder="변경할 비밀번호 확인"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        value={newPasswordAgain}
+                        onChange={(e) => setNewPasswordAgain(e.target.value)}
                     />
                 </div>
-                <button type="submit">변경하기</button>
+                <button className="modal-button" type="button" class="btn btn-primary btn-sm" onClick={handleButton}>변경하기</button>
             </form>
         </div>
     );
