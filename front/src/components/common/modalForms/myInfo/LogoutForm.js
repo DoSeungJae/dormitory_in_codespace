58a50cf705f5fd16ece8d28297f84036a88f54c9
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-12 10:12:43 +0000

    [FRONT, CREATE] LogoutForm.js - 로그아웃 폼 작성

---
+++ front/src/components/common/modalForms/myInfo/LogoutForm.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import ModalContext from "../../ModalContext";
+
+function LogoutForm () {
+
+    const {isOpen, openModal, closeModal}=useContext(ModalContext);
+
+    const logout = () => {
+        localStorage.clear();
+        window.location.reload();
+    }
+    return (
+        <div>
+            <div className="modal-title">로그아웃 할까요?</div>
+            <div className="modal-alternative-buttons">
+                <button className="modal-button" type="button" class="btn btn-primary btn-sm" onClick={() => (logout())}>네</button>
+                <button className="modal-button" type="button" class="btn btn-secondary btn-sm" onClick={() => (closeModal())}>아니요</button>
+            </div>
+        </div>
+    )
+
+       
+}
+
+export default LogoutForm;
