eb492b6b68244ab3f753e050c4838bd3a2f49435
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-17 10:26:39 +0000

    [FRONT, UPDATE] DormitoryChangeForm.js - 기숙사 변경 로직 구현

--- front/src/components/common/modalForms/myInfo/DormitoryChangeForm.js
+++ front/src/components/common/modalForms/myInfo/DormitoryChangeForm.js
@@ -1,39 +1,71 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { dormNameToDormId } from "../../../home/HomeUtils";
+import axios from "axios";
+import ModalContext from "../../ModalContext";
 
-function DormitoryChangeForm() {
+function DormitoryChangeForm({userId}) {
     const [dormitory, setDormitory] = useState("");
     const [password, setPassword] = useState("");
+    const {isOpen, openModal, closeModal}=useContext(ModalContext);
 
-    const handleSubmit = (e) => {
+    const handleButton = (e) => {
         e.preventDefault();
-        // 기숙사 변경 로직을 여기에 추가하세요
-        console.log("Dormitory:", dormitory);
-        console.log("Password:", password);
+        if(dormitory=="" || password==""){
+            toast.warn("비워진 부분이 있어요, 모두 입력해주세요.");
+            return ;
+        }
+        if(dormNameToDormId[dormitory]==undefined){
+            toast.warn("기숙사 형식이 올바르지 않아요!");
+            return ;
+        }
+        changeDormitory();
     };
+
+    const changeDormitory = async () => {
+        console.log(11);
+        const path=`https://improved-space-tribble-vjvwrwx956jh69w4-8080.app.github.dev/api/v1/user/${userId}`;
+        const body={
+            confirmPassword:password,
+            dormId: dormNameToDormId[dormitory]
+        };
+        try{
+            const response=await axios.patch(path,body,{});
+            toast.success("변경 사항을 반영했어요! 새로고침해 확인하세요.");
+            closeModal();
+        }catch(error){
+            if(error.response.data==="ConfirmPasswordNotCorrect"){
+                toast.error("확인 비밀번호가 올바르지 않아요.");
+            }
+            
+        }
+    }
 
     return (
         <div>
             <div className="modal-title">기숙사 바꾸기</div>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <div className="modal-container">
-                    <div className="modal-container-title">바꿀 기숙사</div>
+                    <div className="modal-container-title">변경할 기숙사</div>
                     <input
+                        class="form-control form-control-sm"
                         type="text"
-                        placeholder="바꿀 기숙사"
+                        placeholder="오름1 ~ 오름3 혹은 푸름1 ~ 푸름4"
                         value={dormitory}
                         onChange={(e) => setDormitory(e.target.value)}
                     />
                 </div>
-                <div className="modal-container">
-                    <div className="modal-container-title">비밀번호 확인</div>
+                <div className="modal-container" style={{paddingBottom:'2vh'}}>
+                    <div className="modal-container-title">비밀번호</div>
                     <input
+                        class="form-control form-control-sm"
                         type="password"
-                        placeholder="비밀번호 확인"
+                        placeholder="비밀번호"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
-                <button type="submit">변경하기</button>
+                <button className="modal-button" type="button" class="btn btn-primary btn-sm" onClick={handleButton}>변경하기</button>
             </form>
         </div>
     );
