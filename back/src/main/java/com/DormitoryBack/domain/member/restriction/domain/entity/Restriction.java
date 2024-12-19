a568a310871ee400015ac2918913ee9c3a9d6d95
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-19 10:08:42 +0000

    [BACK, COMMENT, ALTER] Restriction.java - 같은 도메인의 enum의 파일 이름 변경 사항 반영, 주석 추가

--- back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/entity/Restriction.java
+++ back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/entity/Restriction.java
@@ -1,8 +1,9 @@
 package com.DormitoryBack.domain.member.restriction.domain.entity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import com.DormitoryBack.domain.member.restriction.domain.enums.Functions;
+import com.DormitoryBack.domain.member.restriction.domain.enums.Function;
 import com.DormitoryBack.domain.notification.enums.EntityType;
 
 import jakarta.persistence.Column;
@@ -13,9 +14,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
-
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 @Entity
 @Table(name="restriction")
@@ -31,21 +33,24 @@ public class Restriction {
     @Column(nullable = false)
     private String reason;
 
+    @Column(nullable = false, name = "triggered_time")
+    private LocalDateTime triggeredTime;
+
     @Column(nullable = false)
     private Long suspensionDays;
 
     @Column(nullable = false)
     private int suspendedFunctions;
 
-    public boolean isSuspended(Functions function){
+    public boolean isSuspended(Function function){
         return (suspendedFunctions & function.getValue()) != 0;
     }
 
-    public void suspendFunction(Functions functions){
+    public void suspendFunction(Function functions){
         suspendedFunctions |= functions.getValue();
     }
 
-    public void releaseFunction(Functions functions){
+    public void releaseFunction(Function functions){
         suspendedFunctions &= ~functions.getValue();
     }
 
@@ -62,6 +67,11 @@ public class Restriction {
     if (!restriction.isRestricted(Functions.GROUP)) {
         System.out.println("GROUP 기능은 제한되지 않았습니다.");
     }
+
+
+    Restriction restriction = new Restriction();
+    restriction.suspendFunction(Function.LOGIN);
+    restriction.suspendFunction(Function.ARTICLE);
 
     */
     
