cf5711fdfca97ea2a9a437dcfaf87dd8e98492b9
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-19 12:23:29 +0000

    [BACK, UPDATE] RestrictionAPI.java - 제제 추가, 제제 조회 요청에 대한 api 메서드 추가

--- back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/api/RestrictionAPI.java
+++ back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/api/RestrictionAPI.java
@@ -1,5 +1,53 @@
 package com.DormitoryBack.domain.member.restriction.domain.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.DormitoryBack.domain.member.restriction.domain.dto.RestrictionRequestDTO;
+import com.DormitoryBack.domain.member.restriction.domain.dto.RestrictionResponseDTO;
+import com.DormitoryBack.domain.member.restriction.domain.dto.RestrictionResponseDTOList;
+import com.DormitoryBack.domain.member.restriction.domain.service.RestrictionService;
+import lombok.extern.slf4j.Slf4j;
+
+@CrossOrigin(origins="https://improved-space-tribble-vjvwrwx956jh69w4-3000.app.github.dev")
+@RestController
+@RequestMapping("/api/v1/restriction")
+@Slf4j
 public class RestrictionAPI {
+
+    private final RestrictionService restrictionService;
+
+    @Autowired
+    public RestrictionAPI(RestrictionService restrictionService){
+        this.restrictionService=restrictionService;
+    }
+
+    @GetMapping("/test")
+    public String restrictionTest(){
+        return "test in restriction domain";
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity myRestrictions(@RequestHeader("Authorization") String token){
+        RestrictionResponseDTOList dtoList=restrictionService.getMyRestrictions(token);
+        return ResponseEntity.status(HttpStatus.OK).body(dtoList);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity makeRestriction(@RequestBody RestrictionRequestDTO DTO){
+        RestrictionResponseDTO responseDTO=restrictionService.restrict(DTO);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+
+
+
     
 }
