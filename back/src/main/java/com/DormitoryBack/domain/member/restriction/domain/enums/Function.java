66c86dacd6cb388986e229e7bd12b3585137ddd8
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-19 10:07:29 +0000

    [BACK, FILE_NAME_ALTER, UPDATE] Function.java - Functions.java에서 이름 변경, fromString 메서드 추가

--- back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/enums/Function.java
+++ back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/enums/Function.java
@@ -1,18 +1,23 @@
 package com.DormitoryBack.domain.member.restriction.domain.enums;
 
-public enum Functions {
-    USER(1),
+public enum Function {
+    LOGIN(1),
     ARTICLE(2),
     COMMENT(4),
     GROUP(8);
 
     private final int value;
 
-    Functions(int value){
+    Function(int value){
         this.value=value;
     }
 
     public int getValue(){
         return value;
     }
+
+    public static Function fromString(String function) {
+        return Function.valueOf(function.toUpperCase());
+    }
 }
+
