7e923dacfe533fd7bbbba1438a3b634d4b851595
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-22 04:59:07 +0000

    [BACK, CREATE] DormitoryBackApplicationTests.java - 이전에 삭제했던 테스트 코드 추가

---
+++ back/src/test/java/com/DormitoryBack/DormitoryBackApplicationTests.java
@@ -0,0 +1,14 @@
+package com.DormitoryBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DormitoryBackApplicationTests {
+
+    @Test
+    void contextLoads(){
+
+    }
+    
+}
