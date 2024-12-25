0dd82242cf46b64dc9ced8bc668596fdd5bfdf24
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-25 10:12:19 +0000

    [BACK, UPDATE] RestrictionRepository.java - 쿼리 메서드 추가

--- back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/repository/RestrictionRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/repository/RestrictionRepository.java
@@ -1,8 +1,13 @@
 package com.DormitoryBack.domain.member.restriction.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.DormitoryBack.domain.member.restriction.domain.entity.Restriction;
 
 public interface RestrictionRepository extends JpaRepository<Restriction,Long>{
+
+    List<Restriction> findAllByUserId(Long userId);
+    
     
 }
