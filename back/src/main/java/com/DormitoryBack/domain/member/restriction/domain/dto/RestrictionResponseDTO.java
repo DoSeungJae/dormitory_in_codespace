2abacf23127ec1c906eb082222dd0d365c7f7c3b
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-25 10:09:31 +0000

    [BACK, CREATE] RestrictionResponseDTO.java - Restriction 도메인에서 response에 body로 담을 데이터 타입

--- back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/dto/RestrictionResponseDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/dto/RestrictionResponseDTO.java
@@ -1,5 +1,22 @@
 package com.DormitoryBack.domain.member.restriction.domain.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class RestrictionResponseDTO {
     
+    private Long userId;
+
+    private LocalDateTime expireTime;
+
+    private Boolean isExpired;
+
+    private String reason;
+
+    private List<String> suspendedFunctions;
+
 }
