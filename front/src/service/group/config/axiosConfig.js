1cdb732c79ecb37899f08d815c319d1f1cf7b439
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-28 05:27:07 +0000

    [FRONT, ALTER] axiosConfig.js - 환경변수 사용

--- front/src/service/group/config/axiosConfig.js
+++ front/src/service/group/config/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'https://improved-space-tribble-vjvwrwx956jh69w4-8080.app.github.dev';
+const API_BASE_URL = `${process.env.REACT_APP_HTTP_BASE_URL}`;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
