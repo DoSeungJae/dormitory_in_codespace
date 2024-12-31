454ef4c637272d3c2fc4f9bfd87eb97732ced9e6
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-12-31 12:20:44 +0000

    [BACK, CREATE] GoogleRequestDTO.java - OAuth2를 통해 Google 로그인을 하기 위해 필요한 dto

---
+++ back/src/main/java/com/DormitoryBack/domain/auth/domain/dto/GoogleRequestDTO.java
@@ -0,0 +1,11 @@
+package com.DormitoryBack.domain.auth.domain.dto;
+
+import java.util.Map;
+
+import lombok.Getter;
+
+@Getter
+public class GoogleRequestDTO {
+    private Map<String, String> body;
+    
+}
