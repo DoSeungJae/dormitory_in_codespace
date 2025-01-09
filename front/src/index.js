4b809b6469f5d3e2f5b94b88fafdb5b9514c0938
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-01-09 09:48:09 +0000

    [FRONT] index.js - 임시

--- front/src/index.js
+++ front/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import GoogleLoginButton from './components/logIn/google/GoogleLoginButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,8 +14,10 @@ root.render(
   </React.StrictMode>
    strictMode는 기본적으로 (모든) 컴포넌트들이 중복(2번)으로 렌더링되므로 사용하지 않음 
   */ 
-  <GoogleOAuthProvider clientId='272109241394-9qcdekfl9dhmh8ke5dtjod0hmlaf5t73.apps.googleusercontent.com'>
-    <App/>
+  <GoogleOAuthProvider clientId='272109241394-9qcdekfl9dhmh8ke5dtjod0hmlaf5t73.apps.googleusercontent.com' >
+    
+    <GoogleLoginButton/>
+    {/*<App/>*/}
   </GoogleOAuthProvider>
 
 
