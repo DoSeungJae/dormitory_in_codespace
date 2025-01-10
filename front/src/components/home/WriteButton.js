29f670ca23744f3060701a81acdd7c2b989312c1
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-01-10 11:29:54 +0000

    [FRONT, UPDATE] WriteButton,js - 글쓰기 버튼 변경, 글쓰기 아이콘 추가

--- front/src/components/home/WriteButton.js
+++ front/src/components/home/WriteButton.js
@@ -3,13 +3,14 @@ import { Button } from "@mui/material";
 import { goToPostingPage } from './HomeUtils';
 import { useContext } from 'react';
 import HomeSelectContext from './HomeSelectContext';
-
+import WriteIcon from '@mui/icons-material/Create';;
 const WriteButton = () => {
     const {selectComponentIndex,setSelectComponentIndex}=useContext(HomeSelectContext);
     return (
     <div>        
         <button  className='write-button' onClick={() => goToPostingPage(selectComponentIndex,setSelectComponentIndex)}>
-          글쓰기
+          <div className='write-button-icon'><WriteIcon fontSize='medium'/></div>
+          <div className='write-button-text'>글 쓰기</div>
         </button>
     </div>
 
