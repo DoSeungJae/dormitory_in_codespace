b96ba4dadd25fe0fc66dd7e89c18507c34af4b03
박노현 <38059780+Superstellar@users.noreply.github.com>
2025-01-11 08:04:02 +0000

    Added modal component showing restrictions from Mypage

--- front/src/modules/common/timeModule.js
+++ front/src/modules/common/timeModule.js
@@ -1,7 +1,6 @@
 import dayjs from 'dayjs';
 
 export const getRelativeTime = (absTime) => {
-  
     if(Array.isArray(absTime)===true){
         absTime=arrayToLocalDateTimeString(absTime);
     }
@@ -11,19 +10,20 @@ export const getRelativeTime = (absTime) => {
     const diffInMinutes = now.diff(created, 'minute');
     const diffInHours = now.diff(created, 'hour');
     const diffInDays = now.diff(created, 'day');
-  
+    
+    const beforeAfter = now>created?"전":"후";
 
-    if (diffInSeconds < 60) {
-      return `${diffInSeconds}초 전`;
-    } else if (diffInMinutes < 60) {
-      return `${diffInMinutes}분 전`;
-    } else if (diffInHours < 24) {
-      return `${diffInHours}시간 전`;
-    } else if (diffInDays === 1) {
-      return '하루 전';
-    } else if (diffInDays === 2) {
-      return '이틀 전';
-    } else if (diffInDays < 365) {
+    if (diffInSeconds < 60 && diffInSeconds > -60) {
+      return `${diffInSeconds}초 ${beforeAfter}`;
+    } else if (diffInMinutes < 60 && diffInMinutes > -60) {
+      return `${diffInMinutes}분 ${beforeAfter}`;
+    } else if (diffInHours < 24 && diffInHours > -24) {
+      return `${diffInHours}시간 ${beforeAfter}`;
+    } else if (diffInDays === 1 || diffInDays === -1) {
+      return `하루 ${beforeAfter}`;
+    } else if (diffInDays === 2 || diffInDays === -2) {
+      return `이틀 ${beforeAfter}`;
+    } else if (created.get("year") === now.get("year")) {
       return created.format('MM월 DD일');
     } else {
       return created.format('YYYY년 MM월 DD일');
