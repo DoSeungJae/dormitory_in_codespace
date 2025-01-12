abf57719197ce47916122b369feebbb25a7ea3e9
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-01-12 06:47:13 +0000

    [FRONT, ALTER] Toast.js - 불필요한(작동하지 않는) 코드 라인 제거

--- front/src/modules/common/Toast.js
+++ front/src/modules/common/Toast.js
@@ -7,7 +7,6 @@ export const StyledToastContainer=styled(ToastContainer)`
     padding-left:2vw;
     padding-right:2vw;
     margin-top:3vh;
-    border : 1px solid #FFA07A
   &&&.Toastify__toast-container {
   }
   .Toastify__toast {
