46fc346fd33ee3457bb99e62a256841fbd1d91a7
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-01-23 11:47:06 +0000

    [BACK, ALTER] UserService.java - map을 먼저 삭제하는 방식에서 user를 먼저 삭제하는 방식으로 변경 (user 삭제 시 예외가 발생하는 경우가 있기 때문)

--- back/src/main/java/com/DormitoryBack/domain/member/domain/service/UserService.java
+++ back/src/main/java/com/DormitoryBack/domain/member/domain/service/UserService.java
@@ -249,9 +249,9 @@ public class UserService {
         else if(groupService.isBelongToAnywhere(usrId)){
             throw new RuntimeException("CannotDeleteUserWhileGrouping");
         }
+        
+        userRepository.delete(target);
         encryptedEmailService.deleteEmailMap(target.getEncryptedEmail());
         encryptedPhoneNumService.deleteNumberMap(target.getEncryptedPhoneNum());
-
-        userRepository.delete(target);
     }
 }
