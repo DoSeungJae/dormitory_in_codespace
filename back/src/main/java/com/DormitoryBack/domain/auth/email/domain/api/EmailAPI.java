151a0fcca44b03901b06e780ba722eccffb134e1
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-01-29 06:50:21 +0000

    [BACK, UPDATE] EmailAPI.java, EmailService.java - api 응답이 비동기적으로 처리되도록 코드 추가

--- back/src/main/java/com/DormitoryBack/domain/auth/email/domain/api/EmailAPI.java
+++ back/src/main/java/com/DormitoryBack/domain/auth/email/domain/api/EmailAPI.java
@@ -1,6 +1,8 @@
 package com.DormitoryBack.domain.auth.email.domain.api;
 
-
+import java.util.concurrent.CompletableFuture;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.DormitoryBack.domain.auth.email.domain.dto.EmailRequestDTO;
+import com.DormitoryBack.domain.auth.email.domain.dto.EmailResponseDTO;
 import com.DormitoryBack.domain.auth.email.domain.service.EmailService;
-
 import lombok.extern.slf4j.Slf4j;
 
 @CrossOrigin(origins = {"https://improved-space-tribble-vjvwrwx956jh69w4-3000.app.github.dev", "https://turbo-chainsaw-rpvvwx9pp5c5p55-3000.app.github.dev"})
@@ -25,11 +26,10 @@ public class EmailAPI {
     private EmailService emailService;
 
     @PostMapping("/verifyCode")
-    public ResponseEntity mailConfirm(@RequestBody EmailRequestDTO dto) throws Exception{
-        String code=emailService.sendVerifyMail(dto);
-        return ResponseEntity.status(HttpStatus.OK).body(code);
+    @Produces(MediaType.APPLICATION_JSON)
+    public CompletableFuture<ResponseEntity<EmailResponseDTO>> verifyCode(@RequestBody EmailRequestDTO dto) throws Exception{
+        return emailService.sendVerifyMail(dto)
+            .thenApply(response -> ResponseEntity.status(HttpStatus.ACCEPTED).body(response))
+            .exceptionally(ex -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null));
     }
-
-    
-    
 }
--- back/src/main/java/com/DormitoryBack/domain/auth/email/domain/service/EmailService.java
+++ back/src/main/java/com/DormitoryBack/domain/auth/email/domain/service/EmailService.java
@@ -1,8 +1,13 @@
 package com.DormitoryBack.domain.auth.email.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.DormitoryBack.domain.auth.email.domain.dto.EmailRequestDTO;
+import com.DormitoryBack.domain.auth.email.domain.dto.EmailResponseDTO;
+import com.DormitoryBack.domain.auth.email.domain.enums.CodeStateType;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -11,38 +16,100 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class EmailService {
 
-    public String sendVerifyMail(EmailRequestDTO dto) throws MessagingException {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.naver.com");
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+    @Value("${spring.mail.host}")
+    private String host;
 
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                return new javax.mail.PasswordAuthentication("이메일", "비밀번호");
+    @Value("${spring.mail.port}")
+    private String port;
+
+    @Value("${spring.mail.email}")
+    private String email;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String starttls;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+    public CompletableFuture<EmailResponseDTO> sendVerifyMail(EmailRequestDTO request) {
+        EmailResponseDTO response = EmailResponseDTO.builder()
+            .stateType(CodeStateType.SENDING)
+            .build();
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                Properties props = new Properties();
+                props.put("mail.smtp.host", host);
+                props.put("mail.smtp.port", port);
+                props.put("mail.smtp.auth", auth);
+                props.put("mail.smtp.starttls.enable", starttls);
+
+                Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+                    protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                        return new javax.mail.PasswordAuthentication(email, password);
+                    }
+                });
+
+                String key = createKey();
+                String subject = "DeliveryBox 인증 코드";
+                String msg = "";
+                String to = request.getEmail();
+
+                msg += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">이메일 주소 확인</h1>";
+                msg += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">아래 확인 코드를 회원가입 화면에서 입력해주세요.</p>";
+                msg += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
+                msg += key;
+                msg += "</td></tr></tbody></table></div>";
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(email));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+                message.setSubject(subject);
+                message.setContent(msg, "text/html; charset=utf-8");
+
+                Transport.send(message);
+
+                // Transport.send()가 끝난 후 실행할 작업
+                postSendAction();
+
+
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
             }
-        });
-        String key="03931192";
-        String subject="DeliveryBox 인증 코드";
-        String msg="";
-        String to=dto.getEmail();
-        msg += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">이메일 주소 확인</h1>";
-        msg += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">아래 확인 코드를 회원가입 화면에서 입력해주세요.</p>";
-        msg += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
-        msg += key;
-        msg += "</td></tr></tbody></table></div>";
-        Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("dsj5508@naver.com"));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-        message.setSubject(subject);
-        message.setContent(msg, "text/html; charset=utf-8");
+        }, executorService);
 
-        Transport.send(message);
-        return key;
+        return CompletableFuture.completedFuture(response);
+    }
+
+    private void postSendAction() {
+        // Transport.send()가 끝난 후 실행할 작업을 여기에 작성합니다.
+        System.out.println("Email sent successfully!");
+    }
+
+    public static String createKey() {
+        StringBuffer key = new StringBuffer();
+        Random random = new Random();
+
+        for (int i = 0; i < 6; i++) {
+            int r = random.nextInt(10);
+            key.append(r);
+        }
+
+        return key.toString();
     }
 }
