d56bda9458f3b498ae8c8713c769e9cae9745f1a
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-02-02 07:36:50 +0000

    [BACK, REFACTOR] NewArticleDTO.java -  테스트를 위한 어노테이션 추가

--- back/src/main/java/com/DormitoryBack/domain/article/domain/dto/NewArticleDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/dto/NewArticleDTO.java
@@ -1,8 +1,12 @@
 package com.DormitoryBack.domain.article.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
+@AllArgsConstructor
 public class NewArticleDTO {
 
     private String title;
