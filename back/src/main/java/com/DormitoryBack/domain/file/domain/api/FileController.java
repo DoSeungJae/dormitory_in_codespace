62ea0927ffa0ab4e138bbdcf3bf27687b46f42c9
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-02-09 08:13:05 +0000

    [BACK, UPDATE] FileController.java, FileService.java - presigned url을 생성하여 응답으로 보내는 기능 구현

--- back/src/main/java/com/DormitoryBack/domain/file/domain/api/FileController.java
+++ back/src/main/java/com/DormitoryBack/domain/file/domain/api/FileController.java
@@ -3,8 +3,11 @@ package com.DormitoryBack.domain.file.domain.api;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +27,12 @@ public class FileController {
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException{
         String url=fileService.uploadFile(file);
         return ResponseEntity.status(HttpStatus.OK).body(url);        
+    }
+
+    @GetMapping("/getURL/{fileName}")
+    public ResponseEntity<String> getFile(@PathVariable("fileName") String fileName) throws IOException{
+        String presignedURL=fileService.generatePresignedURL(fileName);
+        return ResponseEntity.status(HttpStatus.OK).body(presignedURL);
     }
 
 }
--- back/src/main/java/com/DormitoryBack/domain/file/domain/service/FileService.java
+++ back/src/main/java/com/DormitoryBack/domain/file/domain/service/FileService.java
@@ -1,15 +1,27 @@
 package com.DormitoryBack.domain.file.domain.service;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.HttpMethod;
 import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 import lombok.RequiredArgsConstructor;
@@ -28,8 +40,23 @@ public class FileService {
     @Autowired
     private AmazonS3 s3Client;
 
-
     private String defaultUrl="https://s3.amazonaws.com";
+
+    public String generatePresignedURL(String fileName){
+        Date expiration=new Date();
+        long expTimeMillis=expiration.getTime();
+        expTimeMillis+=1000*60*60; //1시간
+        expiration.setTime(expTimeMillis);
+        
+        GeneratePresignedUrlRequest generatePresignedUrlRequest=
+            new GeneratePresignedUrlRequest(bucketName, fileName)
+                .withMethod(HttpMethod.GET)
+                .withExpiration(expiration);
+        
+        URL url=s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+        
+        return url.toString();
+    }
 
     public String uploadFile(MultipartFile file) throws IOException{
         String fileName=generateFileName(file);
