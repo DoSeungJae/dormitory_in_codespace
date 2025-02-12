d225d20c6ddee44ad93eceebf4be1947c74edde0
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-02-12 13:30:46 +0000

    [BACK, UPDATE, COMMENT] [UPDATE] FileController.java - 사용자 이미지 처리에 관한 메서드 추가 [COMMENT] FileController.java - 기존에 기본적인 파일 업로드 메서드 주석 처리, 단 샘플 용도로 필요하기 때문에 삭제하지는 않음

--- back/src/main/java/com/DormitoryBack/domain/file/domain/api/FileController.java
+++ back/src/main/java/com/DormitoryBack/domain/file/domain/api/FileController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,11 +24,21 @@ public class FileController {
     @Autowired
     private FileService fileService;
 
+    /* 기본적인 파일 업로드 메서드, 삭제 하는게 맞지만 샘플 용도로 놔둠.
     @PostMapping("")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException{
         String url=fileService.uploadFile(file);
         return ResponseEntity.status(HttpStatus.OK).body(url);        
     }
+    */
+
+    /*
+    @DeleteMapping("/{fileName}")
+    public ResponseEntity<Void> deleteFile(@PathVariable("fileName") String fileName){
+        fileService.deleteFile(fileName);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+    */
 
     @GetMapping("/url/{fileName}")
     public ResponseEntity<String> getFileURL(@PathVariable("fileName") String fileName) throws IOException{
@@ -36,9 +46,15 @@ public class FileController {
         return ResponseEntity.status(HttpStatus.OK).body(presignedURL);
     }
 
-    @DeleteMapping("/{fileName}")
-    public ResponseEntity<Void> deleteFile(@PathVariable("fileName") String fileName){
-        fileService.deleteFile(fileName);
+    @PostMapping("/userImage")
+    public ResponseEntity<Void> setUserImage(@RequestParam("file") MultipartFile file, @RequestHeader("Authorization") String token){
+        fileService.setUserProfileImage(file, token);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }       
+
+    @DeleteMapping("/userImage")
+    public ResponseEntity<Void> deleteUserImage(@RequestHeader("Authorization") String token){
+        fileService.deleteUserProfileImage(token);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 
