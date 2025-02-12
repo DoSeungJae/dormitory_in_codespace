6e1de3719b837ebb1f5bea5ebeb96f1065c8af0d
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-02-12 14:34:58 +0000

    [BACK, DELETE] SecurityConfig.java - 필요없는 import문 삭제

--- back/src/main/java/com/DormitoryBack/config/SecurityConfig.java
+++ back/src/main/java/com/DormitoryBack/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.DormitoryBack.config;
-import com.DormitoryBack.domain.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
