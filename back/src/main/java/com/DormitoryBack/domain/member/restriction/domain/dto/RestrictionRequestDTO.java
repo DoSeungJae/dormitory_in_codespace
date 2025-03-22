19bb04df28bc557141ef628ef5b96d00324a0586
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-03-22 11:38:12 +0000

    [BACK, UPDATE] RestrictionRequestDTO.java - 각 데이터 필드에 @NotNull 어노테이션 추가

--- back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/dto/RestrictionRequestDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/member/restriction/domain/dto/RestrictionRequestDTO.java
@@ -1,5 +1,7 @@
 package com.DormitoryBack.domain.member.restriction.domain.dto;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,9 +9,16 @@ import lombok.Getter;
 @Builder
 public class RestrictionRequestDTO {
     
+    @NotNull
     private String accessKey;
+
+    @NotNull
     private Long userId;
+
+    @NotNull
     private String reason;
+
+    @NotNull
     private Long durationDays;
 
     public void setAccessKey(String accessKey){
