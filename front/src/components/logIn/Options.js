df50344546e8f2585786596252e5a1e771fdeba3
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-03-24 13:55:50 +0000

    [BACK, UPDATE] Options.js - modal 추가

--- front/src/components/logIn/Options.js
+++ front/src/components/logIn/Options.js
@@ -2,18 +2,28 @@ import {React,useContext} from 'react'
 import 'bootstrap/dist/css/bootstrap.css'; 
 import 'bootstrap/dist/js/bootstrap.js'; 
 import HomeSelectContext from '../home/HomeSelectContext';
+import ModalContext from '../common/ModalContext';
+import Modal from '../common/Modal';
+
 
 const Options = () => {
     const {selectComponentIndex,setSelectComponentIndex}=useContext(HomeSelectContext);
-    const handleTextClick = () => {
-        setSelectComponentIndex(7);
+    const {isOpen, openModal, closeModal}=useContext(ModalContext);
+
+    const signIn = (modalContent) => {
+        openModal(modalContent);
     }
+
+    const initPW = (modalContent) => {
+        openModal(modalContent);
+    }
+    
     return (
         <div className="continer options-container">
+            <div><Modal/></div>
             <div className="options-row mt-1">
-                <div className="" onClick={handleTextClick}>회원가입</div>
-                <div className="">아이디 찾기</div>
-                <div className="">비밀번호 변경</div>
+                <div className="" onClick={()=>signIn(<div>회원가입</div>)}>회원가입</div>
+                <div className="" onClick={()=>initPW(<div>비번 초기화</div>)}>비밀번호 초기화</div>
             </div>
             <div className="separator mt-4">혹은</div>
         </div> 
