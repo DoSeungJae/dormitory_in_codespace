74d48344c968471f684c3dbe74dd82bd1691f0c6
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-03-31 04:48:18 +0000

    [BACK, ALTER] ChatManager.java - 불필요한 코드 삭제 및 userRepository 대신 userService를 사용하도록 변경, 추후 방향성을 위한 주석 추가

--- back/src/main/java/com/DormitoryBack/domain/group/chat/domain/module/ChatManager.java
+++ back/src/main/java/com/DormitoryBack/domain/group/chat/domain/module/ChatManager.java
@@ -1,17 +1,13 @@
 package com.DormitoryBack.domain.group.chat.domain.module;
 
-import com.DormitoryBack.domain.group.domain.entitiy.Group;
-import com.DormitoryBack.domain.member.domain.entity.User;
-import com.DormitoryBack.domain.member.domain.repository.UserRepository;
+import com.DormitoryBack.domain.member.domain.dto.UserResponseDTO;
+import com.DormitoryBack.domain.member.domain.service.UserServiceExternal;
 import com.corundumstudio.socketio.SocketIOClient;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-
 import java.util.HashMap;
-import java.util.List;
 
 @Slf4j
 @Component
@@ -19,13 +15,15 @@ import java.util.List;
 public class ChatManager {
 
     @Autowired
-    private UserRepository userRepository;
-    private HashMap<Long, SocketIOClient> socketClientsOfUsers=new HashMap<>();
-    public void mapUserIdToSocketClient(String username,SocketIOClient client) {
-        User user=userRepository.findByNickName(username);
+    private UserServiceExternal userService;
+
+    private HashMap<Long, SocketIOClient> socketClientsOfUsers=new HashMap<>(); //부적절한 방식임. 여러 대의 서버로 운용할 경우에 문제가 생김. db에서 관리해야함... 
+    
+    public void mapUserIdToSocketClient(String username, SocketIOClient client) {
+        UserResponseDTO user=userService.getUserByNickName(username);
         Long userId=user.getId();
         socketClientsOfUsers.put(userId,client);
-        //동시성 이슈 발생 가능성 고려해야함
+        //동시성 이슈 발생 가능성 고려해야함(아마 24.04?) <- 어떤 의미로 적어놓은건지 기억이 안난다....(25.03.30)
     }
     public SocketIOClient getSocketClientByUserId(Long userId){
         SocketIOClient client=socketClientsOfUsers.get(userId);
@@ -35,10 +33,7 @@ public class ChatManager {
         return socketClientsOfUsers.get(userId);
     }
     public void removeSocketClientByUserId(String username){
-        User user=userRepository.findByNickName(username);
-        if(user==null){
-            return ;
-        }
+        UserResponseDTO user=userService.getUserByNickName(username);
         Long userId=user.getId();
         socketClientsOfUsers.remove(userId);
     }
