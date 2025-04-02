cf074a986607104b29c6c57b68f88a28d9231667
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-02 11:18:43 +0000

    [BACK, UPDATE] ArticleRepository.java - 차단된 글을 제외하고 글을 불러오는 메서드 추가

--- back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/article/domain/repository/ArticleRepository.java
@@ -2,12 +2,11 @@ package com.DormitoryBack.domain.article.domain.repository;
 
 import com.DormitoryBack.domain.article.domain.entity.Article;
 import com.DormitoryBack.domain.member.domain.entity.User;
-
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
 
 @EnableMongoRepositories 
 public interface ArticleRepository extends MongoRepository<Article,Long> {
@@ -17,5 +16,12 @@ public interface ArticleRepository extends MongoRepository<Article,Long> {
     Page<Article> findAllByUsrId(User user, Pageable pageable); //이름에 Id가 포함돼있긴 하나, 사실상 user를 조회하는 메서드
 
     Page<Article> findAllByUserId(Long user, Pageable pageable);
+    
+    
+    Page<Article> findByUserIdNotIn(List<Long> blockedIdList, Pageable pageable);
+
+    Page<Article> findByDormIdAndUserIdNotIn(Long dormId, List<Long> blockedIdList, Pageable pageable);
+
+    List<Article> findByIdAndUserIdNotIn(List<Long> idList, List<Long> blockedIdList);
 
 }
