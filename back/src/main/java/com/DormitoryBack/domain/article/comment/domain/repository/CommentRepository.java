1068986a68d8a31f6e4132ab7a84b8e04469da0b
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-03 11:57:04 +0000

    [BACK, COMMENT] CommentRepository.java - edge case 찾아냄

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/repository/CommentRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/repository/CommentRepository.java
@@ -30,6 +30,8 @@ public interface CommentRepository extends JpaRepository<Comment,Long> {
                     "(SELECT r.user_id FROM comment r WHERE r.id = c.root_comment_id) NOT IN (:blockedIdList))", nativeQuery = true)
     List<Comment> findByArticleIdExcludingBlockedComments(@Param("articleId") Long articleId, @Param("blockedIdList") List<Long> blockedIdList);
     //count 용도로 사용. 이 메서드를 통해 얻은 List<Comment>를 listStringify하면 serializer 관련 에러가 나타나기 때문에 실제 comment 필터링은 응용 계층에서 수행.
+    //이 메서드는 blockedIdList의 size가 0일 때 제대로 작동하지 않음
+    //왜냐하면 blockedIdList가 비어있다면 SQL에선 NULL로 처리되어 NOT IN NULL이 되는데, 이것은 항상 FALSE이기 때문. <<- 포스팅?
 
     
 }
