9d30ee11bc91cf0ae0f422585899b338681ded48
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-03 10:42:08 +0000

    [BACK, ALTER] CommentApi.java - 여러 줄로 되어있던 api 메서드 한 줄로 수정, 불필요 import문 삭제

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/api/CommentApi.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/api/CommentApi.java
@@ -3,10 +3,8 @@ package com.DormitoryBack.domain.article.comment.domain.api;
 import com.DormitoryBack.domain.article.comment.domain.dto.*;
 import com.DormitoryBack.domain.article.comment.domain.entity.Comment;
 import com.DormitoryBack.domain.article.comment.domain.service.CommentService;
-import com.DormitoryBack.domain.jwt.TokenProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,42 +15,33 @@ import java.util.List;
 @RequestMapping("api/v1/comment")
 @Slf4j
 public class CommentApi {
-    private final CommentService commentService;
-    private final TokenProvider tokenProvider;
-
+    
     @Autowired
-    public CommentApi(CommentService commentService,TokenProvider tokenProvider){
-        this.tokenProvider=tokenProvider;
-        this.commentService=commentService;
-    }
+    private CommentService commentService;
 
     @GetMapping("/test")
-    public ResponseEntity commentTest(){
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body("comment test");
+    public ResponseEntity<String> commentTest(){
+        return ResponseEntity.status(HttpStatus.OK).body("comment test");
     }
+    
     @GetMapping("/{commentId}")
-    public ResponseEntity Comment(@PathVariable("commentId") Long commentId){
+    public ResponseEntity<String> Comment(@PathVariable("commentId") Long commentId){
         Comment comment=commentService.getComment(commentId);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(comment.toJsonString());
+        return ResponseEntity.status(HttpStatus.OK).body(comment.toJsonString());
     }
+
     @GetMapping("")
-    public ResponseEntity allComments() {
+    public ResponseEntity<List<String>> allComments() {
         List<Comment> comments = commentService.getAllComments();
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(commentService.listStringify(comments));
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.listStringify(comments));
     }
+
     @GetMapping("/user/{userId}")
-    public ResponseEntity userComments(@PathVariable("userId") Long userId){
+    public ResponseEntity<List<String>> userComments(@PathVariable("userId") Long userId){
         List<Comment> comments=commentService.getUserComments(userId);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(commentService.listStringify(comments));
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.listStringify(comments));
     }
+
     //토큰 인증이 필요하지 않을까?
     /*
     @GetMapping("/article/{articleId}")
@@ -65,51 +54,42 @@ public class CommentApi {
     */
     
     @GetMapping("article/{articleId}")
-    public ResponseEntity articleCommentsPerPage(
-            @PathVariable("articleId") Long articleId){
-        CommentResponseDTO responseDTO=commentService
-                .getArticleComments(articleId);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(responseDTO);
+    public ResponseEntity<CommentResponseDTO> articleCommentsPerPage(@PathVariable("articleId") Long articleId, @RequestHeader("Authorization") String token){
+        CommentResponseDTO responseDTO=commentService.getArticleComments(articleId, token);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
     }
 
     @GetMapping("/numComments")
-    public ResponseEntity numberOfComments(@RequestParam(name="articleId", defaultValue = "-1") Long articleId){
-        Long number=commentService.getNumberOfComments(articleId);
+    public ResponseEntity<Long> numberOfComments(@RequestParam(name="articleId", defaultValue = "-1") Long articleId, @RequestHeader("Authorization") String token){
+        Long number=commentService.getNumberOfComments(articleId,token);
         return ResponseEntity.status(HttpStatus.OK).body(number);
     }
     
     @PostMapping("/new")
-    public ResponseEntity newComment(@RequestBody CommentDTO dto, @RequestHeader("Authorization") String token){
+    public ResponseEntity<String> newComment(@RequestBody CommentDTO dto, @RequestHeader("Authorization") String token){
         Comment comment=commentService.newComment(dto,token);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(comment.toJsonString());
+        return ResponseEntity.status(HttpStatus.OK).body(comment.toJsonString());
     }
+
     @PostMapping("/newReply")
-    public ResponseEntity newReply(@RequestBody CommentReplyDTO dto, @RequestHeader("Authorization") String token){
+    public ResponseEntity<CommentReplyResponseDTO> newReply(@RequestBody CommentReplyDTO dto, @RequestHeader("Authorization") String token){
         CommentReplyResponseDTO responseDTO=commentService.newReply(dto,token);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(responseDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
         //StrUtil을 사용하면 time이 Array 타입으로 반환됨,
         //dto 그대로 반환시 time은 string 타입으로 반환됨.
 
     }
+
     @PatchMapping("/{commentId}")
-    public ResponseEntity updateComment(@RequestBody CommentUpdateDTO dto,@PathVariable("commentId") Long commentId, @RequestHeader("Authorization") String token){
+    public ResponseEntity<String> updateComment(@RequestBody CommentUpdateDTO dto,@PathVariable("commentId") Long commentId, @RequestHeader("Authorization") String token){
         Comment updatedComment=commentService.updateComment(dto,commentId,token);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(updatedComment.toJsonString());
+        return ResponseEntity.status(HttpStatus.OK).body(updatedComment.toJsonString());
     }
+
     @DeleteMapping("/{commentId}")
-    public ResponseEntity deleteComment(@PathVariable("commentId") Long commentId, @RequestHeader("Authorization") String token){
+    public ResponseEntity<String> deleteComment(@PathVariable("commentId") Long commentId, @RequestHeader("Authorization") String token){
         commentService.deleteComment(commentId,token);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body("CommentDeleted");
+        return ResponseEntity.status(HttpStatus.OK).body("CommentDeleted");
     }
 
 }
