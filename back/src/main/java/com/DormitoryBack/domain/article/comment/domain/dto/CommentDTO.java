5581e777108bfd34d21badf1d27d0c46a932df9d
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-03 10:41:26 +0000

    [BACK, DELETE] CommentDTO.java - 불필요 import문 삭제

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/dto/CommentDTO.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/dto/CommentDTO.java
@@ -3,9 +3,7 @@ package com.DormitoryBack.domain.article.comment.domain.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
