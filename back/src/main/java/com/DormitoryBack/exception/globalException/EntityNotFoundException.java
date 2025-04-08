e94262b70e1234818844589962ea688fe5163f49
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-08 13:40:21 +0000

    [BACK, ALTER] EntityNotFoundException.java - 파일 이름 변경됨 (from "Entitiy"~.java)

--- back/src/main/java/com/DormitoryBack/exception/globalException/EntityNotFoundException.java
+++ back/src/main/java/com/DormitoryBack/exception/globalException/EntityNotFoundException.java
@@ -2,19 +2,25 @@ package com.DormitoryBack.exception.globalException;
 
 import com.DormitoryBack.exception.ErrorInfo;
 
-public class EntitiyNotFoundException extends RuntimeException {
+public class EntityNotFoundException extends RuntimeException {
 
     private final ErrorInfo errorInfo;
 
-    public EntitiyNotFoundException(ErrorInfo errorInfo){
+    public EntityNotFoundException(ErrorInfo errorInfo){
         super(errorInfo.getErrorMessage());
         this.errorInfo=errorInfo;
     }
 
-    public EntitiyNotFoundException(ErrorInfo errorInfo, Throwable cause){
+    public EntityNotFoundException(ErrorInfo errorInfo, Throwable cause){
         super(errorInfo.getErrorMessage(), cause); // 메시지와 원인 설정
         this.errorInfo = errorInfo; // ErrorInfo 필드 초기화
     }
+
+    /*
+    public ErrorInfo getErrorInfo(){
+        return errorInfo;
+    }
+    */
 
     public ErrorInfo getErrorInfo(){
         return errorInfo;
