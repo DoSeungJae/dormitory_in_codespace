bb16b127eb644d3ba9ccbf8244d139afba69849b
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-08 13:52:35 +0000

    [BACK, UPDATE] ExceptionManager.java - 처리할 수 있는 예외 클래스 추가

--- back/src/main/java/com/DormitoryBack/exception/ExceptionManager.java
+++ back/src/main/java/com/DormitoryBack/exception/ExceptionManager.java
@@ -1,13 +1,13 @@
 package com.DormitoryBack.exception;
 
 import io.jsonwebtoken.JwtException;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.DormitoryBack.domain.auth.adminKey.exception.AccessDeniedException;
+import com.DormitoryBack.exception.globalException.EntityNotFoundException;
 
 @RestControllerAdvice
 public class ExceptionManager {
@@ -31,6 +31,11 @@ public class ExceptionManager {
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorInfo> accessDeniedExceptionHandler(AccessDeniedException e){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getErrorInfo());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorInfo> entityNotFoundExceptionHandler(EntityNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getErrorInfo());
     }
 
 
