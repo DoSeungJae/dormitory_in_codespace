6e8c4e0b6d14c38dab65d402fe5da86baa24684f
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-09 09:52:24 +0000

    [BACK, CREATE] CommentServiceExternal.java - orphan comment 처리를 위한 로직 구현

---
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/service/CommentServiceExternal.java
@@ -0,0 +1,47 @@
+package com.DormitoryBack.domain.article.comment.domain.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.DormitoryBack.domain.article.comment.domain.entity.Comment;
+import com.DormitoryBack.domain.article.comment.domain.entity.OrphanComment;
+import com.DormitoryBack.domain.article.comment.domain.repository.CommentRepository;
+import com.DormitoryBack.domain.article.comment.domain.repository.OrphanCommentRepository;
+import com.DormitoryBack.domain.member.domain.entity.DeletedUser;
+import com.DormitoryBack.domain.member.domain.entity.User;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class CommentServiceExternal {
+    
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private OrphanCommentRepository orphanCommentRepository;
+
+    
+    public List<Comment> getUserComments(User user){
+        List<Comment> userComments=commentRepository.findAllByUser(user); 
+        return userComments;
+    }
+
+    public void makeAllUserCommentsOrphans(DeletedUser deletedUser, User user, List<Comment> userComments){
+        userComments.stream()
+            .forEach(comment -> {
+                Long commentId=comment.getId();
+                OrphanComment orphanComment=OrphanComment.builder()
+                    .id(commentId)
+                    .comment(comment)
+                    .deletedUser(deletedUser) 
+                    .build();
+                
+                orphanCommentRepository.save(orphanComment);
+            });
+
+    }
+
+}
