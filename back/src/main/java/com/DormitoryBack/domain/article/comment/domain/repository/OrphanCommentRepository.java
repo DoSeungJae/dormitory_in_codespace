1fd3b04db33d761ef4ada2bc9de2a50121fff3ab
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-09 12:12:31 +0000

    [BACK, UPDATE] OrphanCommentRepository.java - commentService의 articlePreview 구현에 필요한 메서드 추가

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/repository/OrphanCommentRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/repository/OrphanCommentRepository.java
@@ -1,9 +1,14 @@
 package com.DormitoryBack.domain.article.comment.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.DormitoryBack.domain.article.comment.domain.entity.OrphanComment;
+import com.DormitoryBack.domain.article.domain.entity.Article;
 
 public interface OrphanCommentRepository extends JpaRepository<OrphanComment,Long> {
+
+    int countByArticle(Article article);
     
 }
