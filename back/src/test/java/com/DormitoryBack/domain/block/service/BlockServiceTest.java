f080ce08bf88f517986bcf8c53d0883df4674d46
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-15 14:19:05 +0000

    [BACK, TEST, UPDATE] BlockServiceTest.java

--- back/src/test/java/com/DormitoryBack/domain/block/service/BlockServiceTest.java
+++ back/src/test/java/com/DormitoryBack/domain/block/service/BlockServiceTest.java
@@ -2,6 +2,7 @@ package com.DormitoryBack.domain.block.service;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -17,9 +18,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import com.DormitoryBack.domain.block.entity.Block;
+import com.DormitoryBack.domain.block.exception.UnblockableException;
 import com.DormitoryBack.domain.block.repository.BlockRepository;
 import com.DormitoryBack.domain.jwt.TokenProvider;
 import com.DormitoryBack.domain.member.domain.service.UserServiceExternal;
+import com.DormitoryBack.exception.ErrorType;
 import com.DormitoryBack.module.TimeOptimizer;
 
 @ExtendWith(MockitoExtension.class)
@@ -48,16 +51,23 @@ public class BlockServiceTest {
         String token="validToken";
         Long blockedUserId=1L;
         Long blockerId=2L;
+        Block block=Block.builder()
+            .id(1L)
+            .blockedUserId(blockedUserId)
+            .blockerId(blockerId)
+            .blockedTime(TimeOptimizer.now())
+            .build();
 
-        when(tokenProvider.validateToken(token)).thenReturn(true);
-        when(tokenProvider.getUserIdFromToken(token)).thenReturn(blockerId);
         when(userService.isUserExist(blockedUserId)).thenReturn(true);
+        assertDoesNotThrow(()->tokenProvider.validateTokenOrThrow(token));
+        when(tokenProvider.getUserIdFromToken(token)).thenReturn(blockerId);
+        assertDoesNotThrow(()->blockService.checkBlockableOrThrow(blockerId, blockedUserId));
         
-
         blockService.block(token, blockedUserId);
 
         verify(blockRepository,times(1)).save(any(Block.class));
-        assertDoesNotThrow(()->userService.checkDeletedUserExistsOrThrow(blockedUserId));
+        assertEquals(blockerId, block.getBlockerId());
+        assertEquals(blockedUserId, block.getBlockedUserId());
     }
 
     @Test
@@ -88,7 +98,7 @@ public class BlockServiceTest {
         List<Block> blockedList=new ArrayList<>(List.of(b1,b2,b3)); 
         List<Long> blockedIdListExpected=new ArrayList<>(List.of(2L,3L,4L));
 
-        when(tokenProvider.validateToken(token)).thenReturn(true);
+        assertDoesNotThrow(()->tokenProvider.validateTokenOrThrow(token));
         when(tokenProvider.getUserIdFromToken(token)).thenReturn(userId);
         when(blockRepository.findAllByBlockerId(userId)).thenReturn(blockedList);
 
@@ -99,5 +109,59 @@ public class BlockServiceTest {
         assertEquals(blockedIdListExpected.get(2), blockedIdList.get(2));
         assertEquals(blockedIdListExpected.size(), blockedIdList.size());
     }
+
+
+    @Test
+    public void testCheckBlockableOrThrow_Success(){
+        Long blockerId=1L;
+        Long blockedUserId=2L;
+
+        when(userService.isUserExist(blockedUserId)).thenReturn(true);
+        //when(userService.isDeletedUserExist(blockedUserId)).thenReturn(true);
+
+        assertDoesNotThrow(()->blockService.checkBlockableOrThrow(blockerId, blockedUserId));
+    }
+
+    @Test
+    public void testCheckBlockableOrThrow_Success2(){
+        Long blockerId=1L;
+        Long blockedUserId=2L;
+
+        when(userService.isUserExist(blockedUserId)).thenReturn(false);
+        when(userService.isDeletedUserExist(blockedUserId)).thenReturn(true);
+
+        assertDoesNotThrow(()->blockService.checkBlockableOrThrow(blockerId, blockedUserId));
+    }
+
+    @Test
+    public void testCheckBlockableOrThrow_UnblockableException_BlockSelf(){
+        Long blockerId=1L;
+        Long blockedUserId=1L;
+
+        UnblockableException exception=assertThrows(UnblockableException.class, ()->{
+            blockService.checkBlockableOrThrow(blockerId, blockedUserId);
+        });
+
+        assertEquals("자기 자신을 차단할 수 없습니다.", exception.getErrorInfo().getErrorMessage());
+        assertEquals(ErrorType.BlockSelf, exception.getErrorInfo().getErrorType());
+    }
+
+    @Test
+    public void testCheckBlockableOrThorw_UnblockableException_EntityNotFound(){
+        Long blockerId=1L;
+        Long blockedUserId=2L;
+
+        when(userService.isUserExist(blockedUserId)).thenReturn(false);
+        when(userService.isDeletedUserExist(blockedUserId)).thenReturn(false);
+
+        UnblockableException exception=assertThrows(UnblockableException.class, ()->{
+            blockService.checkBlockableOrThrow(blockerId, blockedUserId);
+        });
+
+        assertEquals("차단할 수 없는 사용자입니다.", exception.getErrorInfo().getErrorMessage());
+        assertEquals(ErrorType.EntityNotFound, exception.getErrorInfo().getErrorType());
+    }
+
+
 
 }
