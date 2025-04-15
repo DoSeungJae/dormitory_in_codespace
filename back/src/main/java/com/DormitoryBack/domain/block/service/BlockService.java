19605d0c3dca991fcae6d5cf0cc730abc892ae90
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-15 14:16:39 +0000

    [BACK, ALTER, UPDATE] BlockService.java - 예외처리 로직 분리, 토큰 검증 메서드 변경

--- back/src/main/java/com/DormitoryBack/domain/block/service/BlockService.java
+++ back/src/main/java/com/DormitoryBack/domain/block/service/BlockService.java
@@ -6,9 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.DormitoryBack.domain.block.entity.Block;
+import com.DormitoryBack.domain.block.exception.UnblockableException;
 import com.DormitoryBack.domain.block.repository.BlockRepository;
 import com.DormitoryBack.domain.jwt.TokenProvider;
 import com.DormitoryBack.domain.member.domain.service.UserServiceExternal;
+import com.DormitoryBack.exception.ErrorInfo;
+import com.DormitoryBack.exception.ErrorType;
 import com.DormitoryBack.module.TimeOptimizer;
 
 @Service
@@ -24,13 +27,9 @@ public class BlockService {
     private UserServiceExternal userService;
 
     public void block(String token, Long blockedUserId){
-        if(!tokenProvider.validateToken(token)){
-            throw new RuntimeException("InvalidToken");
-        }
+        tokenProvider.validateTokenOrThrow(token);
         Long blockerId=tokenProvider.getUserIdFromToken(token);
-        if(!userService.isUserExist(blockedUserId)){
-            throw new RuntimeException("UserNotFound"); //userService(External)에서 예외처리 하는 것이 더 좋을 듯...
-        }
+        checkBlockableOrThrow(blockerId, blockedUserId);
 
         Block block=Block.builder()
             .blockedUserId(blockedUserId)
@@ -41,10 +40,18 @@ public class BlockService {
         blockRepository.save(block);
     }
 
-    public List<Long> getBlockedIdList(String token){
-        if(!tokenProvider.validateToken(token)){
-            throw new RuntimeException("InvalidToken");
+    public void checkBlockableOrThrow(Long blockerId, Long blockedUserId){
+        if(blockerId==blockedUserId){
+            throw new UnblockableException(new ErrorInfo(ErrorType.BlockSelf, "자기 자신을 차단할 수 없습니다."));
         }
+        if(!userService.isUserExist(blockedUserId) && !userService.isDeletedUserExist(blockedUserId)){
+            throw new UnblockableException(new ErrorInfo(ErrorType.EntityNotFound, "차단할 수 없는 사용자입니다."));
+        }
+    }
+    
+
+    public List<Long> getBlockedIdList(String token){
+        tokenProvider.validateTokenOrThrow(token);
         Long userId=tokenProvider.getUserIdFromToken(token);
         List<Block> blockList=blockRepository.findAllByBlockerId(userId);
         List<Long> blockedIdList=new ArrayList<>();
@@ -54,6 +61,7 @@ public class BlockService {
         }
         return blockedIdList;
     }
+
 
 
 }
