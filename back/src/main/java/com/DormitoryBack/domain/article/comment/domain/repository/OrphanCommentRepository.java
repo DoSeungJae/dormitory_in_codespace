a2e686db93790fb7f018b8ec2da5a6de2fa51715
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-15 14:13:46 +0000

    [BACK, ALTER] OrphanCommentRepository.java - article -> articleId

--- back/src/main/java/com/DormitoryBack/domain/article/comment/domain/repository/OrphanCommentRepository.java
+++ back/src/main/java/com/DormitoryBack/domain/article/comment/domain/repository/OrphanCommentRepository.java
@@ -1,14 +1,10 @@
 package com.DormitoryBack.domain.article.comment.domain.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.DormitoryBack.domain.article.comment.domain.entity.OrphanComment;
-import com.DormitoryBack.domain.article.domain.entity.Article;
 
 public interface OrphanCommentRepository extends JpaRepository<OrphanComment,Long> {
 
-    int countByArticle(Article article);
+    int countByArticleId(Long articleId); 
     
 }
