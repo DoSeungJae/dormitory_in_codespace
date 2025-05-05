e42a0a78ed148b38bfaa7393d8b21b5f545448c6
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-05-05 08:04:38 +0000

    [BACK, REFACTOR] JwtApi.java - ResponseEntity의 바디 타입 명시

--- back/src/main/java/com/DormitoryBack/domain/jwt/api/JwtApi.java
+++ back/src/main/java/com/DormitoryBack/domain/jwt/api/JwtApi.java
@@ -1,13 +1,21 @@
 package com.DormitoryBack.domain.jwt.api;
 
 
-import com.DormitoryBack.domain.jwt.TokenProvider;
-import com.DormitoryBack.domain.jwt.dto.JwtDTO;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.DormitoryBack.domain.jwt.TokenProvider;
+import com.DormitoryBack.domain.jwt.dto.JwtDTO;
+
+import lombok.extern.slf4j.Slf4j;
 
 @CrossOrigin(origins = {"https://improved-space-tribble-vjvwrwx956jh69w4-3000.app.github.dev", "https://turbo-chainsaw-rpvvwx9pp5c5p55-3000.app.github.dev"})
 @RestController
@@ -27,14 +35,15 @@ public class JwtApi {
         return message;
 
     }
+
     @PostMapping("/validate") //<boolean>(primitive) -> <Boolean>(wrapper) ResponseEntity의 body에 primitive type을 담을 수 없음.
-    public ResponseEntity tokenValidation(@RequestBody JwtDTO dto){
+    public ResponseEntity<Boolean> tokenValidation(@RequestBody JwtDTO dto){
         Boolean isValid=jwtService.validateToken(dto.getToken());
         return ResponseEntity.status(HttpStatus.OK).body(isValid);
     }
 
     @GetMapping("/userId")
-    public ResponseEntity fromTokenToUserId(@RequestHeader("Authorization") String token){
+    public ResponseEntity<?> fromTokenToUserId(@RequestHeader("Authorization") String token){
         Boolean isValid=jwtService.validateToken(token);
         if(!isValid){
             ResponseEntity
